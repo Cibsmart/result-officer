b85cc5fce614f872a96ef48b1a765b9d5b7c26bf
Barnabas Ifebude <doncibsmart@gmail.com>
2024-06-24 20:46:05 +0100

    Created Faculty, Department and Program Filament Resources

---
+++ app/Filament/Resources/FacultyResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FacultyResource\Pages;
+use App\Filament\Resources\FacultyResource\RelationManagers;
+use App\Models\Faculty;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class FacultyResource extends Resource
+{
+    protected static ?string $model = Faculty::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('code'),
+                TextInput::make('name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('code'),
+                TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFaculties::route('/'),
+            'create' => Pages\CreateFaculty::route('/create'),
+            'edit' => Pages\EditFaculty::route('/{record}/edit'),
+        ];
+    }
+}
