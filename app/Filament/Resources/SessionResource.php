6b5aebaffd0e1c1fb9f4b8fc9e4f3ee1d88964a4
Barnabas Ifebude <doncibsmart@gmail.com>
2024-06-25 21:44:20 +0100

    Created Session and Semester Resource

---
+++ app/Filament/Resources/SessionResource.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SessionResource\Pages;
+use App\Models\Session;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SessionResource extends Resource
+{
+    protected static ?string $model = Session::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Semester';
+
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Session')
+                    ->required()
+                    ->required()->rules(['min:7', 'max:7']),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->label('ID'),
+                TextColumn::make('name')->label('Session'),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSessions::route('/'),
+            'create' => Pages\CreateSession::route('/create'),
+            'edit' => Pages\EditSession::route('/{record}/edit'),
+        ];
+    }
+}
