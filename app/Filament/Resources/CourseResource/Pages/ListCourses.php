76c3d059d5d8d533aae2defd590bddd529c132d2
Barnabas Ifebude <doncibsmart@gmail.com>
2024-06-27 17:45:11 +0100

    Fixed Code Issues

--- app/Filament/Resources/CourseResource/Pages/ListCourses.php
+++ app/Filament/Resources/CourseResource/Pages/ListCourses.php
@@ -1,16 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\CourseResource\Pages;
 
 use App\Filament\Resources\CourseResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
-class ListCourses extends ListRecords
+final class ListCourses extends ListRecords
 {
 
     protected static string $resource = CourseResource::class;
 
+    /** @return array<int, \Filament\Actions\Action|\Filament\Actions\ActionGroup> */
     protected function getHeaderActions(): array
     {
         return [
