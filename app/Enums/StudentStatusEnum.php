56de4ed5ae5d434d2cbaa5dff68ae9c9efc62cc1
Barnabas Ifebude <doncibsmart@gmail.com>
2024-06-30 22:35:14 +0100

    Created Student Status Model, Migration and Seeder

---
+++ app/Enums/StudentStatusEnum.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum StudentStatusEnum: string
+{
+    case NEW = 'new';
+    case ACTIVE = 'active';
+    case PROBATION = 'probation';
+    case WITHDRAWN = 'withdrawn';
+    case EXPELLED = 'expelled';
+    case SUSPENDED = 'suspended';
+    case DECEASED = 'deceased';
+    case TRANSFERRED = 'transferred';
+    case GRADUATED = 'graduated';
+}
