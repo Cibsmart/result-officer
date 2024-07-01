b4c9c2bc1697354bfea1be2c10f48ff1cd65617c
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-01 16:08:36 +0100

    Moved Vue View Files and Controllers

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+
+final class HandleInertiaRequests extends Middleware
+{
+    /** @var string */
+    protected $rootView = 'app';
+
+    public function version(Request $request): ?string
+    {
+        return parent::version($request);
+    }
+
+    /**
+     * @return array<string, mixed>
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.DisallowMixedTypeHint
+     * */
+    public function share(Request $request): array
+    {
+        return [
+            ...parent::share($request),
+            'auth' => [
+                'user' => $request->user(),
+            ],
+        ];
+    }
+}
