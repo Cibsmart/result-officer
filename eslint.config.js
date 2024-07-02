80b22fd856b31b87424b3e7d9051f34c54ac5188
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-02 18:06:06 +0100

    Installed Eslint Config Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import pluginJs from "@eslint/js";
 import pluginTs from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default pluginTs.config(
   pluginJs.configs.recommended,
@@ -17,6 +18,42 @@ export default pluginTs.config(
         extraFileExtensions: [".vue"],
         sourceType: "module",
       },
+    },
+  },
+  eslintConfigPrettier,
+  {
+    rules: {
+      "vue/match-component-import-name": "warn",
+      "vue/match-component-file-name": [
+        "error",
+        {
+          extensions: ["vue"],
+          shouldMatchCase: true,
+        },
+      ],
+      "vue/component-definition-name-casing": ["error", "PascalCase"],
+      "vue/block-tag-newline": [
+        "warn",
+        {
+          singleline: "always",
+          multiline: "always",
+          maxEmptyLines: 0,
+        },
+      ],
+      "vue/html-self-closing": [
+        "error",
+        {
+          html: {
+            void: "any",
+            normal: "always",
+            component: "always",
+          },
+          svg: "always",
+          math: "always",
+        },
+      ],
+      "vue/require-default-prop": "off",
+      "vue/multi-word-component-names": "off",
     },
   },
 );
