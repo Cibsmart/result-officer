0e0e468d1c1343378d0b3f8352a90250c87d0025
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-02 12:04:42 +0100

    Installed Tailwin Prettier Plugin for Formatting Tailwind Classes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import {Head} from "@inertiajs/vue3";
+import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
-  <Head title="Dashboard"/>
+  <Head title="Dashboard" />
 
   <AuthenticatedLayout>
     <template #header>
