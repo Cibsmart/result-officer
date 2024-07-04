dd5d133cd3be5521b0ea4eb96d228c61c3a2b8a2
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-04 12:17:24 +0100

    Removed Unnecessary Filament Resources

--- app/Filament/Resources/SessionResource.php
+++ app/Filament/Resources/SessionResource.php
@@ -18,7 +18,7 @@ final class SessionResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?string $navigationGroup = 'Semester';
+    protected static ?string $navigationGroup = 'Setup';
 
     protected static ?int $navigationSort = 1;
 
