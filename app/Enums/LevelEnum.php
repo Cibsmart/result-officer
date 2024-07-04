b834506ee83f7ba83ecc3742dcb1dde2f1353bc7
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-04 17:33:05 +0100

    Created Level Enum

---
+++ app/Enums/LevelEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum LevelEnum: int
+{
+    case LEVEL_100 = 100;
+    case LEVEL_200 = 200;
+    case LEVEL_300 = 300;
+    case LEVEL_400 = 400;
+    case LEVEL_500 = 500;
+    case LEVEL_600 = 600;
+}
