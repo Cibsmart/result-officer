6a1a7484bed0f5d9738546202a6afcb3cccadb6b
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-06 21:31:23 +0100

    Subject: Persistent Layout
    Body:
    - Applied Guest Persistent Layout to the Guest Pages

--- resources/js/pages/auth/confirmPassword.vue
+++ resources/js/pages/auth/confirmPassword.vue
@@ -6,6 +6,8 @@ import TextInput from "@/components/textInput.vue";
 import LayoutGuest from "@/layouts/guest/layoutGuest.vue";
 import { Head, useForm } from "@inertiajs/vue3";
 
+defineOptions({ layout: LayoutGuest });
+
 const form = useForm({
   password: "",
 });
@@ -20,41 +22,39 @@ const submit = () => {
 </script>
 
 <template>
-  <LayoutGuest>
-    <Head title="Confirm Password" />
+  <Head title="Confirm Password" />
 
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-      This is a secure area of the application. Please confirm your password before continuing.
+  <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+    This is a secure area of the application. Please confirm your password before continuing.
+  </div>
+
+  <form @submit.prevent="submit">
+    <div>
+      <InputLabel
+        for="password"
+        value="Password" />
+
+      <TextInput
+        id="password"
+        v-model="form.password"
+        autocomplete="current-password"
+        autofocus
+        class="mt-1 block w-full"
+        required
+        type="password" />
+
+      <InputError
+        :message="form.errors.password"
+        class="mt-2" />
     </div>
 
-    <form @submit.prevent="submit">
-      <div>
-        <InputLabel
-          for="password"
-          value="Password" />
-
-        <TextInput
-          id="password"
-          v-model="form.password"
-          autocomplete="current-password"
-          autofocus
-          class="mt-1 block w-full"
-          required
-          type="password" />
-
-        <InputError
-          :message="form.errors.password"
-          class="mt-2" />
-      </div>
-
-      <div class="mt-4 flex justify-end">
-        <PrimaryButton
-          :class="{ 'opacity-25': form.processing }"
-          :disabled="form.processing"
-          class="ms-4">
-          Confirm
-        </PrimaryButton>
-      </div>
-    </form>
-  </LayoutGuest>
+    <div class="mt-4 flex justify-end">
+      <PrimaryButton
+        :class="{ 'opacity-25': form.processing }"
+        :disabled="form.processing"
+        class="ms-4">
+        Confirm
+      </PrimaryButton>
+    </div>
+  </form>
 </template>
--- resources/js/pages/auth/resetPassword.vue
+++ resources/js/pages/auth/resetPassword.vue
@@ -6,6 +6,8 @@ import TextInput from "@/components/textInput.vue";
 import LayoutGuest from "@/layouts/guest/layoutGuest.vue";
 import { Head, useForm } from "@inertiajs/vue3";
 
+defineOptions({ layout: LayoutGuest });
+
 const props = defineProps<{
   email: string;
   token: string;
@@ -28,72 +30,70 @@ const submit = () => {
 </script>
 
 <template>
-  <LayoutGuest>
-    <Head title="Reset Password" />
+  <Head title="Reset Password" />
 
-    <form @submit.prevent="submit">
-      <div>
-        <InputLabel
-          for="email"
-          value="Email" />
+  <form @submit.prevent="submit">
+    <div>
+      <InputLabel
+        for="email"
+        value="Email" />
 
-        <TextInput
-          id="email"
-          v-model="form.email"
-          autocomplete="username"
-          autofocus
-          class="mt-1 block w-full"
-          required
-          type="email" />
+      <TextInput
+        id="email"
+        v-model="form.email"
+        autocomplete="username"
+        autofocus
+        class="mt-1 block w-full"
+        required
+        type="email" />
 
-        <InputError
-          :message="form.errors.email"
-          class="mt-2" />
-      </div>
+      <InputError
+        :message="form.errors.email"
+        class="mt-2" />
+    </div>
 
-      <div class="mt-4">
-        <InputLabel
-          for="password"
-          value="Password" />
+    <div class="mt-4">
+      <InputLabel
+        for="password"
+        value="Password" />
 
-        <TextInput
-          id="password"
-          v-model="form.password"
-          autocomplete="new-password"
-          class="mt-1 block w-full"
-          required
-          type="password" />
+      <TextInput
+        id="password"
+        v-model="form.password"
+        autocomplete="new-password"
+        class="mt-1 block w-full"
+        required
+        type="password" />
 
-        <InputError
-          :message="form.errors.password"
-          class="mt-2" />
-      </div>
+      <InputError
+        :message="form.errors.password"
+        class="mt-2" />
+    </div>
 
-      <div class="mt-4">
-        <InputLabel
-          for="password_confirmation"
-          value="Confirm Password" />
+    <div class="mt-4">
+      <InputLabel
+        for="password_confirmation"
+        value="Confirm Password" />
 
-        <TextInput
-          id="password_confirmation"
-          v-model="form.password_confirmation"
-          autocomplete="new-password"
-          class="mt-1 block w-full"
-          required
-          type="password" />
+      <TextInput
+        id="password_confirmation"
+        v-model="form.password_confirmation"
+        autocomplete="new-password"
+        class="mt-1 block w-full"
+        required
+        type="password" />
 
-        <InputError
-          :message="form.errors.password_confirmation"
-          class="mt-2" />
-      </div>
+      <InputError
+        :message="form.errors.password_confirmation"
+        class="mt-2" />
+    </div>
 
-      <div class="mt-4 flex items-center justify-end">
-        <PrimaryButton
-          :class="{ 'opacity-25': form.processing }"
-          :disabled="form.processing">
-          Reset Password
-        </PrimaryButton>
-      </div>
-    </form>
-  </LayoutGuest>
+    <div class="mt-4 flex items-center justify-end">
+      <PrimaryButton
+        :class="{ 'opacity-25': form.processing }"
+        :disabled="form.processing">
+        Reset Password
+      </PrimaryButton>
+    </div>
+  </form>
 </template>
