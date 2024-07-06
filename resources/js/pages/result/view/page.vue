fb0f3aaa1fd602063c4a431eb691582b0ac05fff
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-06 15:53:51 +0100

    Subject: View Student Result Form
    Body:
    - Created View Student Result Component

--- resources/js/pages/result/view/page.vue
+++ resources/js/pages/result/view/page.vue
@@ -1,13 +1,8 @@
 <script lang="ts" setup>
 import { Head } from "@inertiajs/vue3";
-import LayoutMain from "@/layouts/main/layoutMain.vue";
 import BasePage from "@/layouts/main/partials/basePage.vue";
 import BaseHeader from "@/layouts/main/partials/baseHeader.vue";
-import UpdatePasswordForm from "@/pages/profile/partials/updatePasswordForm.vue";
-import DeleteUserForm from "@/pages/profile/partials/deleteUserForm.vue";
-import UpdateProfileInformationForm from "@/pages/profile/partials/updateProfileInformationForm.vue";
-
-defineOptions({ layout: LayoutMain });
+import StudentResultForm from "@/pages/result/view/partials/studentResultForm.vue";
 </script>
 
 <template>
@@ -19,9 +14,7 @@ defineOptions({ layout: LayoutMain });
     <div class="py-12">
       <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
         <div class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
-          <UpdateProfileInformationForm
-            :status="status"
-            class="max-w-xl" />
+          <StudentResultForm class="max-w-xl" />
         </div>
       </div>
     </div>
