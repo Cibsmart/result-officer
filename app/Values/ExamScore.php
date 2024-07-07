adcce0ca0c13100bedbe902aba7178e5407f7123
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-07 19:33:39 +0100

    Subject: ExamScore Value Object
    Body:
    - Implemented ExamScore Value Object
    - Wrote Unit Test for the ExamScore Value Object

---
+++ app/Values/ExamScore.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Values;
+
+use InvalidArgumentException;
+
+final readonly class ExamScore
+{
+    public function __construct(private int $value)
+    {
+        if ($value < 0 || $value > 70) {
+            throw new InvalidArgumentException('Value must be between 0 and 70');
+        }
+    }
+}
