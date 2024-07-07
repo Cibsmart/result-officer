330f805bf24d4263f3dbe2bafb16e0e6d22ca491
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-07 19:50:51 +0100

    Subject: Value Objects
    Body:
    - Made Property Public
    - Added Test Suffix to Test Files

--- app/Values/ExamScore.php
+++ app/Values/ExamScore.php
@@ -6,7 +6,7 @@ use InvalidArgumentException;
 
 final readonly class ExamScore
 {
-    public function __construct(private int $value)
+    public function __construct(public int $value)
     {
         if ($value < 0 || $value > 70) {
             throw new InvalidArgumentException('Value must be between 0 and 70');
--- app/Values/InCourseScore.php
+++ app/Values/InCourseScore.php
@@ -6,7 +6,7 @@ use InvalidArgumentException;
 
 final readonly class InCourseScore
 {
-    public function __construct(private int $value)
+    public function __construct(public int $value)
     {
         if ($value < 0 || $value > 30) {
             throw new InvalidArgumentException('In-course score value must be between 0 and 30');
