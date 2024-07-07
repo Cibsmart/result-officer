b2fd5c7e94d7d02dfbb51e02dedc186c1fdf62e7
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-07 14:46:40 +0100

    Subject: Backend Code Quality
    Body:
    - Code Code Fixes

--- app/Classes/Score.php
+++ app/Classes/Score.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Classes;
 
 final readonly class Score
 {
     public function __construct(
         private int $course_work,
-        private int $exam
+        private int $exam,
     ) {
     }
 
