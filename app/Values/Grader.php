2f3fe97341c8b29f24ff8c1f7ad6739dc45831e2
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-07 20:38:30 +0100

    Subject: Grader Value Object
    Body:
    - Replace Multiple IFs with Match Expression

--- app/Values/Grader.php
+++ app/Values/Grader.php
@@ -5,35 +5,26 @@ declare(strict_types=1);
 namespace App\Values;
 
 use App\Enums\Grade;
+use InvalidArgumentException;
 
 final readonly class Grader
 {
     public function __construct(private Score $score)
     {
+        if ($this->score->value() < 0 || $this->score->value() > 100) {
+            throw new InvalidArgumentException('Score must be between 0 and 100');
+        }
     }
 
     public function grade(): Grade
     {
-        if ($this->score->value() >= 70 && $this->score->value() <= 100) {
-            return Grade::A;
-        }
-
-        if ($this->score->value() >= 60 && $this->score->value() < 70) {
-            return Grade::B;
-        }
-
-        if ($this->score->value() >= 50 && $this->score->value() < 60) {
-            return Grade::C;
-        }
-
-        if ($this->score->value() >= 45 && $this->score->value() < 50) {
-            return Grade::D;
-        }
-
-        if ($this->score->value() >= 40 && $this->score->value() < 45) {
-            return Grade::E;
-        }
-
-        return Grade::F;
+        return match (true) {
+            $this->score->value() >= 70 && $this->score->value() <= 100 => Grade::A,
+            $this->score->value() >= 60 && $this->score->value() < 70 => Grade::B,
+            $this->score->value() >= 50 && $this->score->value() < 60 => Grade::C,
+            $this->score->value() >= 45 && $this->score->value() < 50 => Grade::D,
+            $this->score->value() >= 40 && $this->score->value() < 45 => Grade::E,
+            default => Grade::F,
+        };
     }
 }
