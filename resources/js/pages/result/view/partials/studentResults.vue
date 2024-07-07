ca82a1960edcecaf9b662062031dc7677bd378ae
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-07 19:33:39 +0100

    Subject: ExamScore Value Object
    Body:
    - Implemented ExamScore Value Object
    - Wrote Unit Test for the ExamScore Value Object

--- resources/js/pages/result/view/partials/studentResults.vue
+++ resources/js/pages/result/view/partials/studentResults.vue
@@ -1,20 +1,11 @@
-<script lang="ts" setup>
-import PrimaryButton from "@/components/primaryButton.vue";
-import TextInput from "@/components/textInput.vue";
-import InputLabel from "@/components/inputLabel.vue";
-import InputError from "@/components/inputError.vue";
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <section>
     <header>
-      <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Result Information</h2>
+      <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Student Results</h2>
 
       <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">Input student's matriculation number to view results</p>
     </header>
-
-    <form
-      class="mt-6 space-y-6"
-      @submit.prevent="form.patch(route('profile.update'))"></form>
   </section>
 </template>
---
+++ tests/Unit/Values/ExamScore.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Values\ExamScore;
+
+test('throws no exception for valid exam scores', function () {
+    new ExamScore(0);
+    new ExamScore(50);
+    new ExamScore(70);
+})->throwsNoExceptions();
+
+test('throws exception for negative exam score', function () {
+    new ExamScore(-1);
+})->throws(InvalidArgumentException::class, 'Value must be between 0 and 70');
+
+test('throws exception for exam score greater than 70', function () {
+    new ExamScore(71);
+})->throws(InvalidArgumentException::class, 'Value must be between 0 and 70');
