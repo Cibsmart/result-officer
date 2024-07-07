2686e7489e6ec6a229b746dce8e2609a0693d41f
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-07 14:44:10 +0100

    Subject: Enum and Classes
    Body:
    - Created Grade Enum
    - Created Score and Grader Classes

---
+++ app/Classes/Score.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Classes;
+
+final readonly class Score
+{
+    public function __construct(
+        private int $course_work,
+        private int $exam
+    ) {
+    }
+
+    public function total(): int
+    {
+        return $this->course_work + $this->exam;
+    }
+}
