e67c99a604ab16bd24e1751b642a740e14c11245
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-08 17:06:43 +0100

    Subject: Session Result DTO
    Body:
    - Created and Implemented Session Result DTO

---
+++ app/Data/Results/SessionResultData.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Data\Results;
+
+use App\Models\Enrollment;
+use App\Models\Semester;
+use Illuminate\Support\Collection;
+use Spatie\LaravelData\Data;
+
+class SessionResultData extends Data
+{
+    public function __construct(
+        /** @var \Illuminate\Support\Collection<\App\Data\Results\SemesterResultData> */
+        public Collection $semesterResults,
+        public string $session,
+        public float $cummulativeGradePointAverage
+    ) {
+    }
+
+    public static function fromModel(Enrollment $enrollment): self
+    {
+        $first = Semester::query()->findOrFail(1);
+        $second = Semester::query()->findOrFail(2);
+
+        $semesterResults = collect(SemesterResultData::collect([
+            SemesterResultData::fromModel($first, $enrollment),
+            SemesterResultData::fromModel($second, $enrollment),
+        ]));
+
+        $cumulativeGradePointAverage = round(
+            $semesterResults->sum('gradePointAverage') / $semesterResults->count(),
+            3
+        );
+
+        return new self(
+            $semesterResults,
+            $enrollment->session->name,
+            $cumulativeGradePointAverage
+        );
+    }
+}
