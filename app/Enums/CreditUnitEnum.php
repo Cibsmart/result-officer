d788902aa764c2060d89cca6680150f37755f301
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-08 15:40:54 +0100

    Subject: CreditUnit Value Object
    Body:
    - Created CreditUnit Value Object
    - Created Credit Unit Enum
    - Wrote Unit Test for CreditUnit Value Object

---
+++ app/Enums/CreditUnitEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+enum CreditUnitEnum: int
+{
+    case ZERO = 0;
+    case ONE = 1;
+    case TWO = 2;
+    case THREE = 3;
+    case FOUR = 4;
+    case FIVE = 5;
+    case SIX = 6;
+    case TWELVE = 12;
+    case FIFTY = 15;
+    case EIGHTEEN = 18;
+}
---
+++ app/Values/CreditUnit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Values;
+
+use App\Enums\CreditUnitEnum;
+use InvalidArgumentException;
+
+class CreditUnit
+{
+    public function __construct(public int $value)
+    {
+        if (is_null(CreditUnitEnum::tryFrom($this->value))) {
+            throw new InvalidArgumentException('The credit unit is not a valid credit unit value');
+        }
+    }
+
+    public static function new(int $value): self
+    {
+        return new self($value);
+    }
+}
