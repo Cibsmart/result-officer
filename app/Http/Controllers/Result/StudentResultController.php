8818e3416ef8e050098a22c7ac06aa33d879ed4c
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-08 21:29:18 +0100

    Subject: Result View Page
    Body:
    - Created ResultViewPage ViewModel
    - Created Result View Page Component
    - Updated Routes and Navigation to Reflect the changes

--- app/Http/Controllers/Result/StudentResultController.php
+++ app/Http/Controllers/Result/StudentResultController.php
@@ -4,21 +4,31 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Result;
 
+use App\Data\Results\StudentResultData;
+use App\Data\Students\StudentData;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Results\ResultRequest;
+use App\Models\Student;
+use App\Pages\Results\ResultViewPage;
 use Inertia\Inertia;
 use Inertia\Response;
 
 final class StudentResultController extends Controller
 {
-    public function page(): Response
+    public function form(): Response
     {
-        return Inertia::render('result/view/page');
+        return Inertia::render('results/form/page');
     }
 
-    public function results(ResultRequest $request)
+    public function view(ResultRequest $request): Response
     {
-        $input = $request->input('matriculation_number');
-        dd($input);
+        $matriculationNumber = $request->input('matriculation_number');
+
+        $studentModel = Student::query()->where('matriculation_number', $matriculationNumber)->firstOrFail();
+
+        return Inertia::render('results/view/page', new ResultViewPage(
+            student: StudentData::from($studentModel),
+            results: StudentResultData::from($studentModel)
+        ));
     }
 }
---
+++ resources/js/pages/results/view/page.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { Head } from "@inertiajs/vue3";
+import BasePage from "@/layouts/main/partials/basePage.vue";
+import BaseHeader from "@/layouts/main/partials/baseHeader.vue";
+import StudentResults from "@/pages/results/view/partials/table.vue";
+import BaseSection from "@/components/baseSection.vue";
+import { BreadcrumbItem } from "@/types";
+import Breadcrumb from "@/components/breadcrumb.vue";
+
+defineProps<{
+  student?: App.Data.Students.StudentData;
+  results?: App.Data.Results.StudentResultData;
+}>();
+
+const pages: BreadcrumbItem[] = [
+  { name: "Result Form", href: route("results.form"), current: route().current("results.form") },
+  { name: "Result View", href: route("results.view"), current: route().current("results.view") },
+];
+</script>
+
+<template>
+  <Head title="View Student Result" />
+
+  <Breadcrumb :pages="pages" />
+
+  <BaseHeader> View Student Results</BaseHeader>
+
+  <BasePage>
+    <BaseSection> </BaseSection>
+  </BasePage>
+</template>
