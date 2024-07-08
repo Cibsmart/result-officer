425564a9ee9bde8c443ea7e4aa7e88e916d4d717
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-08 19:18:26 +0100

    Subject: Student DTO
    Body:
    - Created and Implemented Student DTO

---
+++ app/Data/Students/StudentData.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Data\Students;
+
+use App\Enums\GenderEnum;
+use App\Models\Student;
+use Carbon\Carbon;
+use Spatie\LaravelData\Data;
+
+final class StudentData extends Data
+{
+    public function __construct(
+        public readonly string $matriculationNumber,
+        public readonly string $name,
+        public readonly string $lastName,
+        public readonly string $firstName,
+        public readonly ?string $otherNames,
+        public readonly GenderEnum $gender,
+        public readonly Carbon $birthDate,
+        public readonly string $program,
+        public readonly string $department,
+        public readonly string $faculty,
+        public readonly string $admissionYear,
+    ) {
+    }
+
+    public static function fromModel(Student $student): StudentData
+    {
+        return new self(
+            $student->matriculation_number,
+            $student->name,
+            $student->last_name,
+            $student->first_name,
+            $student->other_names,
+            $student->gender,
+            $student->date_of_birth,
+            $student->program->name,
+            $student->program->department->name,
+            $student->program->department->faculty->name,
+            $student->entrySession->name
+        );
+    }
+}
