14aaee1b481f547fbaec030c0272c37ee789118c
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-09 21:20:48 +0100

    Subject: View Result Page
    Body:
    - Created Session Result Partial
    - Created Semester Result Partial
    - Implemented the View Result Page

---
+++ resources/js/pages/results/view/partials/session.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import Semester from "@/pages/results/view/partials/semester.vue";
+
+defineProps<{
+  session: App.Data.Results.SessionResultData;
+}>();
+</script>
+
+<template>
+  <div class="-mx-4 mt-5 px-3 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg dark:ring-gray-600">
+    <div class="min-w-full pt-2 text-center text-lg font-extrabold">{{ session.session }} SESSION</div>
+
+    <template
+      v-for="semester in session.semesterResults"
+      :key="semester.id">
+      <Semester :semester="semester" />
+    </template>
+
+    <div class="p-2 text-center">
+      <span class="font-bold uppercase"> CGPA: {{ session.cummulativeGradePointAverage }} </span>
+    </div>
+  </div>
+</template>
