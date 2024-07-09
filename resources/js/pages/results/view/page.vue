ecc4f324fb07d6cd8e81a4ae99e09349b65567cf
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-09 21:20:48 +0100

    Subject: View Result Page
    Body:
    - Created Session Result Partial
    - Created Semester Result Partial
    - Implemented the View Result Page

--- resources/js/pages/results/view/page.vue
+++ resources/js/pages/results/view/page.vue
@@ -6,6 +6,7 @@ import BaseSection from "@/components/baseSection.vue";
 import { BreadcrumbItem } from "@/types";
 import Breadcrumb from "@/components/breadcrumb.vue";
 import ButtonIndigo from "@/components/buttons/buttonIndigo.vue";
+import Session from "@/pages/results/view/partials/session.vue";
 
 defineProps<{
   student: App.Data.Students.StudentData;
@@ -15,26 +16,6 @@ defineProps<{
 const pages: BreadcrumbItem[] = [
   { name: "Result Form", href: route("results.form"), current: route().current("results.form") },
   { name: "Result View", href: route("results.view"), current: route().current("results.view") },
-];
-
-const plans = [
-  {
-    id: 1,
-    name: "CSC 101",
-    memory: "INTRODUCTION TO COMPUTER SCIENCE",
-    storage: 3,
-    price: "A",
-    isCurrent: false,
-  },
-  {
-    id: 2,
-    name: "CSC 102",
-    memory: "INTRODUCTION TO PROGRAMMING",
-    storage: 3,
-    price: "F",
-    isCurrent: true,
-  },
-  // More plans...
 ];
 </script>
 
@@ -47,98 +28,26 @@ const plans = [
 
   <BasePage>
     <BaseSection>
-      <div class="px-4 sm:px-6 lg:px-8 dark:bg-gray-900">
+      <div class="rounded px-4 py-4 sm:px-6 lg:px-8 dark:bg-gray-900">
         <div class="sm:flex sm:items-center">
           <div class="sm:flex-auto">
             <h1 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">Student Results</h1>
 
             <p class="mt-2 text-sm text-gray-700 dark:text-gray-300">
-              This page is shows all results for the selected student
+              This page shows all results for the selected student
             </p>
           </div>
 
           <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-            <ButtonIndigo> Print</ButtonIndigo>
+            <ButtonIndigo>Print</ButtonIndigo>
           </div>
         </div>
 
-        <div class="-mx-4 mt-10 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg dark:ring-gray-600">
-          <table class="min-w-full divide-y divide-gray-300 dark:divide-gray-700">
-            <thead>
-              <tr>
-                <th
-                  class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 dark:text-white"
-                  scope="col">
-                  Course Code
-                </th>
-
-                <th
-                  class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
-                  scope="col">
-                  Course Title
-                </th>
-
-                <th
-                  class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
-                  scope="col">
-                  Credit Unit
-                </th>
-
-                <th
-                  class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-white"
-                  scope="col">
-                  Grade
-                </th>
-              </tr>
-            </thead>
-
-            <tbody>
-              <tr
-                v-for="(plan, planIdx) in plans"
-                :key="plan.id">
-                <td
-                  class="relative py-4 pl-4 pr-3 text-sm sm:pl-6"
-                  :class="[planIdx === 0 ? '' : 'border-t border-transparent']">
-                  <div class="font-medium text-gray-900 dark:text-white">
-                    {{ plan.name }}
-                  </div>
-
-                  <div class="mt-1 flex flex-col text-gray-700 sm:block lg:hidden dark:text-gray-300">
-                    <span>{{ plan.memory }} </span>
-
-                    <span class="hidden sm:inline"> - </span>
-
-                    <span>{{ plan.storage }}</span>
-                  </div>
-
-                  <div
-                    v-if="planIdx !== 0"
-                    class="absolute -top-px left-6 right-0 h-px bg-gray-200 dark:bg-gray-700" />
-                </td>
-
-                <td
-                  class="hidden px-3 py-3.5 text-sm text-gray-700 lg:table-cell dark:text-gray-300"
-                  :class="[planIdx === 0 ? '' : 'border-t border-gray-200 dark:border-gray-700']">
-                  {{ plan.memory }}
-                </td>
-
-                <td
-                  class="hidden px-3 py-3.5 text-sm text-gray-700 lg:table-cell dark:text-gray-300"
-                  :class="[planIdx === 0 ? '' : 'border-t border-gray-200 dark:border-gray-700']">
-                  {{ plan.storage }}
-                </td>
-
-                <td
-                  class="px-3 py-3.5 text-sm text-gray-700 dark:text-gray-300"
-                  :class="[planIdx === 0 ? '' : 'border-t border-gray-200 dark:border-gray-700']">
-                  <div class="sm:hidden">{{ plan.price }}</div>
-
-                  <div class="hidden sm:block">{{ plan.price }}</div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <template
+          v-for="session in results?.enrollments"
+          :key="session.id">
+          <Session :session="session" />
+        </template>
       </div>
     </BaseSection>
   </BasePage>
---
+++ resources/js/pages/results/view/partials/semester.vue
@@ -0,0 +1,151 @@
+<script lang="ts" setup>
+defineProps<{
+  semester: App.Data.Results.SemesterResultData;
+}>();
+</script>
+
+<template>
+  <div class="-mx-4 mt-2 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg dark:ring-gray-600">
+    <div class="min-w-full pt-2 text-center text-sm font-extrabold">{{ semester.semester }} SEMESTER</div>
+
+    <table class="min-w-full divide-y divide-gray-300 dark:divide-gray-700">
+      <thead>
+        <tr>
+          <th
+            class="hidden px-3 py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+            scope="col">
+            SN
+          </th>
+
+          <th
+            class="py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 dark:text-white"
+            scope="col">
+            Course Code
+          </th>
+
+          <th
+            class="hidden px-3 py-2 text-left text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+            scope="col">
+            Course Title
+          </th>
+
+          <th
+            class="hidden px-3 py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+            scope="col">
+            Credit Unit
+          </th>
+
+          <th
+            class="hidden px-3 py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+            scope="col">
+            Score
+          </th>
+
+          <th
+            class="px-3 py-2 text-center text-sm font-semibold text-gray-900 dark:text-white"
+            scope="col">
+            Grade
+          </th>
+
+          <th
+            class="hidden px-3 py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+            scope="col">
+            Grade Point
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr
+          v-for="(result, index) in semester.results"
+          :key="result.id">
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
+            {{ index + 1 }}
+          </td>
+
+          <td class="relative border-t border-gray-200 py-2 pl-4 pr-3 text-sm sm:pl-6 dark:border-gray-700">
+            <div class="font-medium text-gray-900 dark:text-white">
+              {{ result.courseCode }}
+            </div>
+
+            <div class="mt-1 flex flex-col text-gray-700 sm:block lg:hidden dark:text-gray-300">
+              <span>{{ result.courseTitle }} </span>
+
+              <span class="hidden sm:inline"> || </span>
+
+              <span>
+                <span>Unit: {{ result.creditUnit }} |</span>
+
+                <span>| Score: {{ result.totalScore }} </span>
+              </span>
+            </div>
+
+            <div
+              v-if="index !== 0"
+              class="absolute -top-px left-6 right-0 h-px bg-gray-200 dark:bg-gray-700" />
+          </td>
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
+            {{ result.courseTitle }}
+          </td>
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
+            {{ result.creditUnit }}
+          </td>
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
+            {{ result.totalScore }}
+          </td>
+
+          <td
+            class="border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 dark:border-gray-700 dark:text-gray-300">
+            <div class="sm:hidden">{{ result.grade }}</div>
+
+            <div class="hidden sm:block">{{ result.grade }}</div>
+          </td>
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
+            {{ result.gradePoint }}
+          </td>
+        </tr>
+
+        <tr>
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm font-bold text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300"
+            colspan="3">
+            TOTAL
+          </td>
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm font-bold text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
+            {{ semester.totalCreditUnit }}
+          </td>
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300" />
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300" />
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm font-bold text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
+            {{ semester.totalGradePoint }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="flex justify-around pb-2 text-right">
+      <span class="lg:hidden">Total Credit Unit: {{ semester.totalCreditUnit }}</span>
+
+      <span class="lg:hidden">Total Grade Point: {{ semester.totalGradePoint }}</span>
+
+      <span class="font-bold">GPA: {{ semester.gradePointAverage }}</span>
+    </div>
+  </div>
+</template>
