f998cad50165deb8ba1ec6e64cd494138dca3638
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-10 22:50:37 +0100

    Subject: Value Object
    Body:
    - Implemented Matriculation Number value object
    - Wrote Test for the value object

---
+++ app/Values/MatriculationNumber.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Values;
+
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+
+final readonly class MatriculationNumber
+{
+    public function __construct(public string $value)
+    {
+        if (! preg_match('/^EBSU\/\d{4}\/\d{4,6}[A-Z]?$/i', Str::trim($this->value))) {
+            throw new InvalidArgumentException('Invalid matriculation number');
+        }
+    }
+
+    public static function new(string $value): self
+    {
+        return new self($value);
+    }
+}
