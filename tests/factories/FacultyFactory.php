14015d0181b38051f0b15579a3674548d72b62bc
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-11 12:01:46 +0100

    Subject: Factories
    Body:
    - Implemented Faculty, Department and Program Factories

---
+++ tests/factories/FacultyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Factories;
+
+use App\Models\Faculty;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Faculty>
+ */
+class FacultyFactory extends Factory
+{
+    protected $model = Faculty::class;
+
+    /** @return array<string, string> */
+    public function definition(): array
+    {
+        return [
+            'code' => fake()->unique()->countryCode,
+            'name' => fake()->country,
+        ];
+    }
+}
---
+++ tests/factories/ProgramFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Factories;
+
+use App\Models\Program;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Program>
+ */
+class ProgramFactory extends Factory
+{
+    protected $model = Program::class;
+
+    /**
+     * Define the model's default state.
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'department_id' => DepartmentFactory::new(),
+            'code' => fake()->unique()->countryCode,
+            'name' => fake()->country,
+        ];
+    }
+}
