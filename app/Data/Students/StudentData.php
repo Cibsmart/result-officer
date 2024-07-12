d43c938381e348c6ae7af1df1c022e1abc3cea92
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-12 18:57:51 +0100

    Subject: DTO Tests
    Body:
    - Wrote test for Student DTO

--- app/Data/Students/StudentData.php
+++ app/Data/Students/StudentData.php
@@ -7,21 +7,17 @@ namespace App\Data\Students;
 use App\Enums\GenderEnum;
 use App\Models\Student;
 use Carbon\Carbon;
-use Spatie\LaravelData\Attributes\WithCast;
-use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
-use Spatie\LaravelData\Casts\EnumCast;
 use Spatie\LaravelData\Data;
 
 final class StudentData extends Data
 {
     public function __construct(
+        public readonly int $id,
         public readonly string $matriculationNumber,
         public readonly string $lastName,
         public readonly string $firstName,
         public readonly ?string $otherNames,
-        #[WithCast(EnumCast::class)]
         public readonly GenderEnum $gender,
-        #[WithCast(DateTimeInterfaceCast::class)]
         public readonly Carbon $birthDate,
         public readonly string $program,
         public readonly string $department,
@@ -33,16 +29,17 @@ final class StudentData extends Data
     public static function fromModel(Student $student): self
     {
         return new self(
-            $student->matriculation_number,
-            $student->last_name,
-            $student->first_name,
-            $student->other_names,
-            $student->gender,
-            $student->date_of_birth,
-            $student->program->name,
-            $student->program->department->name,
-            $student->program->department->faculty->name,
-            $student->entrySession->name,
+            id: $student->id,
+            matriculationNumber: $student->matriculation_number,
+            lastName: $student->last_name,
+            firstName: $student->first_name,
+            otherNames: $student->other_names,
+            gender: $student->gender,
+            birthDate: $student->date_of_birth,
+            program: $student->program->name,
+            department: $student->program->department->name,
+            faculty: $student->program->department->faculty->name,
+            admissionYear: $student->entrySession->name,
         );
     }
 }
---
+++ tests/Feature/Data/StudentDataTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Data\Students\StudentData;
+use Tests\Factories\StudentFactory;
+
+test('student data object is correct', function (): void {
+    $student = StudentFactory::new()->create();
+
+    $studentData = StudentData::from($student);
+
+    expect($studentData)->toBeInstanceOf(StudentData::class)
+        ->and($studentData->matriculationNumber)->toBe($student->matriculation_number)
+        ->and($studentData->lastName)->toBe($student->last_name)
+        ->and($studentData->firstName)->toBe($student->first_name)
+        ->and($studentData->gender)->toBe($student->gender)
+        ->and($studentData->birthDate->equalTo($student->date_of_birth))->toBeTrue()
+        ->and($studentData->program)->toBe($student->program->name)
+        ->and($studentData->department)->toBe($student->program->department->name)
+        ->and($studentData->faculty)->toBe($student->program->department->faculty->name)
+        ->and($studentData->admissionYear)->toBe($student->entrySession->name);
+});
