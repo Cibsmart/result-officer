fb198a0c0d243f5a0a2dbe0e123fe446a260c58f
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-13 11:03:43 +0100

    Subject: Class of Degree
    Body:
    - Created a Class of Degree Enum
    - Implemented a service to compute Class of Degree

---
+++ app/Enums/DegreeClass.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum DegreeClass: string
+{
+    case FIRST_CLASS = 'FIRST CLASS HONOURS';
+    case SECOND_CLASS_UPPER = 'SECOND CLASS HONOURS (UPPER DIVISION)';
+    case SECOND_CLASS_LOWER = 'SECOND CLASS HONOURS (LOWER DIVISION)';
+    case THIRD_CLASS = 'THIRD CLASS HONOURS';
+    case PASS = 'PASS';
+    case FAIL = 'FAIL';
+}
