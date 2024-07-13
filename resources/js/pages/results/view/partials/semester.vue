d37aa043989c8cf30e02fec7e6cd6ee5678bf3c4
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-13 10:11:41 +0100

    Subject: No Result Presentation
    Body:
    - Added check for NR remark and - for courses without result
    - Added a Column in the Semester Result Table for GPA

--- resources/js/pages/results/view/partials/semester.vue
+++ resources/js/pages/results/view/partials/semester.vue
@@ -50,7 +50,13 @@ defineProps<{
           <th
             class="hidden px-3 py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
             scope="col">
-            Grade Point
+            GP
+          </th>
+
+          <th
+            class="hidden px-3 py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+            scope="col">
+            GPA
           </th>
         </tr>
       </thead>
@@ -77,7 +83,7 @@ defineProps<{
               <span>
                 <span>Unit: {{ result.creditUnit }} |</span>
 
-                <span>| Score: {{ result.totalScore }} </span>
+                <span>| Score: {{ result.remark === "NR" ? "-" : result.totalScore }} </span>
               </span>
             </div>
 
@@ -98,20 +104,23 @@ defineProps<{
 
           <td
             class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
-            {{ result.totalScore }}
+            {{ result.remark === "NR" ? "-" : result.totalScore }}
           </td>
 
           <td
             class="border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 dark:border-gray-700 dark:text-gray-300">
-            <div class="sm:hidden">{{ result.grade }}</div>
+            <div class="sm:hidden">{{ result.remark === "NR" ? "-" : result.grade }}</div>
 
-            <div class="hidden sm:block">{{ result.grade }}</div>
+            <div class="hidden sm:block">{{ result.remark === "NR" ? "-" : result.grade }}</div>
           </td>
 
           <td
             class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
             {{ result.gradePoint }}
           </td>
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300" />
         </tr>
 
         <tr>
@@ -136,14 +145,19 @@ defineProps<{
             class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm font-bold text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
             {{ semester.gradePointTotal }}
           </td>
+
+          <td
+            class="hidden border-t border-gray-200 px-3 py-2 text-center text-sm font-bold text-gray-700 lg:table-cell dark:border-gray-700 dark:text-gray-300">
+            {{ semester.gradePointAverage }}
+          </td>
         </tr>
       </tbody>
     </table>
 
-    <div class="flex justify-around pb-2 text-right">
-      <span class="lg:hidden">Total Credit Unit: {{ semester.creditUnitTotal }}</span>
+    <div class="flex justify-around pb-2 text-right lg:hidden">
+      <span>Total Credit Unit: {{ semester.creditUnitTotal }}</span>
 
-      <span class="lg:hidden">Total Grade Point: {{ semester.gradePointTotal }}</span>
+      <span>Total Grade Point: {{ semester.gradePointTotal }}</span>
 
       <span class="font-bold">GPA: {{ semester.gradePointAverage }}</span>
     </div>
