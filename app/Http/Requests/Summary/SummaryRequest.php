e151948d9a4307ae51f2336d54aacfe59a2e4429
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-29 21:56:11 +0100

    Result Summary DTOs
    - Add Student Result Summary Data
    - Add Department Result Summary Data
    - Wrote Tests for both DTOs
    - Add test helper method for creating multiple students

---
+++ app/Http/Requests/Summary/SummaryRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Summary;
+
+use App\Models\Department;
+use App\Models\Level;
+use App\Models\Session;
+use Illuminate\Foundation\Http\FormRequest;
+
+final class SummaryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<string>>
+     */
+    public function rules(): array
+    {
+        return [
+            'department' => ['required', 'array'],
+            'department.id' => ['required', 'integer', 'exists:departments,id'],
+            'level' => ['required', 'array'],
+            'level.id' => ['required', 'integer', 'exists:levels,id'],
+            'session' => ['required', 'array'],
+            'session.id' => ['required', 'integer', 'exists:academic_sessions,id'],
+        ];
+    }
+
+    protected function passedValidation(): void
+    {
+        $this->replace([
+            'department' => Department::query()->findOrFail($this->input('department.id')),
+            'level' => Level::query()->findOrFail($this->input('level.id')),
+            'session' => Session::query()->findOrFail($this->input('session.id')),
+        ]);
+    }
+}
