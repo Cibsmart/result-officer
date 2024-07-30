5d7a013989cf3d586045391506ae98aa7c38ecb1
Barnabas Ifebude <doncibsmart@gmail.com>
2024-07-30 17:14:17 +0100

    Move Print and Download Result Controllers
    - Move print result controller into view student result controller
    - Move download transcript controller into view student result controller
    - Remove the download transcript and print result controllers

--- app/Http/Controllers/Result/ViewStudentResultController.php
+++ app/Http/Controllers/Result/ViewStudentResultController.php
@@ -5,13 +5,20 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Result;
 
 use App\Data\Results\StudentResultData;
+use App\Data\Results\TranscriptData;
 use App\Data\Students\StudentData;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Results\ResultRequest;
 use App\Models\Student;
 use App\ViewModels\Results\ResultViewPage;
+use Illuminate\Support\Facades\Config;
+use Illuminate\View\View;
 use Inertia\Inertia;
 use Inertia\Response;
+use Spatie\Browsershot\Browsershot;
+use Spatie\LaravelPdf\Enums\Format;
+use Spatie\LaravelPdf\Facades\Pdf;
+use Spatie\LaravelPdf\PdfBuilder;
 
 final class ViewStudentResultController extends Controller
 {
@@ -20,11 +27,39 @@ final class ViewStudentResultController extends Controller
         return Inertia::render('results/form/page');
     }
 
+    public function print(Student $student): View
+    {
+        return view('pdfs.results.view', [
+            'results' => StudentResultData::from($student),
+            'student' => StudentData::from($student),
+        ]);
+    }
+
+    public function transcript(Student $student): Pdf|PdfBuilder|View
+    {
+        $studentData = StudentData::from($student);
+
+        return Pdf::view('pdfs.results.transcript', [
+            'results' => StudentResultData::from($student),
+            'student' => $studentData,
+            'transcript' => TranscriptData::from($student->applyEGrade()),
+        ])
+            ->withBrowsershot(static function (Browsershot $browsershot): void {
+                $browsershot->setChromePath(Config::string('rp-pdf.chromium.path'));
+                $browsershot->scale(0.90);
+            })
+            ->format(Format::A4)
+            ->margins(5, 5, 5, 5)
+            ->name("$studentData->matriculationNumber-results.pdf");
+    }
+
     public function view(ResultRequest $request): Response
     {
         $matriculationNumber = $request->input('matriculation_number');
 
-        $studentModel = Student::query()->where('matriculation_number', $matriculationNumber)->firstOrFail();
+        $studentModel = Student::query()
+            ->where('matriculation_number', $matriculationNumber)
+            ->firstOrFail();
 
         return Inertia::render('results/view/page', new ResultViewPage(
             student: StudentData::from($studentModel),
