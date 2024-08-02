cb387adcfad0b7b9fdf9ad37f3be2bdc8d385b79
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-02 15:48:58 +0100

    Setup HTTP Macro
    - Add HTTP Portal Macros in the App Service Provider
    - Add Result Pro HTTP Config
    - Remove Static Closure PHPCS Rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -23,5 +25,11 @@ final class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict(App::isLocal());
+
+        Http::macro(
+            'portal',
+            fn () => Http::acceptJson()
+                ->baseUrl(Config::string('rp-http.base_url')),
+        );
     }
 }
