615d434127acb1ffcd44006855487b956662d0e1
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-08 18:47:21 +0100

    Implement Student Repository
    - The student repository is that data access layer that get and store student details
    - Add Tests to cover the Student Repository class

---
+++ app/Repositories/StudentRepository.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Classes\PendingStudent;
+use App\Data\Ingest\PortalStudentData;
+use App\Models\Student;
+use App\Services\Api\StudentService;
+use Exception;
+use Illuminate\Support\Collection;
+use ValueError;
+
+final class StudentRepository
+{
+    public function __construct(public StudentService $service)
+    {
+    }
+
+    public function getStudentByRegistrationNumber(string $registrationNumber): PortalStudentData
+    {
+        return $this->service->getStudentByRegistration($registrationNumber);
+    }
+
+    /** @return \Illuminate\Support\Collection<int, \App\Data\Ingest\PortalStudentData> */
+    public function getStudentsByDepartmentAndSession(string $departmentId, string $session): Collection
+    {
+        return $this->service->getStudentsByDepartmentAndSession($departmentId, $session);
+    }
+
+    /** @return \Illuminate\Support\Collection<int, \App\Data\Ingest\PortalStudentData> */
+    public function getStudentsBySession(string $session): Collection
+    {
+        return $this->service->getStudentsBySession($session);
+    }
+
+    /**
+     * @param \Illuminate\Support\Collection<int, \App\Data\Ingest\PortalStudentData> $students
+     * @return array<string, string|true>
+     */
+    public function saveStudents(Collection $students): array
+    {
+        $results = [];
+
+        foreach ($students as $student) {
+            try {
+                $this->saveStudent($student);
+                $results[$student->registrationNumber] = true;
+            } catch (ValueError $e) {
+                $results[$student->registrationNumber] = $e->getMessage();
+
+                continue;
+            } catch (Exception $e) {
+                $results[$student->registrationNumber] = $e->getMessage();
+
+                continue;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * @throws \Exception
+     * @throws \ValueError
+     */
+    public function saveStudent(PortalStudentData $studentData): Student
+    {
+        $pendingStudent = '';
+
+        try {
+            $pendingStudent = PendingStudent::new($studentData);
+            $pendingStudent->save();
+        } catch (Exception $exception) {
+            throw $exception;
+        }
+
+        return $pendingStudent->student;
+    }
+}
