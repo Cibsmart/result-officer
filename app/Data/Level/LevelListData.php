18a18358226b0ef2da8482050658f270e1d60115
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-08 18:27:54 +0100

    Reverse Typescript Annotation
    - Remove typescript annotation from all DTOs

--- app/Data/Level/LevelListData.php
+++ app/Data/Level/LevelListData.php
@@ -7,9 +7,7 @@ namespace App\Data\Level;
 use App\Models\Level;
 use Illuminate\Support\Collection;
 use Spatie\LaravelData\Data;
-use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
-#[TypeScript]
 final class LevelListData extends Data
 {
     public function __construct(
