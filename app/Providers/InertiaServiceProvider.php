9d6ebedea9694b57f6a7bce3f54e79c17bd52b89
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-09 17:45:04 +0100

    Install and Setup Vue Toastification
    - Install Vue Toastification
    - Add Toast as Vue Plugin
    - Create Notification Plugin
    - Add Notification as Vue Plugin
    - Add InertiaServiceProvider to register RedirectResponse Macros for flashing notifications
    - Create Notification DTO
    - Create NotificationType Enum

---
+++ app/Providers/InertiaServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Enums\NotificationType;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\ServiceProvider;
+
+final class InertiaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        RedirectResponse::macro(name: 'flash',
+            macro: function (NotificationType $type, string $body) {
+                session()->flash('notification', ['type' => $type, 'body' => $body]);
+
+                return $this;
+            },
+        );
+
+        RedirectResponse::macro(name: 'success',
+            macro: fn (string $body): RedirectResponse => $this->flash(NotificationType::SUCCESS, $body));
+
+        RedirectResponse::macro(name: 'error',
+            macro: fn (string $body): RedirectResponse => $this->flash(NotificationType::ERROR, $body));
+
+        RedirectResponse::macro(name: 'warning',
+            macro: fn (string $body): RedirectResponse => $this->flash(NotificationType::WARNING, $body));
+
+        RedirectResponse::macro(name: 'info',
+            macro: fn (string $body): RedirectResponse => $this->flash(NotificationType::INFO, $body));
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+    }
+}
