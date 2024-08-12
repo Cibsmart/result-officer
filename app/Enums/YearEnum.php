0eb323e61a73b47300ba8cd040e15172cf7696c7
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-12 21:23:27 +0100

    Created Year Enum

---
+++ app/Enums/YearEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum YearEnum: int
+{
+    case FIRST = 1;
+    case SECOND = 2;
+    case THIRD = 3;
+    case FOURTH = 4;
+    case FIFTH = 5;
+    case SIXTH = 6;
+    case SEVENTH = 7;
+    case EIGHT = 8;
+}
