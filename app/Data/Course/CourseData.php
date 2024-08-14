bbf78a424823df3957dc631b2a49d9d16f6afabb
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-14 22:42:33 +0100

    Removed code property from CourseData object
    - Combined code and title to form name attribute in Course Model.

--- app/Data/Course/CourseData.php
+++ app/Data/Course/CourseData.php
@@ -11,13 +11,12 @@ final class CourseData extends Data
 {
     public function __construct(
         public readonly int $id,
-        public readonly string $code,
         public readonly string $name,
     ) {
     }
 
     public static function fromModel(Course $course): self
     {
-        return new self(id: $course->id, code: $course->code, name: $course->title);
+        return new self(id: $course->id, name: $course->name);
     }
 }
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,9 +4,22 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 final class Course extends Model
 {
     protected $fillable = ['code', 'title', 'online_id', 'active'];
+
+    /** @return \Illuminate\Database\Eloquent\Casts\Attribute<string, string> */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            /** @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter */
+            get: fn (
+                ?string $value,
+                array $attributes,
+            ): string => "({$attributes['code']}) {$attributes['title']}",
+        );
+    }
 }
