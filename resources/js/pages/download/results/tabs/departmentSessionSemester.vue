8fcb6c504acd2e814371cef4fdc2efcd08c43f95
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-16 15:18:03 +0100

    Implemented the View (Vue) Components for the Download Result Page
    - Added a download result page
    - Create tabs for downloading results by registration number, department session level, department session semester and session course

---
+++ resources/js/pages/download/results/tabs/departmentSessionSemester.vue
@@ -0,0 +1,92 @@
+<script lang="ts" setup>
+import InputLabel from "@/components/inputLabel.vue";
+import PrimaryButton from "@/components/primaryButton.vue";
+import InputError from "@/components/inputError.vue";
+import { useForm } from "@inertiajs/vue3";
+import SelectInput from "@/components/inputs/selectInput.vue";
+import useDepartments from "@/composables/useDepartments";
+import useSessions from "@/composables/useSessions";
+import useSemesters from "@/composables/useSemesters";
+
+const departments = useDepartments.getDepartments();
+const sessions = useSessions.getSessions();
+const semesters = useSemesters.getSemesters();
+
+const form = useForm({
+  department: "",
+  session: "",
+  semester: "",
+});
+
+const submit = () => {
+  form.post(route("download.registrations.department-session-semester.store"));
+};
+</script>
+
+<template>
+  <section>
+    <header>
+      <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Download Result Information</h2>
+
+      <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+        Select Department, Session and Semester to download result records
+      </p>
+    </header>
+
+    <form
+      class="mt-6 space-y-6"
+      @submit.prevent="submit">
+      <div>
+        <InputLabel
+          for="department"
+          value="Department" />
+
+        <SelectInput
+          id="department"
+          v-model="form.department"
+          :items="departments"
+          class="mt-1 block w-full" />
+
+        <InputError
+          :message="form.errors.department"
+          class="mt-2" />
+      </div>
+
+      <div>
+        <InputLabel
+          for="session"
+          value="Session" />
+
+        <SelectInput
+          id="session"
+          v-model="form.session"
+          :items="sessions"
+          class="mt-1 block w-full" />
+
+        <InputError
+          :message="form.errors.session"
+          class="mt-2" />
+      </div>
+
+      <div>
+        <InputLabel
+          for="semester"
+          value="Session" />
+
+        <SelectInput
+          id="semester"
+          v-model="form.semester"
+          :items="semesters"
+          class="mt-1 block w-full" />
+
+        <InputError
+          :message="form.errors.semester"
+          class="mt-2" />
+      </div>
+
+      <div>
+        <PrimaryButton :disabled="form.processing">Download</PrimaryButton>
+      </div>
+    </form>
+  </section>
+</template>
---
+++ resources/js/pages/download/results/tabs/sessionCourse.vue
@@ -0,0 +1,71 @@
+<script lang="ts" setup>
+import InputLabel from "@/components/inputLabel.vue";
+import PrimaryButton from "@/components/primaryButton.vue";
+import InputError from "@/components/inputError.vue";
+import { useForm } from "@inertiajs/vue3";
+import SelectInput from "@/components/inputs/selectInput.vue";
+import useSessions from "@/composables/useSessions";
+import useCourses from "@/composables/useCourses";
+
+const sessions = useSessions.getSessions();
+const courses = useCourses.getCourses();
+
+const form = useForm({
+  session: "",
+  course: "",
+});
+
+const submit = () => {
+  form.post(route("download.registrations.session-course.store"));
+};
+</script>
+
+<template>
+  <section>
+    <header>
+      <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Download Result Information</h2>
+
+      <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">Select session and course to download result records</p>
+    </header>
+
+    <form
+      class="mt-6 space-y-6"
+      @submit.prevent="submit">
+      <div>
+        <InputLabel
+          for="session"
+          value="Session" />
+
+        <SelectInput
+          id="session"
+          v-model="form.session"
+          :items="sessions"
+          class="mt-1 block w-full" />
+
+        <InputError
+          :message="form.errors.session"
+          class="mt-2" />
+      </div>
+
+      <div>
+        <InputLabel
+          for="course"
+          value="Session" />
+
+        <SelectInput
+          id="course"
+          v-model="form.course"
+          :items="courses"
+          class="mt-1 block w-full" />
+
+        <InputError
+          :message="form.errors.course"
+          class="mt-2" />
+      </div>
+
+      <div>
+        <PrimaryButton :disabled="form.processing">Download</PrimaryButton>
+      </div>
+    </form>
+  </section>
+</template>
