0d7deab83cfe399062b512d4cb81daf6b2e11f74
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-17 08:39:25 +0100

    Refactored PortalResultData Object
    - Replaced the ValueObject types with strings
    - Updated Tests accordingly

--- tests/Feature/Data/Download/PortalResultDataTest.php
+++ tests/Feature/Data/Download/PortalResultDataTest.php
@@ -4,12 +4,7 @@ declare(strict_types=1);
 
 use App\Data\Download\PortalDateData;
 use App\Data\Download\PortalResultData;
-use App\Enums\Grade;
 use App\Http\Clients\Fakes\FakeResultClient;
-use App\Values\ExamScore;
-use App\Values\InCourseScore;
-use App\Values\RegistrationNumber;
-use App\Values\TotalScore;
 
 it('constructs a correct portal result data from result array', function (): void {
     $result = FakeResultClient::RESULTS[0];
@@ -19,40 +14,10 @@ it('constructs a correct portal result data from result array', function (): voi
     expect($data)->toBeInstanceOf(PortalResultData::class)
         ->and($data->onlineId)->toBe($result['id'])
         ->and($data->courseRegistrationId)->toBe($result['course_registration_id'])
-        ->and($data->registrationNumber)->toBeInstanceOf(RegistrationNumber::class)
-        ->and($data->registrationNumber->value)->toBe($result['registration_number'])
-        ->and($data->inCourseScore)->toBeInstanceOf(InCourseScore::class)
-        ->and($data->inCourseScore->value)->toBe((int) $result['in_course'])
-        ->and($data->examScore)->toBeInstanceOf(ExamScore::class)
-        ->and($data->examScore->value)->toBe((int) $result['exam_score'])
-        ->and($data->totalScore)->toBeInstanceOf(TotalScore::class)
-        ->and($data->totalScore->value)->toBe((int) $result['total_score'])
-        ->and($data->grade)->toBeInstanceOf(Grade::class)
-        ->and($data->grade->name)->toBe($result['grade'])
+        ->and($data->registrationNumber)->toBe($result['registration_number'])
+        ->and($data->inCourseScore)->toBe($result['in_course'])
+        ->and($data->examScore)->toBe($result['exam_score'])
+        ->and($data->totalScore)->toBe($result['total_score'])
+        ->and($data->grade)->toBe($result['grade'])
         ->and($data->uploadDate)->toBeInstanceOf(PortalDateData::class);
 });
-
-it('throws invalid argument exception for invalid scores', function (string $key, string $value): void {
-    $result = FakeResultClient::RESULTS[0];
-
-    $result[$key] = $value;
-
-    PortalResultData::from($result);
-})
-    ->with([
-        ['in_course', '51'],
-        ['exam_score', '101'],
-    ])
-    ->throws(InvalidArgumentException::class);
-
-it('throws invalid argument exception for invalid registration', function (string $key, string $value): void {
-    $result = FakeResultClient::RESULTS[0];
-
-    $result[$key] = $value;
-
-    PortalResultData::from($result);
-})
-    ->with([
-        ['registration_number', 'EBSU/209/51486'],
-    ])
-    ->throws(InvalidArgumentException::class);
