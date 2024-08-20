978857b10257c050d13abbb5cd8948a1b647d201
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-20 16:53:39 +0100

    Refactored type of courses property to Closure in DownloadCourseRegistrationPage ViewModel
    - Updated references to the ViewModel to a closure to enable lazy loading of courses

--- app/Http/Controllers/Download/CourseRegistrations/DownloadCourseRegistrationPageController.php
+++ app/Http/Controllers/Download/CourseRegistrations/DownloadCourseRegistrationPageController.php
@@ -10,18 +10,21 @@ use App\Data\Level\LevelListData;
 use App\Data\Semester\SemesterListData;
 use App\Data\Session\SessionListData;
 use App\ViewModels\Downloads\DownloadCourseRegistrationPage;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 final readonly class DownloadCourseRegistrationPageController
 {
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
+        $searchTerm = $request->string('search')->value();
+
         return Inertia::render('download/registrations/page', new DownloadCourseRegistrationPage(
             departments: DepartmentListData::new(),
             sessions: SessionListData::new(),
             semesters: SemesterListData::new(),
-            courses: CourseListData::new(),
+            courses: fn () => CourseListData::new($searchTerm),
             levels: LevelListData::new(),
         ));
     }
