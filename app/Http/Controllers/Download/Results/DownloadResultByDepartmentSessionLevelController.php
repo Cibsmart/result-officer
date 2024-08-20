c0ef9c3844ed283f9e57da467aaac9c531ab2293
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-20 17:04:05 +0100

    Implemented the Download Results By Department Session and Level|Semester Controllers

--- app/Http/Controllers/Download/Results/DownloadResultByDepartmentSessionLevelController.php
+++ app/Http/Controllers/Download/Results/DownloadResultByDepartmentSessionLevelController.php
@@ -4,13 +4,45 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Download\Results;
 
+use App\Actions\SaveResults;
+use App\Helpers\GetResponse;
+use App\Models\Department;
+use App\Services\Api\ResultService;
+use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
-final class DownloadResultByDepartmentSessionLevelController
+final readonly class DownloadResultByDepartmentSessionLevelController
 {
+    public function __construct(private ResultService $service, private SaveResults $saveResult)
+    {
+    }
+
+    /** @throws \Exception */
     public function __invoke(Request $request): RedirectResponse
     {
-        dd($request->all());
+        $departmentId = $request->string('department.id')->value();
+        $session = $request->string('session.name')->value();
+        $level = $request->string('level.name')->value();
+
+        $departmentOnlineId = Department::find($departmentId)->firstOrFail()->online_id;
+
+        assert($departmentOnlineId !== null);
+
+        try {
+            $results = $this->service->getResultsByDepartmentSessionAndLevel(
+                departmentId: $departmentOnlineId,
+                session: $session,
+                level: $level,
+            );
+
+            $responses = $this->saveResult->execute($results);
+
+            $response = GetResponse::fromArray($responses);
+
+            return back()->{$response->type->value}($response->message);
+        } catch (Exception $e) {
+            return back()->error($e->getMessage());
+        }
     }
 }
