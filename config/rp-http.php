d44ef5547b97f45c9116fa4d6bdc39d13c3faeb8
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-22 12:31:32 +0100

    Added endpoints config in the rp-http config file for courses, departments, registration, results and students

--- config/rp-http.php
+++ config/rp-http.php
@@ -3,5 +3,12 @@
 declare(strict_types=1);
 
 return [
-    'base_url' => env('RP_HTTP_BASE_URL', 'https://portal.ebsu.net/api'),
+    'base_url' => env('RP_HTTP_BASE_URL', 'https://portal.ebsu.edu.ng/api'),
+    'endpoints' => [
+        'courses' => env('RP_HTTP_ENDPOINT_COURSE', 'course.ashx'),
+        'departments' => env('RP_HTTP_ENDPOINT_DEPARTMENT', 'departments.ashx'),
+        'registrations' => env('RP_HTTP_ENDPOINT_REGISTRATION', 'registration.ashx'),
+        'results' => env('RP_HTTP_ENDPOINT_RESULT', 'results.ashx'),
+        'students' => env('RP_HTTP_ENDPOINT_STUDENT', 'students.ashx'),
+    ],
 ];
