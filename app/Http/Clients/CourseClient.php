230ffb735f63e9297972612e490e89953bcf0a95
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-22 13:33:08 +0100

    Refactored Course Client
    - Utilized endpoint from the config
    - Replaced Url parameters with query parameters
    - Wrote test for the department client

--- app/Http/Clients/CourseClient.php
+++ app/Http/Clients/CourseClient.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Clients;
 
+use Config;
+
 final readonly class CourseClient extends ApiClient
 {
+    private string $endpoint;
+
+    public function __construct()
+    {
+        $this->endpoint = Config::string('rp-http.endpoints.courses');
+    }
+
     /**
      * @return array<int, array{id: string, course_code: string, course_title: string}>
      * @throws \Exception
@@ -16,7 +25,7 @@ final readonly class CourseClient extends ApiClient
          * phpcs:ignore SlevomatCodingStandard.Files.LineLength
          * @var array<int, array{id: string, course_code: string, course_title: string}> $courses
          */
-        $courses = $this->get('courses');
+        $courses = $this->get($this->endpoint);
 
         return $courses;
     }
@@ -28,7 +37,7 @@ final readonly class CourseClient extends ApiClient
     public function fetchCourseById(string $id): array
     {
         /** @var array<int, array{id: string, course_code: string, course_title: string}> $course */
-        $course = $this->get("courses/$id");
+        $course = $this->get(endpoint: $this->endpoint, parameters: ['course_id' => $id]);
 
         return $course;
     }
