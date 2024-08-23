b227a3f156df01a216123d0efca4e409b6f4e396
Barnabas Ifebude <cibsmarte@gmail.com>
2024-08-23 18:38:05 +0100

    Added Base Form Section Component

---
+++ resources/js/components/baseFormSection.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+defineProps<{
+  header: string;
+  description: string;
+}>();
+</script>
+
+<template>
+  <section class="max-w-xl">
+    <header>
+      <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">{{ header }}</h2>
+
+      <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+        {{ description }}
+      </p>
+    </header>
+
+    <slot />
+  </section>
+</template>
