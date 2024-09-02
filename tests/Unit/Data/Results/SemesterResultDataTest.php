ae46760938fe2f114fe3266bdac60f544fa7e2ca
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-02 21:18:53 +0100

    Added Formatted CU and GP totals to Semester Result Data
    - Updated Test

--- tests/Unit/Data/Results/SemesterResultDataTest.php
+++ tests/Unit/Data/Results/SemesterResultDataTest.php
@@ -22,6 +22,8 @@ test('semester result data is correct', function (): void {
     $totalCU = $courses->sum('credit_unit');
     $totalGP = $courses->sum('result.grade_point');
     $gpa = round($totalGP / $totalCU, 3);
+    $totalCUFormatted = str($totalCU)->padLeft(2, '0')->value();
+    $totalGPFormatted = str($totalGP)->padLeft(2, '0')->value();
     $gpaFormatted = number_format($gpa, 3);
 
     expect($semesterData)->toBeInstanceOf(SemesterResultData::class)
@@ -30,6 +32,8 @@ test('semester result data is correct', function (): void {
         ->and($semesterData->creditUnitTotal)->toBe($totalCU)->toBeInt()
         ->and($semesterData->gradePointTotal)->toBe($totalGP)->toBeInt()
         ->and($semesterData->gradePointAverage)->toBe($gpa)->toBeFloat()
+        ->and($semesterData->formattedCreditUnitTotal)->toBe($totalCUFormatted)->toBeString()
+        ->and($semesterData->formattedGradePointTotal)->toBe($totalGPFormatted)->toBeString()
         ->and($semesterData->formattedGPA)->toBe($gpaFormatted)->toBeString();
 
 });
@@ -41,7 +45,9 @@ test('semester enrollment without result data returns zeroes', function (): void
 
     expect($semesterData)->toBeInstanceOf(SemesterResultData::class)
         ->and($semesterData->gradePointTotal)->toBe(0)->toBeInt()
+        ->and($semesterData->formattedGradePointTotal)->toBe('00')->toBeString()
         ->and($semesterData->creditUnitTotal)->toBe(0)->toBeInt()
+        ->and($semesterData->formattedCreditUnitTotal)->toBe('00')->toBeString()
         ->and($semesterData->gradePointAverage)->toBe(0.000)->toBeFloat()
         ->and($semesterData->formattedGPA)->toBeString('0.000');
 });
