9c46fc41b3f47e93622b0827ffa04f9e9a1f7eac
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-05 20:20:57 +0100

    Added DTOs
    - Added Faculty Data Object
    - Added Composite Sheet Data Object
    - Moved all methods needed to create Composite Sheet Data from Controller to the DTO
    - Refactored Composite Sheet View to use the Composite Sheet Data Object
    - Added Tests for the DTOs

---
+++ app/Data/Composite/CompositeSheetData.php
@@ -0,0 +1,221 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Composite;
+
+use App\Data\Faculty\FacultyData;
+use App\Data\Level\LevelData;
+use App\Data\Program\ProgramData;
+use App\Data\Results\ResultData;
+use App\Data\Results\SemesterResultData;
+use App\Data\Semester\SemesterData;
+use App\Data\Session\SessionData;
+use App\Models\Level;
+use App\Models\Program;
+use App\Models\ProgramCourse;
+use App\Models\ProgramCurriculum;
+use App\Models\Semester;
+use App\Models\Session;
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\LazyCollection;
+use Illuminate\Support\Str;
+use Spatie\LaravelData\Data;
+
+final class CompositeSheetData extends Data
+{
+    public function __construct(
+        public readonly ProgramData $program,
+        public readonly FacultyData $faculty,
+        public readonly SessionData $session,
+        public readonly SemesterData $semester,
+        public readonly LevelData $level,
+        /** @var \Illuminate\Support\Collection<int, \App\Data\Composite\CompositeCourseListData> */
+        public readonly Collection $courses,
+        /** @var \Illuminate\Support\Collection<int, \App\Data\Composite\CompositeRowData> */
+        public readonly Collection $students,
+    ) {
+    }
+
+    public static function fromModel(
+        Program $program,
+        Session $session,
+        Level $level,
+        Semester $semester,
+    ): self {
+        $courses = self::getCourses($program, $session, $level, $semester);
+
+        $programStudents = self::getProgramStudents($program, $session, $level, $semester);
+
+        $students = self::prepareCompositeRows($programStudents, $session, $semester, $courses);
+
+        return new self(
+            program: ProgramData::fromModel($program),
+            faculty: FacultyData::fromProgram($program),
+            session: SessionData::fromModel($session),
+            semester: SemesterData::fromModel($semester),
+            level: LevelData::fromModel($level),
+            courses: CompositeCourseListData::collect($courses),
+            students: $students,
+        );
+    }
+
+    /**
+     * @param \Illuminate\Support\LazyCollection<int, \App\Models\Student> $students
+     * @param \Illuminate\Support\Collection<int, array{code: string, unit: int}> $courses
+     * @return \Illuminate\Support\Collection<int, \App\Data\Composite\CompositeRowData>
+     */
+    private static function prepareCompositeRows(
+        LazyCollection $students,
+        Session $session,
+        Semester $semester,
+        Collection $courses,
+    ): Collection {
+        $crossTab = [];
+
+        foreach ($students as $student) {
+            $courseList = $courses->pluck('unit', 'code')
+                ->map(fn () => ['code' => '', 'grade' => '-', 'score' => '-'])->all();
+
+            $semesterResultData = self::getSemesterResultDataForStudent($student, $session, $semester);
+
+            [$levelCourses, $otherCourses, $failed] = self::prepareCompositeCourses($semesterResultData, $courseList);
+
+            $crossTab[] = [
+                'creditUnitTotal' => $semesterResultData->formattedCreditUnitTotal,
+                'gradePointAverage' => $semesterResultData->formattedGPA,
+                'gradePointTotal' => $semesterResultData->formattedGradePointTotal,
+                'id' => $student->id,
+                'levelCourses' => $levelCourses,
+                'name' => $student->name,
+                'otherCourses' => $otherCourses,
+                'registrationNumber' => $student->registration_number,
+                'remark' => $failed === '' ? 'PASS' : "FAIL: $failed",
+            ];
+        }
+
+        return CompositeRowData::collect(collect($crossTab));
+    }
+
+    /** @return \Illuminate\Support\Collection<int, array{code: string, unit: int}> */
+    private static function getCourses(
+        Program $program,
+        Session $session,
+        Level $level,
+        Semester $semester,
+    ): Collection {
+        $curriculum = ProgramCurriculum::query()
+            ->where('program_id', $program->id)
+            ->where('session_id', $session->id)
+            ->where('level_id', $level->id)
+            ->where('semester_id', $semester->id)
+            ->first();
+
+        return $curriculum
+            ? $curriculum
+                ->courses()
+                ->select('program_courses.*')
+                ->join('courses', 'courses.id', '=', 'program_courses.course_id')
+                ->orderBy('courses.code')
+                ->with('course')
+                ->get()
+                ->map(
+                    fn (ProgramCourse $course) => ['code' => $course->course->code, 'unit' => $course->credit_unit],
+                )
+            : collect([]);
+    }
+
+    /** @return \Illuminate\Support\LazyCollection<int, \App\Models\Student> */
+    private static function getProgramStudents(
+        Program $program,
+        Session $session,
+        Level $level,
+        Semester $semester,
+    ): LazyCollection {
+        return $program->students()
+            ->with([
+                'enrollments.semesters.courses.course',
+                'enrollments.semesters.courses.result',
+                'enrollments.semesters.semester',
+            ])
+            ->whereHas('enrollments.semesters',
+                function (Builder $query) use ($session, $level, $semester): void {
+                    $query->where('session_id', $session->id)
+                        ->where('level_id', $level->id)
+                        ->where('semester_id', $semester->id);
+                })
+            ->orderBy('registration_number')
+            ->lazy();
+    }
+
+    private static function getSemesterResultDataForStudent(
+        Student $student,
+        Session $session,
+        Semester $semester,
+    ): SemesterResultData {
+        $sessionEnrollment = $student->enrollments
+            ->where('session_id', '===', $session->id)
+            ->firstOrFail();
+
+        $semesterEnrollment = $sessionEnrollment
+            ->semesters
+            ->where('semester_id', '===', $semester->id)
+            ->firstOrFail();
+
+        return SemesterResultData::fromModel($semesterEnrollment);
+    }
+
+    /**
+     * @param array<string, array<string, string>> $courseList
+     * @return array{\Illuminate\Support\Collection<int, \App\Data\Composite\CompositeCourseData>, string, string}
+     */
+    private static function prepareCompositeCourses(SemesterResultData $semesterResultData, array $courseList): array
+    {
+        $levelCourses = [...$courseList];
+
+        $otherCourses = '';
+
+        $failed = '';
+
+        foreach ($semesterResultData->results as $result) {
+
+            [$grade, $score] = self::getGradeScore($result);
+
+            if ($grade === 'F') {
+                $failed .= "{$result->courseCode}, ";
+            }
+
+            if (array_key_exists($result->courseCode, $levelCourses)) {
+                $levelCourses[$result->courseCode] = [
+                    'code' => $result->courseCode, 'grade' => $grade, 'score' => $score,
+                ];
+
+                continue;
+            }
+
+            $otherCourses .= "{$result->courseCode}({$result->grade}), ";
+        }
+
+        return [
+            CompositeCourseData::collect(collect($levelCourses)),
+            Str::trim($otherCourses, ', '),
+            Str::trim($failed, ', '),
+        ];
+    }
+
+    /** @return array<int, string> */
+    private static function getGradeScore(ResultData $result): array
+    {
+        $grade = 'NR';
+        $score = 'NR';
+
+        if ($result->remark !== 'NR') {
+            $grade = $result->grade;
+            $score = (string) $result->totalScore;
+        }
+
+        return [$grade, $score];
+    }
+}
--- resources/js/pages/reports/composite/view/page.vue
+++ resources/js/pages/reports/composite/view/page.vue
@@ -6,10 +6,10 @@ import BaseSection from "@/layouts/main/partials/baseSection.vue";
 import { BreadcrumbItem } from "@/types";
 import Breadcrumb from "@/components/breadcrumb.vue";
 import { computed } from "vue";
+import BaseLink from "@/components/links/baseLink.vue";
 
 const props = defineProps<{
-  students: Array<App.Data.Composite.CompositeRowData>;
-  courses: Array<App.Data.Composite.CompositeCourseListData>;
+  data: App.Data.Composite.CompositeSheetData;
 }>();
 
 const pages: BreadcrumbItem[] = [
@@ -17,7 +17,7 @@ const pages: BreadcrumbItem[] = [
   { name: "Composite Sheet View", href: route("composite.view"), current: route().current("composite.view") },
 ];
 
-const hasOthers = computed(() => props.students.some((student) => student.otherCourses.length > 0));
+const hasOthers = computed(() => props.data.students.some((student) => student.otherCourses.length > 0));
 </script>
 
 <template>
@@ -35,21 +35,48 @@ const hasOthers = computed(() => props.students.some((student) => student.otherC
             <h1 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">Composite Sheet</h1>
 
             <p class="mt-2 text-sm text-gray-700 dark:text-gray-300">
-              This page shows Composite Sheet for the selected department
+              This page shows Composite Sheet for {{ data.program.name }}
             </p>
           </div>
+
+          <BaseLink
+            :href="
+              route('composite.print', {
+                program: data.program.id,
+                session: data.session.id,
+                level: data.level.id,
+                semester: data.semester.id,
+              })
+            ">
+            Download
+          </BaseLink>
         </div>
 
         <div
           class="mt-5 divide-y divide-solid divide-gray-300 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg dark:divide-gray-600 dark:ring-gray-600">
-          <div class="p-2">NAME: <span class="font-bold text-black dark:text-white">IFEBUDE BARNABAS</span></div>
+          <div class="grid grid-flow-col">
+            <div class="p-2">
+              FACULTY: <span class="font-bold text-black dark:text-white">{{ data.faculty.name }}</span>
+            </div>
 
-          <div class="p-2">
-            REGISTRATION NUMBER:
-            <span class="font-bold text-black dark:text-white">EBSU/2009/51486</span>
+            <div class="p-2">
+              DEPARTMENT: <span class="font-bold text-black dark:text-white">{{ data.program.name }}</span>
+            </div>
           </div>
 
-          <div class="p-2">DEPARTMENT: <span class="font-bold text-black dark:text-white">COMPUTER SCIENCE</span></div>
+          <div class="grid grid-flow-col">
+            <div class="p-2">
+              SESSION: <span class="font-bold text-black dark:text-white"> {{ data.session.name }} </span>
+            </div>
+
+            <div class="p-2">
+              SEMESTER: <span class="font-bold text-black dark:text-white"> {{ data.semester.name }} </span>
+            </div>
+
+            <div class="p-2">
+              LEVEL: <span class="font-bold text-black dark:text-white"> {{ data.level.name }} </span>
+            </div>
+          </div>
         </div>
 
         <div class="mt-8 flow-root">
@@ -74,7 +101,7 @@ const hasOthers = computed(() => props.students.some((student) => student.otherC
                     </th>
 
                     <th
-                      v-for="(course, index) in courses"
+                      v-for="(course, index) in data.courses"
                       :key="index"
                       class="sticky top-0 z-10 bg-opacity-75 p-1 text-center text-xs font-semibold text-gray-900 backdrop-blur dark:text-white"
                       colspan="2"
@@ -86,12 +113,17 @@ const hasOthers = computed(() => props.students.some((student) => student.otherC
 
                     <th
                       class="sticky top-0 z-10 bg-opacity-75 p-1 text-center text-xs font-semibold text-gray-900 backdrop-blur dark:text-white"
-                      colspan="4"
+                      colspan="3"
                       scope="col">
                       CURRENT TOTALS
                     </th>
 
-                    <th />
+                    <th
+                      class="sticky top-0 z-10 bg-opacity-75 p-1 text-center text-xs font-semibold text-gray-900 backdrop-blur dark:text-white"
+                      rowspan="2"
+                      scope="col">
+                      REMARKS
+                    </th>
                   </tr>
 
                   <tr class="divide-x divide-gray-200 dark:divide-gray-600">
@@ -108,7 +140,7 @@ const hasOthers = computed(() => props.students.some((student) => student.otherC
                     </th>
 
                     <th
-                      v-for="(course, index) in courses"
+                      v-for="(course, index) in data.courses"
                       :key="index"
                       class="sticky top-0 z-10 bg-opacity-75 p-1 text-center text-xs font-semibold text-gray-900 backdrop-blur dark:text-white"
                       colspan="2"
@@ -140,18 +172,12 @@ const hasOthers = computed(() => props.students.some((student) => student.otherC
                       scope="col">
                       GPA
                     </th>
-
-                    <th
-                      class="sticky top-0 z-10 bg-opacity-75 p-1 text-center text-xs font-semibold text-gray-900 backdrop-blur dark:text-white"
-                      scope="col">
-                      REMARK
-                    </th>
                   </tr>
                 </thead>
 
                 <tbody>
                   <tr
-                    v-for="(student, index) in students"
+                    v-for="(student, index) in data.students"
                     :key="index"
                     class="divide-x divide-gray-200 dark:divide-gray-600">
                     <td
@@ -204,7 +230,7 @@ const hasOthers = computed(() => props.students.some((student) => student.otherC
                     </td>
 
                     <td
-                      class="border-t border-gray-200 p-1 text-center text-xs text-gray-700 dark:border-gray-700 dark:text-gray-300">
+                      class="border-t border-gray-200 p-1 text-left text-xs text-gray-700 dark:border-gray-700 dark:text-gray-300">
                       {{ student.remark }}
                     </td>
                   </tr>
