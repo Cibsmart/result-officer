6be9a9ed1a75f8b9de9b8d50d4a403d37812acb8
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-06 20:12:54 +0100

    Made the Empty State Component Composable
    - Created an IconLink Component
    - Updated References to the Empty State component

--- resources/js/components/emptyState.vue
+++ resources/js/components/emptyState.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import { PlusIcon } from "@heroicons/vue/20/solid";
-
 defineProps<{
   title: string;
   description: string;
@@ -31,14 +29,7 @@ defineProps<{
       </p>
 
       <div class="mt-6">
-        <button
-          class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          type="button">
-          <PlusIcon
-            aria-hidden="true"
-            class="-ml-0.5 mr-1.5 h-5 w-5" />
-          Download Student Results
-        </button>
+        <slot />
       </div>
     </div>
   </div>
