e3fad1f200ba7d3ed81a51a8e30c0fd4ad233c17
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-13 12:00:06 +0100

    Updated Portal Result Data
    - Added ExamDate, LecturerName and LecturerDepartment

--- app/Data/Download/PortalResultData.php
+++ app/Data/Download/PortalResultData.php
@@ -18,14 +18,19 @@ final class PortalResultData extends Data
         public readonly string $examScore,
         public readonly string $totalScore,
         public readonly string $grade,
-        public readonly PortalDateData $uploadDate,
+        public readonly string $uploadDate,
         public readonly RecordSource $source,
+        public readonly string $examDate,
+        public readonly string $lecturerName,
+        public readonly string $lecturerDepartment,
     ) {
     }
 
     /** @param ResultDetail $data */
     public static function fromArray(array $data): self
     {
+        dd($data);
+
         return new self(
             onlineId: $data['id'],
             courseRegistrationId: $data['course_registration_id'],
@@ -34,8 +39,11 @@ final class PortalResultData extends Data
             examScore: $data['exam_score'],
             totalScore: $data['total_score'],
             grade: $data['grade'],
-            uploadDate: PortalDateData::from($data['upload_date']),
+            uploadDate: $data['upload_date'],
             source: RecordSource::PORTAL,
+            examDate: $data['exam_date'] ?? '',
+            lecturerName: $data['lecturer_name'] ?? '',
+            lecturerDepartment: $data['lecturer_department'] ?? '',
         );
     }
 }
