a22634b3343a7c93ff51ffb382eeaab2352982f3
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-14 05:33:00 +0100

    Added Models and Migrations
    - Raw Courses
    - Raw Departments
    - Raw Students
    - Raw Registrations

---
+++ database/migrations/2024_09_14_041459_create_raw_courses_table.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\ImportEvent;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('raw_courses', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(ImportEvent::class);
+            $table->string('online_id');
+            $table->string('code');
+            $table->string('title');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('raw_courses');
+    }
+};
---
+++ database/migrations/2024_09_14_041529_create_raw_registrations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\ImportEvent;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('raw_registrations', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(ImportEvent::class);
+            $table->string('online_id');
+            $table->string('registration_number');
+            $table->string('session');
+            $table->string('semester');
+            $table->string('level');
+            $table->string('course_id');
+            $table->string('course_title');
+            $table->string('credit_unit');
+            $table->string('registration_date');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('raw_registrations');
+    }
+};
