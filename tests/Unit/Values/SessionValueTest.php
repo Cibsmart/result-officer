82f03a15c9f3e3097a0e04a8c301b9804f768392
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-15 18:34:20 +0100

    Replaced '/' with '-' in Session Factory and Test

--- tests/Unit/Values/SessionValueTest.php
+++ tests/Unit/Values/SessionValueTest.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 use App\Values\SessionValue;
 
 it('creates valid session value', function (): void {
-    $stringSession = '2009/2010';
+    $stringSession = '2009-2010';
 
     $session = SessionValue::new($stringSession);
 
     expect($session)->toBeInstanceOf(SessionValue::class)
         ->and($session->value)->toBeString()
-        ->and($session->value)->toBe('2009/2010');
+        ->and($session->value)->toBe('2009-2010');
 });
 
 it('throws exception for invalid session value', function ($stringSession): void {
@@ -19,21 +19,21 @@ it('throws exception for invalid session value', function ($stringSession): void
 })
     ->with([
         [''],
-        ['200/2010'],
-        ['2009/201'],
-        ['20001/2010'],
+        ['200-2010'],
+        ['2009-201'],
+        ['20001-2010'],
         ['20092010'],
     ])
     ->throws(InvalidArgumentException::class);
 
 it('extract the first year from session', function (): void {
-    $session = SessionValue::new('2009/2010');
+    $session = SessionValue::new('2009-2010');
 
     expect($session->firstYear())->toBeInt()->toBe(2009);
 });
 
 it('extract the last year from session', function (): void {
-    $session = SessionValue::new('2009/2010');
+    $session = SessionValue::new('2009-2010');
 
     expect($session->lastYear())->toBeInt()->toBe(2010);
 });
