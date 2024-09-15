4071823b8f01d88e0feb49d30f128f161ff8c143
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-15 18:42:00 +0100

    Added ImportEvent Data and Test

---
+++ tests/Unit/Data/Import/ImportEventDataTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Data\Import\ImportEventData;
+use App\Enums\ImportEventStatus;
+use App\Enums\ImportEventType;
+use Tests\Factories\ImportEventFactory;
+
+test('import event data is correct', function (): void {
+    $event = ImportEventFactory::new()->createOne(['data' => ['courses' => 'all', 'session' => '2009-2010']]);
+
+    $data = ImportEventData::fromModel($event);
+
+    expect($data)->toBeInstanceOf(ImportEventData::class)
+        ->and($data->target)->toBeString()->toBe($event->user->name)
+        ->and($data->type)->toBeInstanceOf(ImportEventType::class)
+        ->and($data->status)->toBeInstanceOf(ImportEventStatus::class)
+        ->and($data->count)->toBeInt()->toBe(0)
+        ->and($data->content)->toBeString()->toBe('ALL COURSES, 2009-2010 SESSION, downloaded by')
+        ->and($data->date)->toBeString()->toBe($event->created_at->diffForHumans());
+});
