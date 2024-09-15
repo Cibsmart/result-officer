93c81f1b4f4678b6fe639a59bd819d6d92e0e44f
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-15 18:41:04 +0100

    Added DownloadCoursePage ViewModel

---
+++ app/ViewModels/Downloads/DownloadCoursesPage.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ViewModels\Downloads;
+
+use App\Data\Import\PendingImportEventData;
+use Closure;
+use Illuminate\Support\Collection;
+use Spatie\LaravelData\Data;
+use Spatie\TypeScriptTransformer\Attributes\TypeScriptType;
+
+final class DownloadCoursesPage extends Data
+{
+    public function __construct(
+        #[TypeScriptType(Collection::class)]
+        public readonly Closure $events,
+        #[TypeScriptType(PendingImportEventData::class)]
+        public readonly Closure $pending,
+    ) {
+    }
+}
