7e7f9637e68aa68beafc78f71111477b825cf5e8
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-15 18:39:23 +0100

    Updated BaseFormSection Component
    - Removed max-width constraint
    - Made the header and description props options
    - Add check for header and description before rendering them

--- resources/js/components/baseFormSection.vue
+++ resources/js/components/baseFormSection.vue
@@ -1,18 +1,22 @@
 <script lang="ts" setup>
 defineProps<{
-  header: string;
-  description: string;
+  header?: string;
+  description?: string;
 }>();
 </script>
 
 <template>
-  <section class="max-w-xl">
+  <section>
     <header>
-      <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">{{ header }}</h2>
+      <template v-if="header">
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">{{ header }}</h2>
+      </template>
 
-      <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-        {{ description }}
-      </p>
+      <template v-if="description">
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+          {{ description }}
+        </p>
+      </template>
     </header>
 
     <slot />
