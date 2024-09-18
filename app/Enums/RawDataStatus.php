f41820738c70e263d13a58ef59f8a67763050be7
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-18 11:54:37 +0100

    Added Duplicate and Failed Cases to RawDataStatus Enum

--- app/Enums/RawDataStatus.php
+++ app/Enums/RawDataStatus.php
@@ -7,5 +7,7 @@ namespace App\Enums;
 enum RawDataStatus: string
 {
     case PENDING = 'pending';
+    case DUPLICATE = 'duplicate';
+    case FAILED = 'failed';
     case PROCESSED = 'processed';
 }
