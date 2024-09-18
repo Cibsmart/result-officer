36809305405ea346429b92d97650a4f6bd1f02ec
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-18 17:00:25 +0100

    Added Courses Actions
    - SaveRawCourses to process collection of PortalCourseData
    - SaveRawCourse to check and insert a single raw course into the database
    - ProcessImportedRawCourses to process collection of RawCourses
    - SaveImportedRawCourse to save individual raw course into the courses table

---
+++ app/Actions/Courses/SaveImportedRawCourse.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Courses;
+
+use App\Enums\RawDataStatus;
+use App\Models\Course;
+use App\Models\RawCourse;
+
+final class SaveImportedRawCourse
+{
+    public function execute(RawCourse $rawCourse): void
+    {
+        $exists = Course::query()
+            ->where('code', $rawCourse->code)
+            ->where('title', $rawCourse->title)
+            ->exists();
+
+        if ($exists) {
+            $rawCourse->updateStatus(RawDataStatus::DUPLICATE);
+
+            return;
+        }
+
+        Course::createFromRawCourse($rawCourse);
+
+        $rawCourse->updateStatus(RawDataStatus::PROCESSED);
+    }
+}
