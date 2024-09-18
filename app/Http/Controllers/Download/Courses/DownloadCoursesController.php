e3c905219f880cd8b7140f70dacff081e4359f76
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-18 16:44:23 +0100

    Refactored Download Courses Controller to Queue an Artisan Command for processing

--- app/Http/Controllers/Download/Courses/DownloadCoursesController.php
+++ app/Http/Controllers/Download/Courses/DownloadCoursesController.php
@@ -4,34 +4,25 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Download\Courses;
 
-use App\Console\Commands\ImportCourses;
-use App\Enums\ImportEventStatus;
-use App\Enums\ImportEventType;
 use App\Models\ImportEvent;
-use Exception;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 final readonly class DownloadCoursesController
 {
     /** @throws \Exception */
     public function __invoke(Request $request): RedirectResponse
     {
-        try {
-            $event = new ImportEvent();
+        $user = $request->user();
 
-            $event->user_id = $request->user()->id;
-            $event->type = ImportEventType::COURSES->value;
-            $event->data = ['courses' => 'all'];
-            $event->count = 0;
-            $event->status = ImportEventStatus::NEW->value;
-            $event->save();
+        assert($user instanceof User);
 
-            defer(fn () => (new ImportCourses())->handle());
+        $event = ImportEvent::new($user, ['course' => 'all']);
 
-            return back()->with(['events' => ImportEvent::all()])->success('Course Import Started');
-        } catch (Exception $e) {
-            return redirect()->back()->error($e->getMessage());
-        }
+        defer(fn () => Artisan::queue('courses:import', ['eventId' => $event->id]));
+
+        return redirect()->back()->success('Course Import Started...');
     }
 }
