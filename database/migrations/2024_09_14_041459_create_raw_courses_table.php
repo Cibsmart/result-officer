140daa629899f84d7425dc19f7f0be88826da032
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-18 11:53:05 +0100

    Added a message field to RawCourse Migration

--- database/migrations/2024_09_14_041459_create_raw_courses_table.php
+++ database/migrations/2024_09_14_041459_create_raw_courses_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('code');
             $table->string('title');
             $table->string('status');
+            $table->text('message')->nullable();
             $table->timestamps();
         });
     }
