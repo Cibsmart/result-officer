1cedcc4b6f84d534721158a847c8e4933357b6ac
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-19 16:49:34 +0100

    Added Faculty Codes and a static method to getOrCreate Faculty

--- app/Models/Faculty.php
+++ app/Models/Faculty.php
@@ -8,17 +8,58 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 final class Faculty extends Model
 {
     use SoftDeletes;
 
+    private const CODES = [
+        'African Institute For Health Policy And Health Systems' => 'IHPHS',
+        'Agricultural and Natural Resources Management' => 'FARM',
+        'Basic Medical Science' => 'FBMS',
+        'Clinical Medicine' => 'FMED',
+        'Education' => 'FEDU',
+        'Engineering and Environmental Science' => 'FEES',
+        'ENTREPRENURSHIP' => 'ENT',
+        'GENERAL STUDIES' => 'GST',
+        'Health Science and Technology' => 'FHST',
+        'Institute for Peace and Strategic Studies' => 'IPSS',
+        'Law' => 'FLAW',
+        'Management Sciences' => 'FMS',
+        'Sciences' => 'FSCI',
+        'Social Science' => 'FSS',
+        'Social Science and Humanities' => 'FSSH',
+    ];
+
     protected $fillable = ['code', 'name'];
+
+    public static function getOrCreate(string $facultyName): self
+    {
+        $facultyCode = self::getCode($facultyName);
+
+        return self::firstOrCreate(
+            ['name' => $facultyName],
+            ['code' => $facultyCode],
+        );
+    }
 
     /** @return \Illuminate\Database\Eloquent\Relations\HasMany<\App\Models\Department> */
     public function departments(): HasMany
     {
         return $this->hasMany(Department::class);
+    }
+
+    private static function getCode(string $facultyName): string
+    {
+        if (array_key_exists($facultyName, self::CODES)) {
+            return self::CODES[$facultyName];
+        }
+
+        return Str::of($facultyName)->prepend('F ')
+            ->explode(' ')
+            ->map(fn ($word) => $word[0])
+            ->join('');
     }
 
     /** @return array<string, string> */
