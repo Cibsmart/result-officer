5e8059cbe639bc9708a3c8d320268055c82c523d
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-19 18:36:25 +0100

    Added Import and PendingImport Events to Download Department Page

--- resources/js/pages/download/departments/page.vue
+++ resources/js/pages/download/departments/page.vue
@@ -7,6 +7,32 @@ import { BreadcrumbItem } from "@/types";
 import Breadcrumb from "@/components/breadcrumb.vue";
 import PrimaryButton from "@/components/buttons/primaryButton.vue";
 import BaseFormSection from "@/components/baseFormSection.vue";
+import { usePoll } from "@/composables/usePoll";
+import { computed, watch } from "vue";
+import SecondaryLink from "@/components/links/secondaryLink.vue";
+import PrimaryLink from "@/components/links/primaryLink.vue";
+import StaticFeeds from "@/components/feeds/staticFeeds.vue";
+import ActiveFeeds from "@/components/feeds/activeFeeds.vue";
+
+const props = defineProps<{
+  events: Array<App.Data.Import.ImportEventData>;
+  pending: App.Data.Import.PendingImportEventData;
+}>();
+
+const hasPendingEvent = computed(() => props.pending !== null);
+
+const { start, stop } = usePoll(hasPendingEvent, ["pending", "events"]);
+
+watch(hasPendingEvent, () => {
+  if (hasPendingEvent.value === true) {
+    start();
+  } else {
+    stop();
+  }
+});
+
+const hasEvent = computed(() => props.events.length > 0);
+const disableButton = computed(() => form.processing || hasPendingEvent.value);
 
 const pages: BreadcrumbItem[] = [
   {
@@ -24,11 +50,11 @@ const submit = () => {
 </script>
 
 <template>
-  <Head title="Download Student Record" />
+  <Head title="Download Department Records" />
 
   <Breadcrumb :pages="pages" />
 
-  <BaseHeader>Download Student Record</BaseHeader>
+  <BaseHeader>Download Department Records</BaseHeader>
 
   <BasePage>
     <BaseSection>
@@ -39,10 +65,41 @@ const submit = () => {
           class="mt-6 space-y-6"
           @submit.prevent="submit">
           <div>
-            <PrimaryButton :disabled="form.processing">Download</PrimaryButton>
+            <PrimaryButton :disabled="disableButton">Download</PrimaryButton>
           </div>
         </form>
       </BaseFormSection>
     </BaseSection>
+
+    <template v-if="hasPendingEvent">
+      <BaseSection>
+        <BaseFormSection description="Pending Department Download">
+          <ActiveFeeds :data="pending" />
+
+          <SecondaryLink
+            :href="route('import.event.cancel', { event: pending.id })"
+            class="mt-6">
+            Cancel
+          </SecondaryLink>
+
+          <PrimaryLink
+            v-if="pending.canBeContinued"
+            :href="route('import.event.continue', { event: pending.id })"
+            class="ml-4 mt-4">
+            Continue
+          </PrimaryLink>
+        </BaseFormSection>
+      </BaseSection>
+    </template>
+
+    <template v-if="hasEvent">
+      <BaseSection>
+        <BaseFormSection description="Department Download History">
+          <StaticFeeds
+            :events="events"
+            class="my-4" />
+        </BaseFormSection>
+      </BaseSection>
+    </template>
   </BasePage>
 </template>
