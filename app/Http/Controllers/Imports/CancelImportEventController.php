dcf187e30e91e940741dd63446f8491f26e12cec
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-19 18:27:55 +0100

    Created a Generic ImportEvent Controllers
    - CancelImportEvent
    - ContinueImportEvent
    - Removed the Specific Cancel and Continue Controllers for Courses

--- app/Http/Controllers/Imports/CancelImportEventController.php
+++ app/Http/Controllers/Imports/CancelImportEventController.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Controllers\Download\Courses;
+namespace App\Http\Controllers\Imports;
 
 use App\Enums\ImportEventStatus;
 use App\Models\ImportEvent;
 
-final class CancelDownloadCoursesController
+final class CancelImportEventController
 {
     public function __invoke(ImportEvent $event): void
     {
