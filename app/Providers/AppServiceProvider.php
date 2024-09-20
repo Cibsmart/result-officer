a24eaba305459646c5cb714b27530c57bbf788ea
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-20 14:40:19 +0100

    Added Automatic Resolution of PortalDataService using the type of the Event in the Context

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Contracts\CourseRegistrationClient;
+use App\Contracts\PortalDataService;
 use App\Contracts\ResultClient;
 use App\Contracts\StudentClient;
+use App\Enums\ImportEventType;
 use App\Http\Clients\PortalCourseRegistrationClient;
 use App\Http\Clients\PortalResultClient;
 use App\Http\Clients\PortalStudentClient;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Context;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -24,6 +27,11 @@ final class AppServiceProvider extends ServiceProvider
         $this->app->bind(ResultClient::class, PortalResultClient::class);
         $this->app->bind(StudentClient::class, PortalStudentClient::class);
         $this->app->bind(CourseRegistrationClient::class, PortalCourseRegistrationClient::class);
+
+        $this->app->bind(
+            PortalDataService::class,
+            fn ($app) => $app->make(ImportEventType::from(Context::pull('import-event'))->services()),
+        );
     }
 
     /**
