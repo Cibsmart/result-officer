f545cd802d38f4e2dd72dae934733244c951fbc4
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-20 14:44:38 +0100

    Added service method in the ImportEventType Enum to get the Service Class for each case

--- app/Enums/ImportEventType.php
+++ app/Enums/ImportEventType.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
+use App\Services\Api\CourseService;
+use App\Services\Api\DepartmentService;
+use App\Services\Api\RegistrationService;
+use App\Services\Api\ResultService;
+use App\Services\Api\StudentService;
+
 enum ImportEventType: string
 {
     case RESULTS = 'results';
@@ -11,4 +17,15 @@ enum ImportEventType: string
     case DEPARTMENTS = 'departments';
     case STUDENTS = 'students';
     case REGISTRATIONS = 'registrations';
+
+    public function service(): string
+    {
+        return match ($this) {
+            self::COURSES => CourseService::class,
+            self::DEPARTMENTS => DepartmentService::class,
+            self::STUDENTS => StudentService::class,
+            self::REGISTRATIONS => RegistrationService::class,
+            self::RESULTS => ResultService::class,
+        };
+    }
 }
