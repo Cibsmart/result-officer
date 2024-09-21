adb5a34a92f6dd7417997b15901bb6e4f54cd4f1
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-21 17:29:46 +0100

    Created SavePortalStudent Action
    - This action handles saving of retrieved student into the raw student table

---
+++ app/Actions/Students/SavePortalStudent.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Students;
+
+use App\Data\Download\PortalStudentData;
+use App\Enums\RawDataStatus;
+use App\Models\ImportEvent;
+use App\Models\RawStudent;
+
+final class SavePortalStudent
+{
+    public function execute(ImportEvent $event, PortalStudentData $data): void
+    {
+        $exists = RawStudent::query()
+            ->where('registration_number', $data->registrationNumber)
+            ->where('status', RawDataStatus::PROCESSED)
+            ->exists();
+
+        if ($exists) {
+            return;
+        }
+
+        RawStudent::createFromPortalStudentData($data, $event);
+    }
+}
