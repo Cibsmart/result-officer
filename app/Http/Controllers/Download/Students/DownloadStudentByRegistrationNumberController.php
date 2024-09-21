acc428561edafa8b94b5bd617b11cc077afda6a0
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-21 16:16:17 +0100

    Refactored Student Download Controller to deffer to Queued PortalDataImport Commands

--- app/Http/Controllers/Download/Students/DownloadStudentByRegistrationNumberController.php
+++ app/Http/Controllers/Download/Students/DownloadStudentByRegistrationNumberController.php
@@ -4,29 +4,28 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Download\Students;
 
+use App\Enums\ImportEventType;
 use App\Http\Requests\Download\DownloadStudentByRegistrationRequest;
-use App\Repositories\StudentRepository;
-use Exception;
+use App\Models\ImportEvent;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Artisan;
 
 final readonly class DownloadStudentByRegistrationNumberController
 {
-    public function __construct(private StudentRepository $repository)
-    {
-    }
-
     public function __invoke(DownloadStudentByRegistrationRequest $request): RedirectResponse
     {
-        try {
-            $data = $this->repository->getStudentByRegistrationNumber(
-                $request->string('registration_number')->value(),
-            );
+        $user = $request->user();
 
-            $result = $this->repository->saveStudent($data);
+        assert($user instanceof User);
 
-            return back()->success("$result->registration_number Record Downloaded and Saved Successfully");
-        } catch (Exception $e) {
-            return back()->error($e->getMessage());
-        }
+        $event = ImportEvent::new($user, ImportEventType::STUDENTS,
+            [
+                'registration_number' => $request->string('registration_number')->value(),
+            ]);
+
+        Artisan::queue('portal-data:import', ['eventId' => $event->id]);
+
+        return redirect()->back()->success('Students Import Started...');
     }
 }
