6488f2848f3fd6c6fd97b7d2b6fd0a9be8de6870
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-25 20:08:09 +0100

    Added a static method to get State using StateName

--- app/Models/State.php
+++ app/Models/State.php
@@ -11,6 +11,11 @@ final class State extends Model
 {
     protected $fillable = ['country_id', 'name'];
 
+    public static function getUsingName(string $stateName): self
+    {
+        return self::query()->where('name', $stateName)->firstOrFail();
+    }
+
     /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\Country, \App\Models\State> */
     public function country(): BelongsTo
     {
