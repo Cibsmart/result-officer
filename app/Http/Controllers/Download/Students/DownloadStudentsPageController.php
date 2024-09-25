80156aece062ebd4fef3dc53a44bc657eb2fb4df
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-25 21:41:46 +0100

    Added events and pending event data to DownloadStudentPage ViewModel

--- app/Http/Controllers/Download/Students/DownloadStudentsPageController.php
+++ app/Http/Controllers/Download/Students/DownloadStudentsPageController.php
@@ -5,18 +5,29 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Download\Students;
 
 use App\Data\Department\DepartmentListData;
+use App\Data\Import\ImportEventData;
+use App\Data\Import\PendingImportEventData;
 use App\Data\Session\SessionListData;
+use App\Enums\ImportEventType;
+use App\Models\User;
 use App\ViewModels\Downloads\DownloadStudentPage;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 final readonly class DownloadStudentsPageController
 {
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
+        $user = $request->user();
+
+        assert($user instanceof User);
+
         return Inertia::render('download/students/page', new DownloadStudentPage(
             department: DepartmentListData::new(),
             session: SessionListData::new(),
-        ));
+            events: fn () => ImportEventData::new($user, ImportEventType::STUDENTS),
+            pending: fn () => PendingImportEventData::new($user, ImportEventType::STUDENTS)),
+        );
     }
 }
--- app/ViewModels/Downloads/DownloadStudentPage.php
+++ app/ViewModels/Downloads/DownloadStudentPage.php
@@ -5,14 +5,22 @@ declare(strict_types=1);
 namespace App\ViewModels\Downloads;
 
 use App\Data\Department\DepartmentListData;
+use App\Data\Import\PendingImportEventData;
 use App\Data\Session\SessionListData;
+use Closure;
+use Illuminate\Support\Collection;
 use Spatie\LaravelData\Data;
+use Spatie\TypeScriptTransformer\Attributes\TypeScriptType;
 
 final class DownloadStudentPage extends Data
 {
     public function __construct(
         public readonly DepartmentListData $department,
         public readonly SessionListData $session,
+        #[TypeScriptType(Collection::class)]
+        public readonly Closure $events,
+        #[TypeScriptType(PendingImportEventData::class)]
+        public readonly Closure $pending,
     ) {
     }
 }
