3750c9b74699025ce518a002a2220aff3a41f25b
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-25 20:06:46 +0100

    Added a static method to get EntryMode using the EntryMode Code

--- app/Models/EntryMode.php
+++ app/Models/EntryMode.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 
 final class EntryMode extends Model
 {
+    public static function getUsingCode(string $entryModeCode): self
+    {
+        return self::query()->where('code', $entryModeCode)->firstOrFail();
+    }
 }
