d9e82cfe864d3716eca52044cb93e4950e396124
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-26 09:37:54 +0100

    Wrote Test to Cover Download Registration Controllers

---
+++ tests/Feature/Controllers/Downloads/DownloadRegistrationControllersTest.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Foundation\Console\QueuedCommand;
+use Tests\Factories\CourseFactory;
+use Tests\Factories\DepartmentFactory;
+use Tests\Factories\LevelFactory;
+use Tests\Factories\SemesterFactory;
+use Tests\Factories\SessionFactory;
+use Tests\Factories\StudentFactory;
+use Tests\Factories\UserFactory;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\withoutExceptionHandling;
+
+beforeEach(function (): void {
+    Queue::fake();
+});
+
+it('renders the download course registration page', function (): void {
+    $user = UserFactory::new()->createOne();
+
+    $response = actingAs($user)->get(route('download.registrations.page'));
+
+    $response->assertStatus(200);
+});
+
+it('can start download of registrations by registration number', function (): void {
+    $user = UserFactory::new()->createOne();
+    $student = StudentFactory::new()->createOne(['registration_number' => 'EBSU/2009/51486']);
+
+    $response = actingAs($user)
+        ->from(route('download.registrations.page'))
+        ->post(route('download.registrations.registration-number.store'),
+            ['registration_number' => $student->registration_number]);
+
+    $response->assertRedirect(route('download.registrations.page'));
+
+    assertDatabaseHas('import_events',
+        ['user_id' => $user->id, 'data' => json_encode(['registration_number' => $student->registration_number])]);
+
+    Queue::assertPushed(QueuedCommand::class);
+});
+
+it('can start download of registrations by department, session and level', function (): void {
+    $user = UserFactory::new()->createOne();
+    $department = DepartmentFactory::new()->createOne(['online_id' => 1]);
+    $session = SessionFactory::new()->createOne();
+    $level = LevelFactory::new()->createOne();
+
+    $response = actingAs($user)
+        ->from(route('download.registrations.page'))
+        ->post(route('download.registrations.department-session-level.store'), [
+            'department' => ['id' => $department->id, 'name' => $department->name],
+            'level' => ['id' => $level->id, 'name' => $level->name],
+            'session' => ['id' => $session->id, 'name' => $session->name],
+        ]);
+
+    $response->assertRedirect(route('download.registrations.page'));
+
+    assertDatabaseHas('import_events',
+        [
+            'data' => json_encode([
+                'department' => $department->name,
+                'level' => (int) $level->name,
+                'online_department_id' => $department->online_id,
+                'session' => $session->name,
+            ]),
+            'user_id' => $user->id,
+        ]);
+
+    Queue::assertPushed(QueuedCommand::class);
+});
+
+it('can start download of registrations by department, session and semester', function (): void {
+    $user = UserFactory::new()->createOne();
+    $department = DepartmentFactory::new()->createOne(['online_id' => 1]);
+    $session = SessionFactory::new()->createOne();
+    $semester = SemesterFactory::new()->createOne();
+
+    $response = actingAs($user)
+        ->from(route('download.registrations.page'))
+        ->post(route('download.registrations.department-session-semester.store'), [
+            'department' => ['id' => $department->id, 'name' => $department->name],
+            'semester' => ['id' => $semester->id, 'name' => $semester->name],
+            'session' => ['id' => $session->id, 'name' => $session->name],
+        ]);
+
+    $response->assertRedirect(route('download.registrations.page'));
+
+    assertDatabaseHas('import_events',
+        [
+            'data' => json_encode([
+                'department' => $department->name,
+                'online_department_id' => $department->online_id,
+                'semester' => $semester->name,
+                'session' => $session->name,
+            ]),
+            'user_id' => $user->id,
+        ]);
+
+    Queue::assertPushed(QueuedCommand::class);
+});
+
+it('can start download of registrations by session and course', function (): void {
+
+    withoutExceptionHandling();
+    $user = UserFactory::new()->createOne();
+    $session = SessionFactory::new()->createOne();
+    $course = CourseFactory::new()->createOne(['online_id' => 1]);
+
+    $response = actingAs($user)
+        ->from(route('download.registrations.page'))
+        ->post(route('download.registrations.session-course.store'), [
+            'course' => ['id' => $course->id, 'name' => $course->name],
+            'session' => ['id' => $session->id, 'name' => $session->name],
+        ]);
+
+    $response->assertRedirect(route('download.registrations.page'));
+
+    assertDatabaseHas('import_events',
+        [
+            'data' => json_encode([
+                'course' => $course->name,
+                'online_course_id' => $course->online_id,
+                'session' => $session->name,
+            ]),
+            'user_id' => $user->id,
+        ]);
+
+    Queue::assertPushed(QueuedCommand::class);
+});
