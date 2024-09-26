9f30113e9d04d0def67543bc6ecfe1044cf21cc5
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-26 18:41:53 +0100

    Added a static method to get Semester using Name

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 
 final class Semester extends Model
 {
+    public static function getUsingName(string $semesterName): self
+    {
+        return self::query()->where('name', $semesterName)->firstOrFail();
+    }
 }
