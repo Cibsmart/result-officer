f0584ebf1d6e9341db373775856bb993096e68f4
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-27 18:33:22 +0100

    Added a static method to create RawResult from PortalResult Data

--- app/Models/RawResult.php
+++ app/Models/RawResult.php
@@ -4,19 +4,55 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Data\Download\PortalResultData;
 use App\Enums\RawDataStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 final class RawResult extends Model
 {
+    public static function createFromPortalResultData(PortalResultData $data, ImportEvent $event): void
+    {
+        $rawResult = new self();
+
+        $rawResult->import_event_id = $event->id;
+        $rawResult->online_id = $data->onlineId;
+        $rawResult->registration_number = $data->registrationNumber;
+        $rawResult->in_course = $data->inCourseScore;
+        $rawResult->exam = $data->examScore;
+        $rawResult->total = $data->totalScore;
+        $rawResult->grade = $data->grade;
+        $rawResult->remark = '';
+        $rawResult->upload_date = $data->uploadDate;
+        $rawResult->exam_date = $data->examDate;
+        $rawResult->lecturer_name = $data->lecturerName;
+        $rawResult->lecturer_phone = $data->lecturerPhoneNumber;
+        $rawResult->lecturer_email = $data->lecturerEmail;
+        $rawResult->lecturer_department = $data->lecturerDepartment;
+        $rawResult->status = RawDataStatus::PENDING;
+
+        $rawResult->save();
+    }
+
+    public function updateStatus(RawDataStatus $status): void
+    {
+        $this->status = $status;
+        $this->save();
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+        $this->save();
+    }
+
     /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\ImportEvent, \App\Models\RawResult> */
     public function event(): BelongsTo
     {
         return $this->belongsTo(ImportEvent::class);
     }
 
-    /** @return array<string, string> */
+    /** @return array{status: 'App\Enums\RawDataStatus'} */
     protected function casts(): array
     {
         return [
