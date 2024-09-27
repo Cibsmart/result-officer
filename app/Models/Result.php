c77d6c02bd22de5e4a874599eaca3ba8c6871e54
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-27 18:35:56 +0100

    Added a static method to create Result from RawResult Model
    - Also update the cast with the appropriate type information in the PHPDoc

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\RecordSource;
+use App\Values\DateValue;
+use App\Values\RegistrationNumber;
+use App\Values\TotalScore;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -23,6 +27,39 @@ final class Result extends Model
     /** @var array<int, string> */
     protected $hidden = ['data'];
 
+    public static function createFromRawResult(RawResult $rawResult, CourseRegistration $registration): void
+    {
+        $registrationNumber = RegistrationNumber::new($rawResult->registration_number);
+        $totalScore = TotalScore::new((int) $rawResult->in_course + (int) $rawResult->exam);
+        $grade = $totalScore->grade($registrationNumber->allowEGrade());
+        $gradePoint = $grade->point() * $registration->credit_unit;
+
+        $scores = [
+            'exam' => $rawResult->exam,
+            'grade' => $rawResult->grade,
+            'in-course' => $rawResult->in_course,
+            'total' => $rawResult->total,
+        ];
+
+        $result = new self();
+        $result->course_registration_id = $registration->id;
+        $result->scores = $scores;
+        $result->total_score = $totalScore->value;
+        $result->grade = $grade->value;
+        $result->grade_point = $gradePoint;
+        $result->upload_date = DateValue::fromString($rawResult->upload_date)->value;
+        $result->data = $result->getData();
+        $result->remarks = null;
+        $result->source = RecordSource::PORTAL;
+
+        $result->lecturer_name = $rawResult->lecturer_name;
+        $result->lecturer_phone = $rawResult->lecturer_phone;
+        $result->lecturer_email = $rawResult->lecturer_email;
+        $result->lecturer_department = $rawResult->lecturer_department;
+
+        $result->save();
+    }
+
     /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\CourseRegistration, \App\Models\Result> */
     public function registration(): BelongsTo
     {
@@ -34,12 +71,13 @@ final class Result extends Model
         return "{$this->course_registration_id}-{$this->total_score}-{$this->grade}-{$this->grade_point}";
     }
 
-    /** @return array<string, string> */
+    /** @return array{data: 'hashed', scores: 'array', source: 'App\Enums\RecordSource', upload_date: 'date'} */
     protected function casts(): array
     {
         return [
             'data' => 'hashed',
-            'score' => 'array',
+            'scores' => 'array',
+            'source' => RecordSource::class,
             'upload_date' => 'date',
         ];
     }
