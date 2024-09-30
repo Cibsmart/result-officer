2a3c9083a447a9e9573349b3cc0247387c7d2c2e
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-30 20:29:32 +0100

    Created RawStudentFactory

---
+++ tests/factories/RawStudentFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Factories;
+
+use App\Enums\RawDataStatus;
+use App\Models\RawStudent;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/** @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RawStudent> */
+final class RawStudentFactory extends Factory
+{
+    protected $model = RawStudent::class;
+
+    /** @return array<string, string> */
+    public function definition(): array
+    {
+        return [
+            'date_of_birth' => '27-12-1996',
+            'department_id' => '1',
+            'email' => 'test@gmail.com',
+            'entry_level' => '100',
+            'entry_mode' => 'UTME',
+            'entry_session' => '2009-2010',
+            'first_name' => fake()->firstName(),
+            'gender' => 'M',
+            'import_event_id' => ImportEventFactory::new(),
+            'jamb_registration_number' => '12345678GH',
+            'last_name' => fake()->lastName(),
+            'local_government' => 'ABAKALIKI',
+            'online_id' => '1',
+            'option' => '',
+            'other_names' => fake()->name(),
+            'phone_number' => '703-324-3322',
+            'registration_number' => 'EBSU/2009/51486',
+            'state' => 'EBONYI',
+            'status' => RawDataStatus::PENDING,
+        ];
+    }
+}
