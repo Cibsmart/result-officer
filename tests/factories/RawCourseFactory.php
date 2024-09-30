0ce3f2747179b10b66b348c6754acc0b9bf821dd
Barnabas Ifebude <cibsmarte@gmail.com>
2024-09-30 18:37:34 +0100

    Created RawCourseFactory

---
+++ tests/factories/RawCourseFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Factories;
+
+use App\Enums\RawDataStatus;
+use App\Models\RawCourse;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/** @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RawCourse> */
+final class RawCourseFactory extends Factory
+{
+    protected $model = RawCourse::class;
+
+    /** @return array<string, string> */
+    public function definition(): array
+    {
+        return [
+            'code' => fake()->countryCode(),
+            'import_event_id' => ImportEventFactory::new(),
+            'online_id' => '1',
+            'status' => RawDataStatus::PENDING,
+            'title' => fake()->country(),
+        ];
+    }
+}
