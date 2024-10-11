4a07c0e958b792a9026b418a9fb997a1f9f55f94
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-11 12:47:48 +0100

    Implement OrganizeYear Action
    - The action organizes student enrollment year from first to the last year of study if it is not already organized
    - Added test to cover the action

---
+++ app/Actions/Vetting/OrganizeYear.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Vetting;
+
+use App\Enums\YearEnum;
+use App\Models\Student;
+use Illuminate\Support\Collection;
+
+final class OrganizeYear
+{
+    public function execute(Student $student): void
+    {
+        $enrollments = $student->enrollments()->orderBy('session_id')->get();
+
+        if ($this->enrollmentYearIsOrganized($enrollments)) {
+            return;
+        }
+
+        $currentYear = YearEnum::FIRST;
+
+        foreach ($enrollments as $enrollment) {
+            $enrollment->updateYear($currentYear);
+
+            $currentYear = $currentYear->next();
+        }
+    }
+
+    /** @param \Illuminate\Support\Collection<int, \App\Models\Enrollment> $enrollments */
+    public function enrollmentYearIsOrganized(Collection $enrollments): bool
+    {
+        $sessionYear = $enrollments->pluck('year.value', 'session_id');
+
+        $previousYear = 0;
+
+        foreach ($sessionYear as $year) {
+            if ($year <= $previousYear) {
+                return false;
+            }
+
+            $previousYear = $year;
+        }
+
+        return true;
+    }
+}
