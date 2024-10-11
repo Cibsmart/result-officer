2944f911d1a6674ef1cd76457067f1f4d948249a
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-11 14:30:19 +0100

    Implemented Result Validation Action
    - The action checks through results of a given student and report invalid/tampered results
    - Added test to cover the action

---
+++ tests/Feature/Actions/Vetting/ValidateResultTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Actions\Vetting\ValidateResults;
+
+it('validates the integrity of the students results', function (): void {
+    $student = createStudentWithResults();
+
+    $validation = new ValidateResults();
+
+    $validation->execute($student);
+
+    expect($validation->report())->toBe('PASSED');
+});
+
+it('reports students results with tampered score', function (): void {
+    $student = createStudentWithResults();
+
+    $sessionEnrollment = $student->enrollments->first();
+    $semesterEnrollment = $sessionEnrollment->semesters->first();
+    $registration = $semesterEnrollment->courses->first();
+    $result = $registration->result;
+
+    $session = $sessionEnrollment->session;
+    $semester = $semesterEnrollment->semester;
+    $course = $registration->course;
+
+    $result->total_score = 101;
+    $result->save();
+
+    $validation = new ValidateResults();
+
+    $validation->execute($student);
+
+    expect($validation->report())->toBe("{$course->code} in {$semester->name} {$session->name} is invalid. \n");
+});
+
+it('reports students results with tampered grade', function (): void {
+    $student = createStudentWithResults();
+
+    $sessionEnrollment = $student->enrollments->first();
+    $semesterEnrollment = $sessionEnrollment->semesters->first();
+    $registration = $semesterEnrollment->courses->first();
+    $result = $registration->result;
+
+    $session = $sessionEnrollment->session;
+    $semester = $semesterEnrollment->semester;
+    $course = $registration->course;
+
+    $result->grade = 'Z';
+    $result->save();
+
+    $validation = new ValidateResults();
+
+    $validation->execute($student);
+
+    expect($validation->report())->toBe("{$course->code} in {$semester->name} {$session->name} is invalid. \n");
+});
+
+it('reports students results with tampered grade point', function (): void {
+    $student = createStudentWithResults();
+
+    $sessionEnrollment = $student->enrollments->first();
+    $semesterEnrollment = $sessionEnrollment->semesters->first();
+    $registration = $semesterEnrollment->courses->first();
+    $result = $registration->result;
+
+    $session = $sessionEnrollment->session;
+    $semester = $semesterEnrollment->semester;
+    $course = $registration->course;
+
+    $result->grade_point = 150;
+    $result->save();
+
+    $validation = new ValidateResults();
+
+    $validation->execute($student);
+
+    expect($validation->report())->toBe("{$course->code} in {$semester->name} {$session->name} is invalid. \n");
+});
