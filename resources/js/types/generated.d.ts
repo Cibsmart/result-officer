cb9453f2156840f9e28d7b6ed4c19e801f399c30
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-12 10:20:11 +0100

    Updated Yarn Dependencies

--- resources/js/types/generated.d.ts
+++ resources/js/types/generated.d.ts
@@ -281,7 +281,7 @@ declare namespace App.Enums {
     | "THIRD CLASS HONOURS"
     | "PASS"
     | "FAIL";
-  export type CourseStatusEnum = "F" | "R";
+  export type CourseStatus = "F" | "R";
   export type CreditUnit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 15 | 18 | 24;
   export type Gender = "M" | "F";
   export type Grade = "A" | "B" | "C" | "D" | "E" | "F";
