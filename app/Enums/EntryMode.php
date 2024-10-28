4d62c94a5871a88bf5a0ef81cd599ff2af32c4a0
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-28 12:10:53 +0100

    Added get method to EntryMode Enum to get the appropriate EntryMode for a string EntryMode

--- app/Enums/EntryMode.php
+++ app/Enums/EntryMode.php
@@ -7,7 +7,20 @@ namespace App\Enums;
 enum EntryMode: string
 {
     case UTME = 'UTME';
-    case DE = 'DE';
+    case DENT = 'DENT';
     case PD = 'PD';
     case TRAN = 'TRAN';
+
+    public static function get(string $entryMode): self
+    {
+        if (in_array($entryMode, [self::DENT->value, 'DE', 'DIRECT-ENTRY', 'DIRECT ENTRY'], true)) {
+            return self::DENT;
+        }
+
+        if (in_array($entryMode, [self::TRAN->value, 'TRANSFER'], true)) {
+            return self::TRAN;
+        }
+
+        return self::UTME;
+    }
 }
