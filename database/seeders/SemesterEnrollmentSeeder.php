82118f826049abc49abe2a1387f71a6619875fc6
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-29 13:40:35 +0100

    Fixed Seeders

--- database/seeders/SemesterEnrollmentSeeder.php
+++ database/seeders/SemesterEnrollmentSeeder.php
@@ -10,30 +10,30 @@ use Illuminate\Database\Seeder;
 final class SemesterEnrollmentSeeder extends Seeder
 {
     private array $semesters = [
-        ['enrollment_id' => 1, 'semester_id' => 1],
-        ['enrollment_id' => 2, 'semester_id' => 1],
-        ['enrollment_id' => 2, 'semester_id' => 2],
-        ['enrollment_id' => 3, 'semester_id' => 1],
-        ['enrollment_id' => 3, 'semester_id' => 2],
-        ['enrollment_id' => 4, 'semester_id' => 1],
-        ['enrollment_id' => 4, 'semester_id' => 2],
-        ['enrollment_id' => 5, 'semester_id' => 1],
-        ['enrollment_id' => 5, 'semester_id' => 2],
-        ['enrollment_id' => 6, 'semester_id' => 1],
-        ['enrollment_id' => 6, 'semester_id' => 2],
-        ['enrollment_id' => 7, 'semester_id' => 1],
-        ['enrollment_id' => 7, 'semester_id' => 2],
-        ['enrollment_id' => 8, 'semester_id' => 1],
-        ['enrollment_id' => 8, 'semester_id' => 2],
-        ['enrollment_id' => 9, 'semester_id' => 1],
-        ['enrollment_id' => 9, 'semester_id' => 2],
+        ['session_enrollment_id' => 1, 'semester_id' => 1],
+        ['session_enrollment_id' => 2, 'semester_id' => 1],
+        ['session_enrollment_id' => 2, 'semester_id' => 2],
+        ['session_enrollment_id' => 3, 'semester_id' => 1],
+        ['session_enrollment_id' => 3, 'semester_id' => 2],
+        ['session_enrollment_id' => 4, 'semester_id' => 1],
+        ['session_enrollment_id' => 4, 'semester_id' => 2],
+        ['session_enrollment_id' => 5, 'semester_id' => 1],
+        ['session_enrollment_id' => 5, 'semester_id' => 2],
+        ['session_enrollment_id' => 6, 'semester_id' => 1],
+        ['session_enrollment_id' => 6, 'semester_id' => 2],
+        ['session_enrollment_id' => 7, 'semester_id' => 1],
+        ['session_enrollment_id' => 7, 'semester_id' => 2],
+        ['session_enrollment_id' => 8, 'semester_id' => 1],
+        ['session_enrollment_id' => 8, 'semester_id' => 2],
+        ['session_enrollment_id' => 9, 'semester_id' => 1],
+        ['session_enrollment_id' => 9, 'semester_id' => 2],
     ];
 
     public function run(): void
     {
         foreach ($this->semesters as $semester) {
             SemesterEnrollment::query()->create([
-                'enrollment_id' => $semester['enrollment_id'],
+                'session_enrollment_id' => $semester['session_enrollment_id'],
                 'semester_id' => $semester['semester_id'],
             ]);
         }
