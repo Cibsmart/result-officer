9cdddf85cdcbc9a412a3c5a5c6d4c1eb10cf58c3
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-29 16:39:05 +0100

    Created ProgramCurriculumLevel and ProgramCurriculumSemester Models and Migrations

---
+++ database/migrations/2024_06_27_193226_create_program_curriculum_semesters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\ProgramCurriculumLevel;
+use App\Models\Semester;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('program_curriculum_semesters', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(ProgramCurriculumLevel::class)->constrained();
+            $table->foreignIdFor(Semester::class)->constrained();
+            $table->unsignedSmallInteger('minimum_elective_units')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('program_curriculum_semesters');
+    }
+};
