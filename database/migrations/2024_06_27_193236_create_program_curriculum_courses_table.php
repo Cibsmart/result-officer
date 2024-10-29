2ee7f9b1c507fed6ec7d20f1b3b0c5383ccb8041
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-29 16:43:56 +0100

    Refactored Program Curricula and ProgramCourses to use Program

--- database/migrations/2024_06_27_193236_create_program_curriculum_courses_table.php
+++ database/migrations/2024_06_27_193236_create_program_curriculum_courses_table.php
@@ -2,26 +2,27 @@
 
 declare(strict_types=1);
 
+use App\Models\Course;
+use App\Models\ProgramCurriculumSemester;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('program_courses', static function (Blueprint $table): void {
+        Schema::create('program_curriculum_courses', static function (Blueprint $table): void {
             $table->id();
-            $table->foreignId('program_curriculum_id')->constrained('program_curricula');
-            $table->foreignId('course_id')->constrained('courses');
+            $table->foreignIdFor(ProgramCurriculumSemester::class)->constrained();
+            $table->foreignIdFor(Course::class)->constrained();
             $table->unsignedSmallInteger('credit_unit');
-            $table->foreignId('course_type_id')->constrained('course_types');
+            $table->string('course_type');
             $table->timestamps();
 
-            $table->unique(['program_curriculum_id', 'course_id']);
+            $table->unique(['program_curriculum_semester_id', 'course_id']);
         });
     }
 
