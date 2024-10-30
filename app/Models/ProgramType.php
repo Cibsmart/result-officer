d52fb4ba7020eadebce3b12cfc23f4e049e9c944
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-30 14:59:08 +0100

    Refactored ResultSeeder to read from CSV and create Session and Semester Enrollments alongside

--- app/Models/ProgramType.php
+++ app/Models/ProgramType.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 
 final class ProgramType extends Model
 {
+    public static function getUsingCode(string $programTypeCode): self
+    {
+        return self::query()->where('code', $programTypeCode)->firstOrFail();
+    }
 }
