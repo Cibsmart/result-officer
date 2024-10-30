f95d5772302f67096c5e47271a358e663732d3d5
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-30 06:16:53 +0100

    Added cast to ProgramCurriculumSeeder

--- app/Models/ProgramCurriculumSemester.php
+++ app/Models/ProgramCurriculumSemester.php
@@ -1,10 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
+use App\Enums\CreditUnit;
 use Illuminate\Database\Eloquent\Model;
 
-class ProgramCurriculumSemester extends Model
+final class ProgramCurriculumSemester extends Model
 {
-    //
+    /** @return array{minimum_elective_units: 'App\Enums\CreditUnit'} */
+    protected function casts(): array
+    {
+        return [
+            'minimum_elective_units' => CreditUnit::class,
+        ];
+    }
 }
