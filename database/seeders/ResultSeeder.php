148303659e311d5533a55b2b04b7f6074aadefce
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-30 19:50:12 +0100

    Refactored ResultSeeder to utilize the CSVFile Reader and reduce cognitive load

--- database/seeders/ResultSeeder.php
+++ database/seeders/ResultSeeder.php
@@ -8,6 +8,7 @@ use App\Enums\CourseStatus;
 use App\Enums\CreditUnit;
 use App\Enums\Grade;
 use App\Enums\Year;
+use App\Helpers\CSVFile;
 use App\Models\Course;
 use App\Models\Level;
 use App\Models\Registration;
@@ -21,82 +22,75 @@ use App\Values\ExamScore;
 use App\Values\InCourseScore;
 use App\Values\TotalScore;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
 
 final class ResultSeeder extends Seeder
 {
     public function run(): void
     {
-        $content = Storage::get('seeders/results.csv');
+        /** @var \Illuminate\Support\Collection<int, \Illuminate\Support\Collection<string, string>> $content */
+        $content = (new CSVFile('seeders/results.csv'))->read();
 
-        assert(! is_null($content));
+        $students = $content->sortBy([
+            ['registration_number', 'asc'],
+            ['session', 'asc'],
+            ['level', 'desc'],
+            ['semester', 'asc'],
+            ['course_code', 'asc'],
+        ])->groupBy('registration_number');
 
-        $lines = explode("\n", $content);
+        foreach ($students as $registrationNumber => $results) {
+            $student = Student::query()->where('registration_number', $registrationNumber)->firstOrFail();
 
-        $header = [];
+            $this->createSessionAndSemesterEnrollments($student, $results);
+        }
+    }
 
-        $currentStudent = '';
-        $currentSession = '';
-        $currentSemester = '';
+    /** @param \Illuminate\Support\Collection<int, \Illuminate\Support\Collection<string, string>> $results */
+    private function createSessionAndSemesterEnrollments(
+        Student $student,
+        Collection $results,
+    ): void {
+        foreach ($results->groupBy('session') as $session => $sessionResults) {
+            $level = $sessionResults->first()['level'];
 
-        $student = null;
-        $sessionEnrollment = null;
-        $semesterEnrollment = null;
+            $sessionEnrollment = SessionEnrollment::query()->firstOrCreate([
+                'level_id' => Level::getUsingName($level)->id,
+                'session_id' => Session::getUsingName($session)->id,
+                'student_id' => $student->id,
+                'year' => Year::FIRST,
+            ]);
 
-        foreach ($lines as $index => $line) {
-            /** @var array<int, string> $data */
-            $data = str_getcsv($line);
-
-            if ($index === 0) {
-                $header = collect($data)->map(fn ($value) => Str::slug($value, '_'))->all();
-
-                continue;
-            }
-
-            /** @var array<string, string> $item */
-            $item = array_combine($header, $data);
-
-            if ($currentStudent !== $item['registration_number']) {
-
-                $currentStudent = $item['registration_number'];
-
-                $student = Student::query()->where('registration_number', $currentStudent)->firstOrFail();
-            }
-
-            if ($currentSession !== $item['session']) {
-
-                $currentSession = $item['session'];
-
-                $sessionEnrollment = SessionEnrollment::query()->firstOrCreate([
-                    'level_id' => Level::getUsingName($item['level'])->id,
-                    'session_id' => Session::getUsingName($currentSession)->id,
-                    'student_id' => $student->id,
-                    'year' => Year::FIRST,
-                ]);
-            }
-
-            if ($currentSemester !== $item['semester']) {
-
-                $currentSemester = $item['semester'];
-
+            foreach ($sessionResults->groupBy('semester') as $semester => $semesterResults) {
                 $semesterEnrollment = SemesterEnrollment::query()->firstOrCreate([
-                    'semester_id' => Semester::getUsingName($currentSemester)->id,
+                    'semester_id' => Semester::getUsingName($semester)->id,
                     'session_enrollment_id' => $sessionEnrollment->id,
                 ]);
-            }
 
-            $course = Course::getUsingCode($item['course_code']);
+                $this->createRegistrationsAndResults($semesterEnrollment, $semesterResults);
+            }
+        }
+    }
+
+    /** @param \Illuminate\Support\Collection<int, \Illuminate\Support\Collection<string, string>> $semesterResults */
+    private function createRegistrationsAndResults(
+        SemesterEnrollment $semesterEnrollment,
+        Collection $semesterResults,
+    ): void {
+        foreach ($semesterResults as $semesterResult) {
+            $course = Course::getUsingCode($semesterResult['course_code']);
 
             $registration = Registration::query()->create([
                 'course_id' => $course->id,
-                'course_status' => CourseStatus::from($item['course_status']),
-                'credit_unit' => CreditUnit::from((int) $item['credit_unit']),
+                'course_status' => CourseStatus::from($semesterResult['course_status']),
+                'credit_unit' => CreditUnit::from((int) $semesterResult['credit_unit']),
                 'semester_enrollment_id' => $semesterEnrollment->id,
             ]);
 
-            $score = TotalScore::fromInCourseAndExam(InCourseScore::new((int) $item['in_course_score']),
-                ExamScore::new((int) $item['exam_score']));
+            $score = TotalScore::fromInCourseAndExam(
+                InCourseScore::new((int) $semesterResult['in_course_score']),
+                ExamScore::new((int) $semesterResult['exam_score']),
+            );
             $grade = Grade::for($score);
             $gradePoint = $grade->point() * $registration->credit_unit->value;
             $data = "{$registration->id}-{$score->value}-{$grade->value}-{$gradePoint}";
@@ -106,7 +100,10 @@ final class ResultSeeder extends Seeder
                 'grade' => $grade->name,
                 'grade_point' => $gradePoint,
                 'registration_id' => $registration->id,
-                'scores' => json_encode(['in-course' => $item['in_course_score'], 'exam' => $item['exam_score']]),
+                'scores' => json_encode([
+                    'exam' => $semesterResult['exam_score'],
+                    'in-course' => $semesterResult['in_course_score'],
+                ]),
                 'total_score' => $score->value,
             ]);
         }
