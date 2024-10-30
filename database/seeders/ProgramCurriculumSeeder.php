cebf2f54ddf7ec193e5b843a9750cf923990d96d
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-30 08:28:46 +0100

    Refactored ProgramCurriculumSeeder to include ProgramCurriculum Level, Semester and Course Seeders

--- database/seeders/ProgramCurriculumSeeder.php
+++ database/seeders/ProgramCurriculumSeeder.php
@@ -4,34 +4,142 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Enums\CourseType;
+use App\Enums\CreditUnit;
+use App\Enums\EntryMode;
+use App\Models\Course;
+use App\Models\Curriculum;
+use App\Models\Level;
+use App\Models\Program;
 use App\Models\ProgramCurriculum;
+use App\Models\ProgramCurriculumCourse;
+use App\Models\ProgramCurriculumLevel;
+use App\Models\ProgramCurriculumSemester;
+use App\Models\Semester;
+use App\Models\Session;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 final class ProgramCurriculumSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        ProgramCurriculum::query()->create([
-            'curriculum_id' => 1,
-            'level_id' => 1,
-            'minimum_elective_units' => 4,
-            'program_id' => 1,
-            'semester_id' => 1,
-            'session_id' => 1,
-        ]);
+        $content = Storage::get('seeders/program_curriculum_courses.csv');
 
-        ProgramCurriculum::query()->create([
-            'curriculum_id' => 1,
-            'level_id' => 1,
-            'minimum_elective_units' => 3,
-            'program_id' => 1,
-            'semester_id' => 2,
-            'session_id' => 1,
-        ]);
+        assert(! is_null($content));
 
-        $this->call([ProgramCurriculumCourseSeeder::class]);
+        $lines = explode("\n", $content);
+
+        $header = [];
+
+        $currentProgram = '';
+        $currentCurriculum = '';
+        $currentEntrySession = '';
+        $currentEntryMode = '';
+
+        $currentLevel = '';
+        $currentSemester = '';
+
+        $programCurriculum = null;
+        $programCurriculumLevel = null;
+        $programCurriculumSemester = null;
+
+        foreach ($lines as $index => $line) {
+            /** @var array<int, string> $data */
+            $data = str_getcsv($line);
+
+            if ($index === 0) {
+                $header = collect($data)->map(fn ($value) => Str::slug($value, '_'))->all();
+
+                continue;
+            }
+
+            /** @var array<string, string> $item */
+            $item = array_combine($header, $data);
+
+            if ($currentProgram !== $item['program']
+                || $currentCurriculum !== $item['curriculum']
+                || $currentEntrySession !== $item['entry_session']
+                || $currentEntryMode !== $item['entry_mode']) {
+
+                $currentProgram = $item['program'];
+                $currentCurriculum = $item['curriculum'];
+                $currentEntrySession = $item['entry_session'];
+                $currentEntryMode = $item['entry_mode'];
+
+                $programCurriculum = $this->getProgramCurriculum(
+                    $currentProgram, $currentCurriculum,
+                    $currentEntrySession, $currentEntryMode);
+            }
+
+            if ($currentLevel !== $item['level']) {
+                $currentLevel = $item['level'];
+
+                $programCurriculumLevel = $this->getProgramCurriculumLevel($currentLevel, $programCurriculum);
+            }
+
+            if ($currentSemester !== $item['semester']) {
+                $currentSemester = $item['semester'];
+
+                $programCurriculumSemester = $this->getProgramCurriculumSemester($currentSemester,
+                    $item['minimum_elective'], $programCurriculumLevel);
+            }
+
+            $course = Course::getUsingCode($item['course']);
+            ProgramCurriculumCourse::query()->create([
+                'course_id' => $course->id,
+                'course_type' => CourseType::from($item['course_type']),
+                'credit_unit' => CreditUnit::from((int) $item['credit_unit']),
+                'program_curriculum_semester_id' => $programCurriculumSemester->id,
+            ]);
+
+        }
+    }
+
+    private function getProgramCurriculum(
+        string $currentProgram,
+        string $currentCurriculum,
+        string $currentEntrySession,
+        string $currentEntryMode,
+    ): ProgramCurriculum {
+        $program = Program::getUsingName($currentProgram);
+        $curriculum = Curriculum::getUsingCode($currentCurriculum);
+        $entrySession = Session::getUsingName($currentEntrySession);
+
+        return ProgramCurriculum::query()->firstOrCreate([
+            'curriculum_id' => $curriculum->id,
+            'entry_mode' => EntryMode::get($currentEntryMode),
+            'entry_session_id' => $entrySession->id,
+            'program_id' => $program->id,
+        ]);
+    }
+
+    private function getProgramCurriculumLevel(
+        string $currentLevel,
+        ProgramCurriculum $programCurriculum,
+    ): ProgramCurriculumLevel {
+        $level = Level::getUsingName($currentLevel);
+
+        return ProgramCurriculumLevel::query()->firstOrCreate([
+            'level_id' => $level->id,
+            'program_curriculum_id' => $programCurriculum->id,
+        ]);
+    }
+
+    private function getProgramCurriculumSemester(
+        string $currentSemester,
+        string $minimumElective,
+        ProgramCurriculumLevel $programCurriculumLevel,
+    ): ProgramCurriculumSemester {
+        $semester = Semester::getUsingName($currentSemester);
+
+        return ProgramCurriculumSemester::query()->firstOrCreate(
+            [
+                'program_curriculum_level_id' => $programCurriculumLevel->id,
+                'semester_id' => $semester->id,
+            ],
+            ['minimum_elective_units' => CreditUnit::from((int) $minimumElective)],
+        );
     }
 }
