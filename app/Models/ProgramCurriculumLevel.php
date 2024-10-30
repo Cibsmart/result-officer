8a1c330add3e6b0cf596c6ad2f43bacae86276bd
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-30 09:13:28 +0100

    Added ProgramCurriculum Levels, Semesters, and Courses Relationships

--- app/Models/ProgramCurriculumLevel.php
+++ app/Models/ProgramCurriculumLevel.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProgramCurriculumLevel extends Model
 {
-    //
+    /** @return \Illuminate\Database\Eloquent\Relations\HasMany<\App\Models\ProgramCurriculumSemester> */
+    public function semesters(): HasMany
+    {
+        return $this->HasMany(ProgramCurriculumSemester::class);
+    }
 }
