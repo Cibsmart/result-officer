b8b7c32d409b0147f06f0a823ff815ea1b4e63c2
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-30 13:39:00 +0100

    Punctuated ProgramType Codes

--- database/seeders/ProgramTypeSeeder.php
+++ database/seeders/ProgramTypeSeeder.php
@@ -11,15 +11,15 @@ final class ProgramTypeSeeder extends Seeder
 {
     /** @var array<string, string> */
     private array $programTypes = [
-        'B.A' => 'BACHELOR OF ARTS',
-        'B.A.Ed' => 'BACHELOR OF ARTS EDUCATION',
-        'B.Agric' => 'BACHELOR OF AGRICULTURE',
-        'B.Ed' => 'BACHELOR OF EDUCATION',
-        'B.Eng' => 'BACHELOR OF ENGINEERING',
+        'B.A.' => 'BACHELOR OF ARTS',
+        'B.A.Ed.' => 'BACHELOR OF ARTS EDUCATION',
+        'B.Agric.' => 'BACHELOR OF AGRICULTURE',
+        'B.Ed.' => 'BACHELOR OF EDUCATION',
+        'B.Eng.' => 'BACHELOR OF ENGINEERING',
         'B.MLS' => 'BACHELOR OF MEDICAL LABORATORY SCIENCE',
-        'B.N.Sc' => 'BACHELOR OF NURSING SCIENCE',
+        'B.N.Sc.' => 'BACHELOR OF NURSING SCIENCE',
         'B.Sc.' => 'BACHELOR OF SCIENCE',
-        'B.Sc.Ed' => 'BACHELOR OF SCIENCE EDUCATION',
+        'B.Sc.Ed.' => 'BACHELOR OF SCIENCE EDUCATION',
         'LL.B' => 'BACHELOR OF LAW',
         'MBBS' => 'BACHELOR OF MEDICINE, BACHELOR OF SURGERY',
 
