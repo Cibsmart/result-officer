03a74fcbbdf66d9e57e485e773f587d257fd1204
Barnabas Ifebude <cibsmarte@gmail.com>
2024-10-31 12:08:06 +0100

    Created Lecturer Model and Migration

---
+++ database/migrations/2024_07_09_085525_create_lecturers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lecturers', function (Blueprint $table): void {
+            $table->id();
+            $table->string('lecturer_name');
+            $table->string('lecturer_phone')->nullable();
+            $table->string('lecturer_email')->nullable();
+            $table->string('lecturer_department')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lecturers');
+    }
+};
