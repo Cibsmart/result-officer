0e346feacc755b73cc1ab549467f653b3dea5752
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-06 22:49:50 +0100

    Created Query Classes
    - StudentCourses to retrieve a collection of all student's courses and results
    - ProgramCurriculumCourses to retrieve courses for each ProgramCurriculum

---
+++ app/Queries/StudentCourses.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Queries;
+
+use App\Models\Student;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+final class StudentCourses
+{
+    private Builder $query;
+
+    public function __construct(public Student $student)
+    {
+        $this->query = DB::table('students')
+            ->join('session_enrollments', 'students.id', '=', 'session_enrollments.student_id')
+            ->join('semester_enrollments', 'session_enrollments.id', '=', 'semester_enrollments.session_enrollment_id')
+            ->join('registrations', 'semester_enrollments.id', '=', 'registrations.semester_enrollment_id')
+            ->join('results', 'registrations.id', '=', 'results.registration_id')
+            ->join('academic_sessions', 'session_enrollments.session_id', '=', 'academic_sessions.id')
+            ->join('levels', 'session_enrollments.level_id', '=', 'levels.id')
+            ->join('semesters', 'semester_enrollments.semester_id', '=', 'semesters.id')
+            ->join('courses', 'registrations.course_id', '=', 'courses.id')
+            ->select(
+                'registrations.id', 'academic_sessions.name as session', 'semesters.name as semester',
+                'courses.id as course_id', 'courses.code as course_code', 'courses.title as course_title',
+                'registrations.credit_unit', 'registrations.course_status', 'results.total_score',
+                'results.grade', 'results.grade_point',
+            )
+            ->where('students.id', $student->id)
+            ->orderBy('academic_sessions.name')
+            ->orderBy('semesters.name')
+            ->orderBy('courses.code');
+    }
+
+    public static function for(Student $student): self
+    {
+        return new self($student);
+    }
+
+    public function query(): Builder
+    {
+        return $this->query;
+    }
+
+    public function get(): Collection
+    {
+        return $this->query->get();
+    }
+}
