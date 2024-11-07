16c9ddbc1b2876f4a0a33d1f8d4c393c29db1992
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-07 17:02:54 +0100

    Added reference to VettingStep instead of VettingEvent and made remarks nullable

--- database/migrations/2024_11_06_144737_create_vetting_reports_table.php
+++ database/migrations/2024_11_06_144737_create_vetting_reports_table.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use App\Models\VettingEvent;
+use App\Models\VettingStep;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('vetting_reports', function (Blueprint $table): void {
             $table->id();
-            $table->foreignIdFor(VettingEvent::class);
+            $table->foreignIdFor(VettingStep::class);
             $table->unsignedBigInteger('vettable_id');
             $table->string('vettable_type');
             $table->string('status');
-            $table->text('remarks');
+            $table->text('remarks')->nullable();
             $table->timestamps();
         });
     }
