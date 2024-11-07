fa9307d8bf4779400272ae60a151929736028900
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-07 16:54:22 +0100

    Added Relationship to VettingEvent and VettingReport Models

--- app/Models/VettingEvent.php
+++ app/Models/VettingEvent.php
@@ -5,7 +5,20 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 final class VettingEvent extends Model
 {
+    /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\Student, \App\Models\VettingEvent> */
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Relations\HasMany<\App\Models\VettingReport, \App\Models\VettingEvent> */
+    public function reports(): HasMany
+    {
+        return $this->hasMany(VettingReport::class);
+    }
 }
--- app/Models/VettingReport.php
+++ app/Models/VettingReport.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 final class VettingReport extends Model
@@ -16,5 +17,11 @@ final class VettingReport extends Model
     public function vettingReport(): MorphTo
     {
         return $this->MorphTo();
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\VettingEvent, \App\Models\VettingReport> */
+    public function vettingEvent(): BelongsTo
+    {
+        return $this->belongsTo(VettingEvent::class);
     }
 }
