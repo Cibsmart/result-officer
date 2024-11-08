79c7c2b3dc7f6ca58a437d9e9070b6d3719cbb4f
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-08 12:43:43 +0100

    Registered Vetting Class resolution in the Service Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,8 @@ use App\Models\Result;
 use App\Models\SemesterEnrollment;
 use App\Models\SessionEnrollment;
 use App\Models\Student;
+use App\Services\Vetting\Steps\CheckResultsValidity;
+use App\Services\Vetting\Vetting;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\App;
@@ -31,6 +33,9 @@ final class AppServiceProvider extends ServiceProvider
         $this->app->bind(ResultClient::class, PortalResultClient::class);
         $this->app->bind(StudentClient::class, PortalStudentClient::class);
         $this->app->bind(RegistrationClient::class, PortalRegistrationClient::class);
+        $this->app->bind(Vetting::class, fn ($app) => new Vetting([
+            $app->make(CheckResultsValidity::class),
+        ]));
     }
 
     /**
