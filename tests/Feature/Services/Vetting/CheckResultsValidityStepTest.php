11e26e874f5d792586bffcc0f526191836f29654
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-08 19:21:40 +0100

    Added Test to Cover CheckResultsValidityStep Class

---
+++ tests/Feature/Services/Vetting/CheckResultsValidityStepTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Actions\Vetting\ValidateResults;
+use App\Enums\VettingStatus;
+use App\Enums\VettingType;
+use App\Services\Vetting\Steps\CheckResultsValidityStep;
+use Tests\Factories\VettingEventFactory;
+
+use function Pest\Laravel\assertDatabaseEmpty;
+use function Pest\Laravel\assertDatabaseHas;
+
+covers(CheckResultsValidityStep::class);
+
+it('validates students results and passed status', function (): void {
+    $student = createStudentWithResults(1, 2);
+    $vettingEvent = VettingEventFactory::new()->createOne(['student_id' => $student->id]);
+
+    $action = new ValidateResults();
+    $step = new CheckResultsValidityStep($action);
+
+    $step->check($vettingEvent);
+
+    assertDatabaseHas('vetting_steps', [
+        'status' => VettingStatus::PASSED,
+        'type' => VettingType::VALIDATE_RESULTS,
+        'vetting_event_id' => $vettingEvent->id,
+    ]);
+
+    assertDatabaseEmpty('vetting_reports');
+});
+
+it('validates students tampered results with failed status', function (): void {
+    $student = createStudentWithResults(1, 2);
+    $vettingEvent = VettingEventFactory::new()->createOne(['student_id' => $student->id]);
+
+    $registration = $student->registrations->first();
+    $result = $registration->result;
+    $result->total_score = 120;
+    $result->save();
+
+    $action = new ValidateResults();
+    $step = new CheckResultsValidityStep($action);
+
+    $step->check($vettingEvent);
+
+    assertDatabaseHas('vetting_steps', [
+        'message' => $action->report(),
+        'status' => VettingStatus::FAILED,
+        'type' => VettingType::VALIDATE_RESULTS,
+        'vetting_event_id' => $vettingEvent->id,
+    ]);
+
+    $vettingStep = $vettingEvent->vettingSteps->first();
+
+    assertDatabaseHas('vetting_reports', [
+        'status' => VettingStatus::FAILED,
+        'vetting_step_id' => $vettingStep->id,
+    ]);
+});
+
+it('validates students without results with unchecked status', function (): void {
+    $vettingEvent = VettingEventFactory::new()->createOne();
+
+    $action = new ValidateResults();
+    $step = new CheckResultsValidityStep($action);
+
+    $step->check($vettingEvent);
+
+    assertDatabaseHas('vetting_steps', [
+        'status' => VettingStatus::UNCHECKED,
+        'type' => VettingType::VALIDATE_RESULTS,
+        'vetting_event_id' => $vettingEvent->id,
+    ]);
+
+    assertDatabaseEmpty('vetting_reports');
+});
