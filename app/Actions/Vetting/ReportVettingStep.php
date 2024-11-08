2ccffb7dbc66fb0ed6d0209012d667959bdec5bc
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-08 21:02:30 +0100

    Moved createdReport into ReportVettingStep Abstract Class

--- app/Actions/Vetting/ReportVettingStep.php
+++ app/Actions/Vetting/ReportVettingStep.php
@@ -4,17 +4,32 @@ declare(strict_types=1);
 
 namespace App\Actions\Vetting;
 
+use App\Enums\VettingStatus;
+use App\Models\VettingReport;
+use App\Models\VettingStep;
+use Illuminate\Database\Eloquent\Model;
+
 abstract class ReportVettingStep
 {
-    private string $report = '';
+    private string $message = '';
 
     public function report(): string
     {
-        return $this->report;
+        return $this->message;
     }
 
-    public function updateReport(string $report): void
+    public function updateReport(string $message): void
     {
-        $this->report .= $report;
+        $this->message .= $message;
+    }
+
+    public function createReport(
+        Model $model,
+        VettingStep $vettingStep,
+        string $message,
+    ): void {
+        VettingReport::updateOrCreateUsingModel($model, $vettingStep, VettingStatus::FAILED);
+
+        $this->updateReport($message);
     }
 }
