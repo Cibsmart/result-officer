4208e79594bfb4d1eab1cba4ff1b37aa255ddce6
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-09 21:48:38 +0100

    Updated VettingType Enum

--- app/Enums/VettingType.php
+++ app/Enums/VettingType.php
@@ -11,9 +11,8 @@ enum VettingType: string
     case VALIDATE_RESULTS = 'validate_result';
     case CHECK_CREDIT_UNITS = 'credit_units';
     case CHECK_SEMESTER_CREDIT_UNITS = 'semester_credit';
-    case CHECK_FIRST_YEAR_COURSES = 'first_year';
-    case CHECK_CURRICULUM_COURSES = 'curriculum_courses';
-    case CHECK_TAKEN_COURSES = 'taken_courses';
+    case CHECK_STUDENT_COURSES = 'student_courses';
+    case CHECK_FAILED_COURSES = 'failed_courses';
     case CHECK_OTHER_COURSES = 'other_courses';
     case CHECK_ELECTIVE_COURSES = 'elective_courses';
 }
