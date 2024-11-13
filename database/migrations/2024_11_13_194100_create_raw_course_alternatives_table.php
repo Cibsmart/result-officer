3bc04713fcb933e35041ee057132aa4cd82533c2
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-13 20:59:51 +0100

    Added RawCourseAlternative Model and Migration

---
+++ database/migrations/2024_11_13_194100_create_raw_course_alternatives_table.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('raw_course_alternatives', function (Blueprint $table): void {
+            $table->id();
+            $table->unsignedBigInteger('original_course_id')->unique();
+            $table->unsignedBigInteger('alternative_course_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('raw_course_alternatives');
+    }
+};
