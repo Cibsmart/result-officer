8938e2ba7702926524433a7eb86af579a89ba167
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-13 21:46:46 +0100

    Implemented Check for alternative course ids using the online_course_id

--- app/Models/RawCourseAlternative.php
+++ app/Models/RawCourseAlternative.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 
 final class RawCourseAlternative extends Model
 {
+    public static function getAlternativeOnlineCourseId(string $originalOnlineCourseId): ?self
+    {
+        return self::query()->firstWhere('original_course_id', $originalOnlineCourseId);
+    }
 }
