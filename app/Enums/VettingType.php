8d3a12c30680ecf924946a6086f2d8e25c8f6ef5
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-17 19:25:08 +0100

    Reordered and grouped VettingTypes

--- app/Enums/VettingType.php
+++ app/Enums/VettingType.php
@@ -7,13 +7,13 @@ namespace App\Enums;
 enum VettingType: string
 {
     case ORGANIZE_STUDY_YEAR = 'organize_year';
-    case MATCH_COURSES = 'match_courses';
     case VALIDATE_RESULTS = 'validate_result';
-    case CHECK_CREDIT_UNITS = 'credit_units';
     case CHECK_SEMESTER_CREDIT_UNITS = 'semester_credit';
+    case CHECK_FAILED_COURSES = 'failed_courses';
+
+    case MATCH_COURSES = 'match_courses';
+    case CHECK_CREDIT_UNITS = 'credit_units';
     case CHECK_CORE_COURSES = 'core_courses';
     case CHECK_ELECTIVE_COURSES = 'elective_courses';
     case CHECK_FIRST_YEAR_COURSES = 'first_year';
-    case CHECK_FAILED_COURSES = 'failed_courses';
-    case CHECK_OTHER_COURSES = 'other_courses';
 }
