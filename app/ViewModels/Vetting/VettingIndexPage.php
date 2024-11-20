a914c21a8053c7a69a3f39ef559596c1509ae9f9
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-20 12:24:18 +0100

    Renamed VettingFormPage to VettingIndexPage and Deleted VettingViewPage

--- app/ViewModels/Vetting/VettingIndexPage.php
+++ app/ViewModels/Vetting/VettingIndexPage.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace App\ViewModels\Vetting;
 
 use App\Data\Department\DepartmentListData;
+use App\Data\Vetting\VettingListData;
+use Closure;
 use Spatie\LaravelData\Data;
+use Spatie\TypeScriptTransformer\Attributes\TypeScriptType;
 
-final class VettingFormPage extends Data
+final class VettingIndexPage extends Data
 {
     public function __construct(
         public readonly DepartmentListData $departments,
+        #[TypeScriptType(VettingListData::class)]
+        public readonly Closure $data,
     ) {
     }
 }
