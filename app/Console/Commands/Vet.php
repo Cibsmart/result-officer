4c8974ccd9cfa42a361b6c21a15330f4a4dfdfd1
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-20 12:25:47 +0100

    Refactored Vet Console Command to utilize vettingEventId argument instead of studentId

--- app/Console/Commands/Vet.php
+++ app/Console/Commands/Vet.php
@@ -4,22 +4,19 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use App\Models\Student;
 use App\Models\VettingEvent;
 use App\Services\Vetting\Vetting;
 use Illuminate\Console\Command;
 
 final class Vet extends Command
 {
-    protected $signature = 'app:vet {studentId}';
+    protected $signature = 'app:vet {vettingEventId}';
 
     protected $description = "Initiate Vetting of Student's Results";
 
     public function handle(Vetting $vetting): void
     {
-        $student = Student::query()->findOrFail($this->argument('studentId'));
-
-        $vettingEvent = VettingEvent::getOrCreateUsingStudent($student);
+        $vettingEvent = VettingEvent::query()->findOrFail($this->argument('vettingEventId'));
 
         $vetting->vet($vettingEvent);
     }
