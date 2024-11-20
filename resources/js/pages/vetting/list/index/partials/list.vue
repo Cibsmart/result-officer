cc2d987d3ae57c4965ba73d6fc2969d05486d30b
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-20 12:16:30 +0100

    Created List and StudentRow View Partials for displaying list of possible grandaunts

---
+++ resources/js/pages/vetting/list/index/partials/list.vue
@@ -0,0 +1,90 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+import EmptyState from "@/components/emptyState.vue";
+import IconLink from "@/components/links/iconLink.vue";
+import StudentRow from "@/pages/vetting/list/index/partials/studentRow.vue";
+
+const props = defineProps<{
+  data: App.Data.Vetting.VettingListData;
+}>();
+
+const hasRows = computed(() => props.data.graduands.length > 0);
+</script>
+
+<template>
+  <div class="rounded px-4 py-4 sm:px-6 lg:px-8 dark:bg-gray-900">
+    <div
+      class="mt-1 divide-y divide-solid divide-gray-300 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg dark:divide-gray-600 dark:ring-gray-600">
+      <div class="grid grid-flow-col">
+        <div class="p-2">
+          FACULTY: <span class="font-bold text-black dark:text-white">{{ data.faculty.name }}</span>
+        </div>
+      </div>
+
+      <div class="grid grid-flow-col">
+        <div class="p-2">
+          DEPARTMENT: <span class="font-bold text-black dark:text-white">{{ data.department.name }}</span>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <template v-if="hasRows">
+        <div class="-mx-4 mt-4 overflow-auto ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg dark:ring-gray-600">
+          <table class="min-w-full divide-y divide-gray-300 dark:divide-gray-700">
+            <thead>
+              <tr>
+                <th
+                  class="hidden px-3 py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+                  scope="col">
+                  SN
+                </th>
+
+                <th
+                  class="py-2 text-left text-sm font-semibold text-gray-900 dark:text-white"
+                  scope="col">
+                  NAME
+                </th>
+
+                <th
+                  class="hidden py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+                  scope="col">
+                  REGISTRATION NUMBER
+                </th>
+
+                <th
+                  class="hidden py-2 text-center text-sm font-semibold text-gray-900 lg:table-cell dark:text-white"
+                  scope="col">
+                  STATUS
+                </th>
+
+                <th
+                  class="py-2 text-center text-sm font-semibold text-gray-900 dark:text-white"
+                  scope="col">
+                  ACTIONS
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <template
+                v-for="(student, index) in data.graduands"
+                :key="student.id">
+                <StudentRow
+                  :index="index"
+                  :student="student" />
+              </template>
+            </tbody>
+          </table>
+        </div>
+      </template>
+
+      <EmptyState
+        v-else
+        description="Get started by downloading students from the Portal"
+        title="No Final Year Student">
+        <IconLink :href="route('download.students.page')">Download Students</IconLink>
+      </EmptyState>
+    </div>
+  </div>
+</template>
