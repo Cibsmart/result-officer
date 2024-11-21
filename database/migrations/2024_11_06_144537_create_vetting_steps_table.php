18f8bcc92609335d5e2d825637bc423b0dbeb56d
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-21 12:57:10 +0100

    Made message in vettingStep migration NOT NULLABLE

--- database/migrations/2024_11_06_144537_create_vetting_steps_table.php
+++ database/migrations/2024_11_06_144537_create_vetting_steps_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->foreignIdFor(VettingEvent::class)->constrained();
             $table->string('type');
             $table->string('status');
-            $table->text('message')->nullable();
+            $table->text('message');
             $table->timestamps();
 
             $table->unique(['vetting_event_id', 'type']);
