7d21b334ad0a2db582c72b789f9033c1f81c1eda
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-22 16:31:24 +0100

    Added Model Relationships

--- app/Models/ProgramCurriculumElectiveCourse.php
+++ app/Models/ProgramCurriculumElectiveCourse.php
@@ -5,7 +5,25 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 final class ProgramCurriculumElectiveCourse extends Model
 {
+    /**
+     * phpcs:ignore SlevomatCodingStandard.Files.LineLength
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\ProgramCurriculumElectiveGroup, \App\Models\ProgramCurriculumElectiveCourse>
+     */
+    public function programCurriculumElectiveGroup(): BelongsTo
+    {
+        return $this->belongsTo(ProgramCurriculumElectiveGroup::class);
+    }
+
+    /**
+     * phpcs:ignore SlevomatCodingStandard.Files.LineLength
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\ProgramCurriculumCourse, \App\Models\ProgramCurriculumElectiveCourse>
+     */
+    public function programCurriculumCourse(): BelongsTo
+    {
+        return $this->belongsTo(ProgramCurriculumCourse::class);
+    }
 }
--- app/Models/ProgramCurriculumElectiveGroup.php
+++ app/Models/ProgramCurriculumElectiveGroup.php
@@ -5,10 +5,18 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 final class ProgramCurriculumElectiveGroup extends Model
 {
+    /** @return \Illuminate\Database\Eloquent\Relations\MorphMany<\App\Models\VettingReport, \App\Models\Registration> */
+    public function vettable(): MorphMany
+    {
+        return $this->MorphMany(VettingReport::class, 'vettable');
+    }
+
     /**
      * phpcs:ignore SlevomatCodingStandard.Files.LineLength
      * @return \Illuminate\Database\Eloquent\Relations\HasMany<\App\Models\ProgramCurriculumElectiveCourse, \App\Models\ProgramCurriculumElectiveGroup>
@@ -16,5 +24,14 @@ final class ProgramCurriculumElectiveGroup extends Model
     public function programCurriculumElectiveCourses(): HasMany
     {
         return $this->hasMany(ProgramCurriculumElectiveCourse::class);
+    }
+
+    /**
+     * phpcs:ignore SlevomatCodingStandard.Files.LineLength
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\ProgramCurriculumSemester, \App\Models\ProgramCurriculumElectiveGroup>
+     */
+    public function programCurriculumSemester(): BelongsTo
+    {
+        return $this->belongsTo(ProgramCurriculumSemester::class);
     }
 }
