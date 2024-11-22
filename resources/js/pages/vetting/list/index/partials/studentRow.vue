fe0ad2bcbb0d3621c648c577278ffeb91236c9ee
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-22 20:30:01 +0100

    Code Quality Fix

--- resources/js/pages/vetting/list/index/partials/studentRow.vue
+++ resources/js/pages/vetting/list/index/partials/studentRow.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, ref } from "vue";
+import { computed } from "vue";
 import PrimaryLinkSmall from "@/components/links/primaryLinkSmall.vue";
 import Badge from "@/components/badge.vue";
 import SecondaryButtonSmall from "@/components/buttons/secondaryButtonSmall.vue";
