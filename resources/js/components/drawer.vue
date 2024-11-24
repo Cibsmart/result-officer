27216bfdd0a612d53b6a5e2bd0d081f09bbe9c2d
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-24 21:01:02 +0100

    Moved the Drawer close button inside

--- resources/js/components/drawer.vue
+++ resources/js/components/drawer.vue
@@ -59,7 +59,7 @@ const maxWidthClass = computed(() => {
         leave="ease-in-out duration-500"
         leave-from="opacity-100"
         leave-to="opacity-0">
-        <div class="fixed inset-0 bg-gray-500/75 transition-opacity" />
+        <div class="fixed inset-0 bg-gray-500/50 transition-opacity" />
       </TransitionChild>
 
       <div class="fixed inset-0 overflow-hidden">
@@ -76,33 +76,28 @@ const maxWidthClass = computed(() => {
               <DialogPanel
                 :class="maxWidthClass"
                 class="pointer-events-auto relative w-screen">
-                <TransitionChild
-                  as="template"
-                  enter="ease-in-out duration-500"
-                  enter-from="opacity-0"
-                  enter-to="opacity-100"
-                  leave="ease-in-out duration-500"
-                  leave-from="opacity-100"
-                  leave-to="opacity-0">
-                  <div class="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
-                    <button
-                      class="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                      type="button"
-                      @click="close">
-                      <span class="absolute -inset-2.5" />
-
-                      <span class="sr-only">Close panel</span>
-
-                      <XMarkIcon
-                        aria-hidden="true"
-                        class="size-6" />
-                    </button>
-                  </div>
-                </TransitionChild>
-
                 <div class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl dark:bg-gray-800">
                   <div class="px-4 sm:px-6">
-                    <DialogTitle class="text-base font-semibold text-gray-900 dark:text-white">{{ title }}</DialogTitle>
+                    <div class="flex items-start justify-between">
+                      <DialogTitle class="text-base font-semibold text-gray-900 dark:text-white">
+                        {{ title }}
+                      </DialogTitle>
+
+                      <div class="ml-3 flex h-7 items-center">
+                        <button
+                          class="relative rounded-md text-gray-700 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-100 dark:hover:text-white"
+                          type="button"
+                          @click="close">
+                          <span class="absolute -inset-2.5" />
+
+                          <span class="sr-only">Close panel</span>
+
+                          <XMarkIcon
+                            aria-hidden="true"
+                            class="size-6" />
+                        </button>
+                      </div>
+                    </div>
                   </div>
 
                   <div class="relative mt-6 flex-1 px-4 sm:px-6">
