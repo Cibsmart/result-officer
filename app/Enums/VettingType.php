04676cb92372aefa860d4234b09130b6c963ef20
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-24 21:33:52 +0100

    Reordered the VettingType Enum and added method to get passedMessage

--- app/Enums/VettingType.php
+++ app/Enums/VettingType.php
@@ -13,7 +13,23 @@ enum VettingType: string
 
     case MATCH_COURSES = 'match_courses';
     case CHECK_CREDIT_UNITS = 'credit_units';
+    case CHECK_FIRST_YEAR_COURSES = 'first_year';
     case CHECK_CORE_COURSES = 'core_courses';
     case CHECK_ELECTIVE_COURSES = 'elective_courses';
-    case CHECK_FIRST_YEAR_COURSES = 'first_year';
+    case CHECK_FAILED_COURSES = 'failed_courses';
+
+    public function passedMessage(): string
+    {
+        return match ($this) {
+            self::ORGANIZE_STUDY_YEAR => 'Study years are organized.',
+            self::VALIDATE_RESULTS => 'Results are valid',
+            self::CHECK_SEMESTER_CREDIT_LOADS => 'Semester credit loads are within limits',
+            self::MATCH_COURSES => 'Courses matches a course in the curriculum',
+            self::CHECK_FIRST_YEAR_COURSES => 'Registered all first year courses in the first year of study',
+            self::CHECK_CREDIT_UNITS => 'Course credit units match curriculum course units',
+            self::CHECK_CORE_COURSES => 'Attempted all Core, Required and General Courses',
+            self::CHECK_ELECTIVE_COURSES => 'Attempted necessary elective courses',
+            self::CHECK_FAILED_COURSES => 'Passed all attempted courses',
+        };
+    }
 }
--- resources/js/types/generated.d.ts
+++ resources/js/types/generated.d.ts
@@ -383,12 +383,12 @@ declare namespace App.Enums {
     | "organize_year"
     | "validate_result"
     | "semester_credit"
-    | "failed_courses"
     | "match_courses"
     | "credit_units"
+    | "first_year"
     | "core_courses"
     | "elective_courses"
-    | "first_year";
+    | "failed_courses";
   export type Year = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
 declare namespace App.ViewModels.Downloads {
