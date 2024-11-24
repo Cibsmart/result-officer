eea7431237dbbc4a4a562a4f69e0bcb32bb0bd6b
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-24 21:34:30 +0100

    Renamed CheckSemesterCreditUnits to CheckSemesterCreditLoad

--- app/Enums/VettingType.php
+++ app/Enums/VettingType.php
@@ -8,9 +8,7 @@ enum VettingType: string
 {
     case ORGANIZE_STUDY_YEAR = 'organize_year';
     case VALIDATE_RESULTS = 'validate_result';
-    case CHECK_SEMESTER_CREDIT_UNITS = 'semester_credit';
-    case CHECK_FAILED_COURSES = 'failed_courses';
-
+    case CHECK_SEMESTER_CREDIT_LOADS = 'semester_credit';
     case MATCH_COURSES = 'match_courses';
     case CHECK_CREDIT_UNITS = 'credit_units';
     case CHECK_FIRST_YEAR_COURSES = 'first_year';
