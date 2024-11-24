a4edbaaee5f483d06967047c85fb5fd251e63379
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-24 22:50:24 +0100

    Protected API Routes with Auth:Sanctum Middleware

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Api\VettingStepsApiController;
 
-Route::get('vetting-steps/{student}', [VettingStepsApiController::class, 'index'])
-    ->name('api.vetting_steps.index');
+Route::middleware(['auth:sanctum'])->group(static function (): void {
+    Route::get('vetting-steps/{student}', [VettingStepsApiController::class, 'index'])
+        ->name('api.vetting_steps.index');
+});
