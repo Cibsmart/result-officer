b18ceafba26d35a225cd630559c1520fe590334d
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-25 01:36:22 +0100

    Added failedMessage method

--- app/Enums/VettingType.php
+++ app/Enums/VettingType.php
@@ -19,7 +19,7 @@ enum VettingType: string
     public function passedMessage(): string
     {
         return match ($this) {
-            self::ORGANIZE_STUDY_YEAR => 'Study years are organized.',
+            self::ORGANIZE_STUDY_YEAR => 'Study years are organized',
             self::VALIDATE_RESULTS => 'Results are valid',
             self::CHECK_SEMESTER_CREDIT_LOADS => 'Semester credit loads are within limits',
             self::MATCH_COURSES => 'Courses matches a course in the curriculum',
@@ -28,6 +28,21 @@ enum VettingType: string
             self::CHECK_CORE_COURSES => 'Attempted all Core, Required and General Courses',
             self::CHECK_ELECTIVE_COURSES => 'Attempted necessary elective courses',
             self::CHECK_FAILED_COURSES => 'Passed all attempted courses',
+        };
+    }
+
+    public function failedMessage(): string
+    {
+        return match ($this) {
+            self::ORGANIZE_STUDY_YEAR => 'Study years not organized',
+            self::VALIDATE_RESULTS => 'The following results are invalid:',
+            self::CHECK_SEMESTER_CREDIT_LOADS => 'The credit loads for the following semester are not within limits:',
+            self::MATCH_COURSES => 'The following courses do not match any curriculum course',
+            self::CHECK_FIRST_YEAR_COURSES => 'The following first-year courses were not taken in the first year',
+            self::CHECK_CREDIT_UNITS => 'The credit units for these courses differ from the curriculum',
+            self::CHECK_CORE_COURSES => 'The following core, required or general courses were not taken',
+            self::CHECK_ELECTIVE_COURSES => 'The following elective requirements not satisfied',
+            self::CHECK_FAILED_COURSES => 'The following failed courses are not yet passed',
         };
     }
 }
