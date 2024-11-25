3de18d719d80e4df9e43514204bbdc8eff7bcd10
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-25 11:00:04 +0100

    Created Card Components

---
+++ resources/js/components/cards/card.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="divide-y divide-gray-300 overflow-hidden rounded-lg bg-white shadow dark:divide-gray-700 dark:bg-gray-800">
+    <div class="px-2 py-3 sm:p-6">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ resources/js/components/cards/cardFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="px-2 py-2 sm:px-6">
+    <slot />
+  </div>
+</template>
