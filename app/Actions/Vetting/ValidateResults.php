fda2ea13d0730f66a8102c3ecbcc9d5301c6ac08
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-27 20:04:24 +0100

    Update ValidateResults Action to only check for Registrations having Result

--- app/Actions/Vetting/ValidateResults.php
+++ app/Actions/Vetting/ValidateResults.php
@@ -6,7 +6,6 @@ namespace App\Actions\Vetting;
 
 use App\Enums\VettingStatus;
 use App\Enums\VettingType;
-use App\Models\Registration;
 use App\Models\Result;
 use App\Models\SemesterEnrollment;
 use App\Models\Session;
@@ -20,6 +19,7 @@ final class ValidateResults extends ReportVettingStep
         $this->createVettingStep($student, VettingType::VALIDATE_RESULTS);
 
         $sessionEnrollments = $student->sessionEnrollments()
+            ->whereHas('semesterEnrollments.registrations.result')
             ->with(['session', 'semesterEnrollments.semester'])
             ->get();
 
@@ -48,6 +48,7 @@ final class ValidateResults extends ReportVettingStep
         Session $session,
     ): bool {
         $registrations = $semesterEnrollment->registrations()
+            ->whereHas('result')
             ->with('result.resultDetail', 'course')
             ->get();
 
@@ -56,12 +57,18 @@ final class ValidateResults extends ReportVettingStep
         foreach ($registrations as $registration) {
             $result = $registration->result;
 
-            if ($result !== null && $this->passCheck($result)) {
+            if ($this->passCheck($result)) {
                 continue;
             }
 
             $passed = false;
-            $this->reportFailedCheck($registration, $semesterEnrollment, $session, $result);
+
+            $code = $registration->course->code;
+            $semester = $semesterEnrollment->semester;
+
+            $message = "{$code} - {$session->name} {$semester->name} semester";
+
+            $this->report($result, $message);
         }
 
         return $passed;
@@ -80,28 +87,5 @@ final class ValidateResults extends ReportVettingStep
         return $result->getData() === $resultDetailValue
             && $resultDetailHash !== ''
             && Hash::check($resultDetailValue, $resultDetailHash);
-    }
-
-    private function reportFailedCheck(
-        Registration $registration,
-        SemesterEnrollment $semesterEnrollment,
-        Session $session,
-        ?Result $result,
-    ): void {
-        $code = $registration->course->code;
-        $semester = $semesterEnrollment->semester;
-
-        $message = "{$code} - {$session->name} {$semester->name} semester";
-
-        $model = $result;
-
-        if (is_null($result)) {
-            $model = $registration;
-            $message .= ' (no result)';
-        }
-
-        assert(! is_null($model));
-
-        $this->report($model, $message);
     }
 }
