abc504f50cc80ace5dd06438dddbad38db4f4438
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-28 20:42:30 +0100

    Created validate results command with test

---
+++ app/Console/Commands/ValidateResults.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ValidateResults extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'rp:results-validate';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Validate results pending validation';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
