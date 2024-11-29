520a529bb51a4a525261cfd3ffe026109e4968fb
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-29 20:45:52 +0100

    Refactored SemesterEnrollment
    - Added gpa column accessor and mutator
    - Moved gpa column conversion to int to the accessor
    - Added course_count column to migration
    - Added courseCount method to get count of associated courses

--- database/migrations/2024_07_03_181440_create_semester_enrollments_table.php
+++ database/migrations/2024_07_03_181440_create_semester_enrollments_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(SessionEnrollment::class)->constrained();
             $table->foreignIdFor(Semester::class)->constrained();
-            $table->unsignedSmallInteger('cus')->default(0);
+            $table->unsignedTinyInteger('course_count')->default(0);
+            $table->unsignedTinyInteger('cus')->default(0);
             $table->unsignedSmallInteger('gps')->default(0);
             $table->unsignedSmallInteger('gpa')->default(0);
             $table->timestamps();
