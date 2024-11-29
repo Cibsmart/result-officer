cbf378cfc9e2d1df9735c58295488103b6e07806
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-29 11:23:54 +0100

    Deleted VettingSteps API Controller

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,5 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Api\VettingStepsApiController;
-
 Route::middleware(['auth:sanctum'])->group(static function (): void {
-    Route::get('vetting-steps/{student}', [VettingStepsApiController::class, 'index'])
-        ->name('api.vetting_steps.index');
 });
