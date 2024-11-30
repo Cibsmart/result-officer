07d2758e50579952578e0cd3fb93dcc92c52964a
Barnabas Ifebude <cibsmarte@gmail.com>
2024-11-30 06:30:06 +0100

    Added updating of student course count, credit unit sum, grade point sums and average in the clearing process

--- app/Actions/Clearing/ClearStudent.php
+++ app/Actions/Clearing/ClearStudent.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Actions\Clearing;
 
 use App\Enums\StudentStatus;
+use App\Models\SemesterEnrollment;
+use App\Models\SessionEnrollment;
 use App\Models\StatusChangeEvent;
 use App\Models\Student;
 use Exception;
@@ -18,8 +20,27 @@ final class ClearStudent
             throw new Exception('Student has not been vetted and cannot be cleared');
         }
 
+        $this->updateFCGPA($student);
+
         StatusChangeEvent::recordStudentStatusChange($student, StudentStatus::CLEARED);
 
         $student->updateStatus(StudentStatus::CLEARED);
+    }
+
+    private function updateFCGPA(Student $student): void
+    {
+        $student->sessionEnrollments()
+            ->with('semesterEnrollments.registrations')
+            ->each(function (SessionEnrollment $sessionEnrollment): void {
+
+                $sessionEnrollment->semesterEnrollments
+                    ->each(function (SemesterEnrollment $semesterEnrollment): void {
+                        $semesterEnrollment->updateSumsAndAverage();
+                    });
+
+                $sessionEnrollment->updateCountSumAndAverages();
+            });
+
+        $student->updateCountSumAndAverages();
     }
 }
