2ba94c6ce6a29a662aafb8c5076091e6a5f8f044
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-02 05:26:07 +0100

    Creating TestingServiceProvider

---
+++ app/Providers/TestingServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Testing\TestResponse;
+use Inertia\Testing\AssertableInertia;
+use Spatie\LaravelData\Data;
+
+final class TestingServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+        if (! $this->app->runningUnitTests()) {
+            return;
+        }
+
+        AssertableInertia::macro('hasData', function (string $key, Data $data) {
+
+            $this->has($key);
+
+            expect($this->prop($key))->toEqual($data->toArray());
+
+            return $this;
+        });
+
+        AssertableInertia::macro('hasDataList', function (string $key, Data $data) {
+            $this->hasData("{$key}", $data);
+
+            expect($this->prop($key))->toHaveKey('data');
+
+            return $this;
+        });
+
+        TestResponse::macro(
+            'assertHasData',
+            fn (string $key, Data $data) => $this->assertInertia(fn (AssertableInertia $inertia) => $inertia
+                ->hasData($key, $data)),
+        );
+
+        TestResponse::macro(
+            'assertHasDataList',
+            fn (string $key, Data $data) => $this->assertInertia(fn (AssertableInertia $inertia) => $inertia
+                ->hasDataList($key, $data)),
+        );
+    }
+}
