ceaa3b083d0b5943cb4f3de7bb090c3cc71f4d10
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-03 11:29:42 +0100

    Created ClearedIndexPage DTO

---
+++ app/ViewModels/Reports/ClearedIndexPageData.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ViewModels\Reports;
+
+use App\Data\Cleared\StudentListData;
+use App\Data\Department\DepartmentListData;
+use Closure;
+use Spatie\LaravelData\Data;
+use Spatie\TypeScriptTransformer\Attributes\TypeScriptType;
+
+final class ClearedIndexPageData extends Data
+{
+    public function __construct(
+        #[TypeScriptType(DepartmentListData::class)]
+        public readonly Closure $departments,
+        #[TypeScriptType(StudentListData::class)]
+        public readonly Closure $students,
+    ) {
+    }
+}
