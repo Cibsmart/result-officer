5e2409c466a97dd6022410126f2f3f1e3c032d73
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-03 12:51:29 +0100

    Renamed StudentData to ClearedStudentData

--- tests/Unit/Data/Cleared/ClearedStudentDataTest.php
+++ tests/Unit/Data/Cleared/ClearedStudentDataTest.php
@@ -2,21 +2,21 @@
 
 declare(strict_types=1);
 
-use App\Data\Cleared\StudentData;
+use App\Data\Cleared\ClearedStudentData;
 use App\Enums\StudentStatus;
 use Tests\Factories\StatusChangeEventFactory;
 use Tests\Factories\StudentFactory;
 
-covers(StudentData::class);
+covers(ClearedStudentData::class);
 
 it('creates cleared student data from student model', function (): void {
     $student = StudentFactory::new()->cleared()
         ->has(StatusChangeEventFactory::new()->cleared())
         ->createOne();
 
-    $data = StudentData::fromModel($student);
+    $data = ClearedStudentData::fromModel($student);
 
-    expect($data)->toBeInstanceOf(StudentData::class)
+    expect($data)->toBeInstanceOf(ClearedStudentData::class)
         ->toHaveProperties(['id', 'name', 'registrationNumber', 'gender', 'status', 'dateCleared'])
         ->id->toBe($student->id)
         ->name->toBe($student->name)
@@ -24,5 +24,5 @@ it('creates cleared student data from student model', function (): void {
         ->registrationNumber->toBe($student->registration_number)
         ->status->toBe(StudentStatus::CLEARED)
         ->fcgpa->toBe(number_format($student->fcgpa, 2))
-        ->dateCleared->toBe($student->statusChangeEvents->first()->date->format('Y-m-d H:i:s'));
+        ->dateCleared->toBe($student->statusChangeEvents->first()->date->format('jS F, Y'));
 });
