285f2a53ce6eee58896367180d99e9a8d73f4e4a
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-03 11:31:16 +0100

    Created ClearedIndex Vue Page with Form partial

---
+++ resources/js/pages/reports/cleared/index/page.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { Head } from "@inertiajs/vue3";
+import Breadcrumb from "@/components/breadcrumb.vue";
+import BaseHeader from "@/layouts/main/partials/baseHeader.vue";
+import BaseSection from "@/layouts/main/partials/baseSection.vue";
+import BasePage from "@/layouts/main/partials/basePage.vue";
+import { BreadcrumbItem } from "@/types";
+import ClearedForm from "@/pages/reports/cleared/index/partials/clearedForm.vue";
+
+defineProps<{
+  departments: App.Data.Department.DepartmentListData;
+}>();
+
+const pages: BreadcrumbItem[] = [
+  { name: "Cleared", href: route("department.cleared.index"), current: route().current("department.cleared.index") },
+];
+</script>
+
+<template>
+  <Head title="Cleared Students" />
+
+  <Breadcrumb :pages="pages" />
+
+  <BaseHeader> Department Cleared Students</BaseHeader>
+
+  <BasePage>
+    <BaseSection>
+      <ClearedForm :departments="departments.data" />
+    </BaseSection>
+  </BasePage>
+</template>
---
+++ resources/js/pages/reports/cleared/index/partials/clearedForm.vue
@@ -0,0 +1,86 @@
+<script lang="ts" setup>
+import PrimaryButton from "@/components/buttons/primaryButton.vue";
+import InputLabel from "@/components/inputs/inputLabel.vue";
+import InputError from "@/components/inputs/inputError.vue";
+import { useForm } from "@inertiajs/vue3";
+import SelectInput from "@/components/inputs/selectInput.vue";
+import { SelectItem } from "@/types";
+import BaseFormSection from "@/components/baseFormSection.vue";
+import { computed } from "vue";
+
+defineProps<{
+  departments: SelectItem[];
+}>();
+
+const form = useForm({ department: "", year: "" });
+
+const submit = () => {
+  form.post(route("department.cleared.store"));
+};
+
+const years = computed(() => {
+  const startYear = 2009;
+  const currentYear = new Date().getFullYear();
+  return [
+    { id: 0, name: "Select Year" },
+    ...Array.from({ length: currentYear - startYear + 2 }, (_, i) => {
+      const year = currentYear - i;
+      return { id: year, name: year.toString() };
+    }),
+  ];
+});
+</script>
+
+<template>
+  <BaseFormSection
+    description="Select Department to View List of Possible Graduands"
+    header="View List of Possible Graduands">
+    <form
+      class="mt-6 space-y-6"
+      @submit.prevent="submit">
+      <div class="flex w-full items-start space-x-4">
+        <div class="flex-1">
+          <InputLabel
+            for="department"
+            value="Department" />
+
+          <SelectInput
+            id="department"
+            v-model="form.department"
+            :items="departments"
+            class="mt-1 block w-full" />
+
+          <InputError
+            :message="form.errors.department"
+            class="mt-2" />
+        </div>
+
+        <div class="flex-1">
+          <InputLabel
+            for="year"
+            value="Year" />
+
+          <SelectInput
+            id="year"
+            v-model="form.year"
+            :items="years"
+            class="mt-1 block w-full" />
+
+          <InputError
+            :message="form.errors.year"
+            class="mt-2" />
+        </div>
+
+        <div class="flex-col">
+          <div>&nbsp;</div>
+
+          <PrimaryButton
+            :disabled="form.processing"
+            class="mt-1"
+            >View</PrimaryButton
+          >
+        </div>
+      </div>
+    </form>
+  </BaseFormSection>
+</template>
