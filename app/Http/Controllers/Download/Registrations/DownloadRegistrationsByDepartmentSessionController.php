cdd5ed2aeda7f1bd03e257b37dbd9e8b4a20547e
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-03 19:32:56 +0100

    Created Controller and Vue Tab partial for DownloadRegistrationsByDepartmentAndEntrySession

---
+++ app/Http/Controllers/Download/Registrations/DownloadRegistrationsByDepartmentSessionController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Download\Registrations;
+
+use App\Enums\ImportEventMethod;
+use App\Enums\ImportEventType;
+use App\Http\Requests\Download\DownloadStudentsByDepartmentSessionRequest;
+use App\Models\ImportEvent;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Artisan;
+
+final class DownloadRegistrationsByDepartmentSessionController
+{
+    public function __invoke(DownloadStudentsByDepartmentSessionRequest $request): RedirectResponse
+    {
+        $user = $request->user();
+
+        $event = ImportEvent::new(
+            user: $user,
+            type: ImportEventType::REGISTRATIONS,
+            method: ImportEventMethod::DEPARTMENT_SESSION,
+            data: [
+                'department' => $request->string('departmentName')->value(),
+                'entry_session' => $request->string('sessionName')->value(),
+                'online_department_id' => $request->integer('onlineDepartmentId'),
+            ],
+        );
+
+        Artisan::queue('portal-data:import', ['eventId' => $event->id]);
+
+        return redirect()->back()->success('Course Registrations Import Started...');
+    }
+}
---
+++ resources/js/pages/download/registrations/tabs/departmentEntrySession.vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+import InputLabel from "@/components/inputs/inputLabel.vue";
+import PrimaryButton from "@/components/buttons/primaryButton.vue";
+import InputError from "@/components/inputs/inputError.vue";
+import { useForm } from "@inertiajs/vue3";
+import SelectInput from "@/components/inputs/selectInput.vue";
+import { SelectItem } from "@/types";
+import BaseFormSection from "@/components/baseFormSection.vue";
+
+defineProps<{
+  departments: SelectItem[];
+  sessions: SelectItem[];
+}>();
+
+const form = useForm({
+  department: "",
+  session: "",
+});
+
+const submit = () => {
+  form.post(route("download.registrations.department-session.store"));
+};
+</script>
+
+<template>
+  <BaseFormSection
+    description="Select Department and Entry Session to download course registration records"
+    header="Download Course Registration Information">
+    <form
+      class="mt-6 space-y-6"
+      @submit.prevent="submit">
+      <div>
+        <InputLabel
+          for="department"
+          value="Department" />
+
+        <SelectInput
+          id="department"
+          v-model="form.department"
+          :items="departments"
+          class="mt-1 block w-full" />
+
+        <InputError
+          :message="form.errors.department"
+          class="mt-2" />
+      </div>
+
+      <div>
+        <InputLabel
+          for="session"
+          value="Entry Session" />
+
+        <SelectInput
+          id="session"
+          v-model="form.session"
+          :items="sessions"
+          class="mt-1 block w-full" />
+
+        <InputError
+          :message="form.errors.session"
+          class="mt-2" />
+      </div>
+
+      <div>
+        <PrimaryButton :disabled="form.processing">Download</PrimaryButton>
+      </div>
+    </form>
+  </BaseFormSection>
+</template>
