8e2aa0049b2a20ff71467d61d9fa562eede1a9ff
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-07 12:37:43 +0100

    Created StudentComprehensiveData DTO and StudentShowPage ViewModel

---
+++ app/Data/Students/StudentComprehensiveData.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Students;
+
+use App\Models\Student;
+use Spatie\LaravelData\Data;
+
+final class StudentComprehensiveData extends Data
+{
+    public function __construct(
+        public readonly StudentData $basic,
+    ) {
+    }
+
+    public static function fromModel(Student $student): self
+    {
+        return new self(basic: StudentData::fromModel($student));
+    }
+}
