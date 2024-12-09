77323839969e3755fa0b58a839f13f27c0d721df
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-09 10:41:47 +0100

    Adjusted padding and border colour

--- resources/js/components/data/dataItem.vue
+++ resources/js/components/data/dataItem.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="px-2 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+  <div class="py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
     <dt class="text-sm/6 font-medium text-gray-900 dark:text-white">{{ title }}</dt>
 
     <dd class="mt-1 text-sm/6 text-gray-800 sm:col-span-2 sm:mt-0 dark:text-gray-200">
