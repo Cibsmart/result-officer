06015051cce7a93c7a4911c1c61ad94861ddddb7
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-13 22:40:35 +0100

    Refactored Pagination Component

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -1,34 +1,77 @@
 <script lang="ts" setup>
 import { Link } from "@inertiajs/vue3";
-import { PaginationLink } from "@/types/paginate";
+import { PaginatedStudentListData } from "@/types/paginate";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/20/solid";
 
 defineProps<{
-  links?: Array<PaginationLink>;
+  paginated: PaginatedStudentListData;
 }>();
 </script>
 
 <template>
-  <div class="-mb-1 mt-6 flex flex-wrap">
-    <template v-for="(link, key) in links">
-      <template v-if="link.url === null">
-        <div
-          :key="key + 'empty'"
-          class="mb-1 mr-1 rounded border px-4 py-3 text-sm">
-          {{ link.label }}
-        </div>
-      </template>
+  <div class="flex items-center justify-between px-4 py-3 sm:px-6">
+    <div class="flex flex-1 justify-between md:hidden">
+      <Link
+        :href="paginated.prev_page_url"
+        class="relative inline-flex items-center rounded-md border border-gray-300 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-gray-600">
+        Previous
+      </Link>
 
-      <template v-else>
-        <Link
-          :key="key + 'non-empty'"
-          :class="{ 'bg-white text-gray-900 dark:bg-gray-700': link.active }"
-          :href="link.url"
-          class="mb-1 mr-1 rounded border border-gray-400 px-4 py-3 text-sm hover:bg-white dark:text-gray-300 dark:hover:bg-gray-700"
-          preserve-scroll
-          preserve-state>
-          {{ link.label }}
-        </Link>
-      </template>
-    </template>
+      <Link
+        :href="paginated.next_page_url"
+        class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-gray-600">
+        Next
+      </Link>
+    </div>
+
+    <div class="hidden md:flex md:flex-1 md:items-center md:justify-between">
+      <div>
+        <p class="text-sm text-gray-700 dark:text-gray-200">
+          Showing
+          {{ " " }}
+          <span class="font-medium">{{ paginated.from }}</span>
+          {{ " " }}
+          to
+          {{ " " }}
+          <span class="font-medium">{{ paginated.to }}</span>
+          {{ " " }}
+          of
+          {{ " " }}
+          <span class="font-medium">{{ paginated.total }}</span>
+          {{ " " }}
+          results
+        </p>
+      </div>
+
+      <div>
+        <nav
+          aria-label="Pagination"
+          class="isolate inline-flex -space-x-px rounded-md shadow-sm">
+          <Link
+            v-for="(link, index) in paginated.links"
+            :key="index"
+            :class="{
+              'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600':
+                link.active,
+              'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0 dark:text-gray-100 dark:ring-gray-400 dark:hover:bg-gray-600':
+                !link.active,
+            }"
+            :href="link.url"
+            class="relative inline-flex items-center px-4 py-2 first-of-type:rounded-l-md first-of-type:px-2 last-of-type:rounded-r-md last-of-type:px-2">
+            <ChevronLeftIcon
+              v-if="index === 0"
+              aria-hidden="true"
+              class="size-5" />
+
+            <ChevronRightIcon
+              v-else-if="index + 1 === paginated.last_page + 2"
+              aria-hidden="true"
+              class="size-5" />
+
+            <template v-else>{{ link.label }}</template>
+          </Link>
+        </nav>
+      </div>
+    </div>
   </div>
 </template>
