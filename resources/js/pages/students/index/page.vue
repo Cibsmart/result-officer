b194a5621841c448f5aab71ba492b019003c293c
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-13 21:06:03 +0100

    Implement StudentList Table

--- resources/js/pages/students/index/page.vue
+++ resources/js/pages/students/index/page.vue
@@ -4,6 +4,23 @@ import Breadcrumb from "@/components/breadcrumb.vue";
 import BaseHeader from "@/layouts/main/partials/baseHeader.vue";
 import BasePage from "@/layouts/main/partials/basePage.vue";
 import { Head } from "@inertiajs/vue3";
+import Card from "@/components/cards/card.vue";
+import CardHeader from "@/components/cards/cardHeader.vue";
+import CardFooter from "@/components/cards/cardFooter.vue";
+import { PaginatedStudentListData } from "@/types/paginate";
+import Pagination from "@/components/Pagination.vue";
+import BaseTable from "@/components/tables/baseTable.vue";
+import BaseTHead from "@/components/tables/baseTHead.vue";
+import BaseTH from "@/components/tables/baseTH.vue";
+import BaseTBody from "@/components/tables/baseTBody.vue";
+import BaseTR from "@/components/tables/baseTR.vue";
+import BaseTD from "@/components/tables/baseTD.vue";
+import PrimaryLinkSmall from "@/components/links/primaryLinkSmall.vue";
+import Badge from "@/components/badge.vue";
+
+defineProps<{
+  paginated: PaginatedStudentListData;
+}>();
 
 const pages: BreadcrumbItem[] = [
   { name: "Student", href: route("students.index"), current: route().current("students.index") },
@@ -17,5 +34,73 @@ const pages: BreadcrumbItem[] = [
 
   <BaseHeader>Students Page</BaseHeader>
 
-  <BasePage>Hello</BasePage>
+  <BasePage>
+    <Card>
+      <CardHeader>Head</CardHeader>
+
+      <BaseTable>
+        <BaseTHead>
+          <BaseTH>SN</BaseTH>
+
+          <BaseTH
+            mobile
+            position="left"
+            >NAME
+          </BaseTH>
+
+          <BaseTH>REGISTRATION NUMBER</BaseTH>
+
+          <BaseTH>STATUS</BaseTH>
+
+          <BaseTH>DEPARTMENT</BaseTH>
+
+          <BaseTH mobile>ACTION</BaseTH>
+        </BaseTHead>
+
+        <BaseTBody>
+          <BaseTR
+            v-for="(student, index) in paginated.data"
+            :key="student.id">
+            <BaseTD>{{ index + 1 }}</BaseTD>
+
+            <BaseTD
+              mobile
+              position="left">
+              {{ student.name }}
+
+              <div class="mt-1 flex flex-col text-gray-700 sm:block lg:hidden dark:text-gray-300">
+                <span>{{ student.registrationNumber }}</span>
+
+                <span class="hidden sm:inline"> || </span>
+
+                <span>
+                  <Badge :color="student.color">{{ student.status }}</Badge>
+                </span>
+
+                <span class="hidden sm:inline"> || </span>
+
+                <span>{{ student.department }}</span>
+              </div>
+            </BaseTD>
+
+            <BaseTD>{{ student.registrationNumber }}</BaseTD>
+
+            <BaseTD>
+              <Badge :color="student.color">{{ student.status }}</Badge>
+            </BaseTD>
+
+            <BaseTD>{{ student.department }}</BaseTD>
+
+            <BaseTD mobile>
+              <PrimaryLinkSmall :href="route('students.show', { student: student })">View</PrimaryLinkSmall>
+            </BaseTD>
+          </BaseTR>
+        </BaseTBody>
+      </BaseTable>
+
+      <CardFooter>
+        <Pagination :links="paginated.links" />
+      </CardFooter>
+    </Card>
+  </BasePage>
 </template>
