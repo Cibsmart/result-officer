b4ba10f7011188f5cd9312255b124285a2a62efe
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-14 15:20:39 +0100

    Created Image Component

---
+++ resources/js/components/image.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const props = defineProps<{
+  name: string;
+}>();
+
+const initials = computed(() =>
+  props.name
+    .split(" ")
+    .map((word) => word[0].toUpperCase())
+    .join("")
+    .slice(0, 2),
+);
+</script>
+
+<template>
+  <span
+    class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-200 bg-white text-[0.625rem] font-medium text-gray-800 hover:border-indigo-600 hover:text-indigo-600 group-hover:border-indigo-600 group-hover:text-indigo-600 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-50 dark:hover:border-white dark:hover:text-white dark:group-hover:border-gray-600 dark:group-hover:text-white">
+    {{ initials }}
+  </span>
+</template>
