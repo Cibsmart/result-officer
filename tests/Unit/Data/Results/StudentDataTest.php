924e799d1de63e578ac4e5065aa02865a836b55f
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-14 14:23:26 +0100

    Updated government relationship to lga

--- tests/Unit/Data/Results/StudentDataTest.php
+++ tests/Unit/Data/Results/StudentDataTest.php
@@ -15,12 +15,12 @@ test('student data object is correct', function (): void {
         ->and($studentData->lastName)->toBe($student->last_name)
         ->and($studentData->firstName)->toBe($student->first_name)
         ->and($studentData->gender)->toBe($student->gender)
-        ->and($studentData->birthDate)->toBe($student->date_of_birth->format('d/m/Y'))
+        ->and($studentData->birthDate)->toBe($student->date_of_birth->format('jS M, Y'))
         ->and($studentData->program)->toBe($student->program->name)
         ->and($studentData->department)->toBe($student->program->department->name)
         ->and($studentData->faculty)->toBe($student->program->department->faculty->name)
         ->and($studentData->admissionYear)->toBe($student->entrySession->firstYear())
-        ->and($studentData->nationality)->toBe($student->government->state->country->demonym);
+        ->and($studentData->nationality)->toBe($student->lga->state->country->demonym);
 });
 
 it('returns blank date for student without date', function (): void {
