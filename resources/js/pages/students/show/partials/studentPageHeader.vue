494666d58cb6801e1ae6b2dd936fb9ab8716562f
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-14 10:06:08 +0100

    Replaced dummy data in the StudentPageHeader Component with the dynamic values

--- resources/js/pages/students/show/partials/studentPageHeader.vue
+++ resources/js/pages/students/show/partials/studentPageHeader.vue
@@ -3,6 +3,10 @@ import Badge from "@/components/badge.vue";
 import SectionHeader from "@/components/sectionHeader.vue";
 import HamburgerMenu from "@/components/hamburger/hamburgerMenu.vue";
 import HamburgerMenuItem from "@/components/hamburger/hamburgerMenuItem.vue";
+
+defineProps<{
+  student: App.Data.Students.StudentBasicData;
+}>();
 </script>
 
 <template>
@@ -23,13 +27,13 @@ import HamburgerMenuItem from "@/components/hamburger/hamburgerMenuItem.vue";
         </div>
 
         <div class="pt-1">
-          <SectionHeader description="EBSU/2009/51486"> Ifebude Barnabas</SectionHeader>
+          <SectionHeader :description="student.registrationNumber"> {{ student.name }}</SectionHeader>
         </div>
       </div>
     </div>
 
     <div class="mt-2 flex items-center justify-between sm:ml-6 sm:shrink-0 sm:justify-start">
-      <Badge color="green">Active</Badge>
+      <Badge :color="student.statusColor">{{ student.status }}</Badge>
 
       <HamburgerMenu>
         <HamburgerMenuItem href="#">Edit</HamburgerMenuItem>
