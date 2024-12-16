bf54c9b003c06df028bca62ebb8027ad945920d5
Barnabas Ifebude <cibsmarte@gmail.com>
2024-12-16 18:33:56 +0100

    Added domain field to Institution migration and updated seeder accordingly

--- database/migrations/2024_11_29_180328_create_institutions_table.php
+++ database/migrations/2024_11_29_180328_create_institutions_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('address');
             $table->string('strategy');
+            $table->string('domain');
             $table->timestamps();
         });
     }
