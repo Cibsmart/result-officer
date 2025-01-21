64c27785d5ffdfda2a5e26f6a25867cc9874b3e5
Barnabas Ifebude <cibsmarte@gmail.com>
2025-01-21 14:47:05 +0100

    Implemented Upload of Legacy Students

---
+++ database/seeders/StudentLegacySeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Enums\RawDataStatus;
+use App\Helpers\CSVFile;
+use App\Models\StudentLegacy;
+use Illuminate\Database\Seeder;
+
+final class StudentLegacySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /** @var \Illuminate\Support\Collection<int, \Illuminate\Support\Collection<string, string>> $content */
+        $content = (new CSVFile('seeders/students_legacy.csv'))->read();
+
+        $students = $content->sortBy('registration_number');
+
+        foreach ($students as $student) {
+            StudentLegacy::query()->create([
+                'birth_date' => $student['birth_date'],
+                'email' => $student['email'],
+                'entry_level' => $student['entry_level'],
+                'entry_mode' => $student['entry_mode'],
+                'entry_year' => $student['entry_year'],
+                'first_name' => $student['firstname'],
+                'gender' => $student['gender'],
+                'jamb_registration_number' => $student['jamb_registration_number'],
+                'last_name' => $student['lastname'],
+                'legacy_id' => $student['id'],
+                'local_government' => $student['lga'],
+                'other_names' => $student['othernames'],
+                'phone_number' => $student['phone_number'],
+                'process_status' => RawDataStatus::PENDING,
+                'program_code' => $student['program_code'],
+                'registration_number' => $student['registration_number'],
+                'status' => $student['status'],
+            ]);
+        }
+    }
+}
