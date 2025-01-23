5a28d038cc376abbbebff6f0bb23224247bee0f2
Barnabas Ifebude <cibsmarte@gmail.com>
2025-01-23 18:11:55 +0100

    Added methods to update the model upon completion

--- app/Models/LegacyFinalResult.php
+++ app/Models/LegacyFinalResult.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\RawDataStatus;
 use Illuminate\Database\Eloquent\Model;
 
 final class LegacyFinalResult extends Model
 {
+    public function updateSuccess(Registration $registration): void
+    {
+        $this->registration_id = $registration->id;
+        $this->status = RawDataStatus::PROCESSED->value;
+        $this->save();
+    }
+
+    public function updateFailure(string $message): void
+    {
+        $this->message = $message;
+        $this->status = RawDataStatus::FAILED->value;
+        $this->save();
+    }
 }
