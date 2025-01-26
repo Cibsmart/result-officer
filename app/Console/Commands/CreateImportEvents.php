2551163cb44c21f3be68fdbbbea3dd6a544bbec0
Barnabas Ifebude <cibsmarte@gmail.com>
2025-01-26 02:05:03 +0100

    Updated

--- app/Console/Commands/CreateImportEvents.php
+++ app/Console/Commands/CreateImportEvents.php
@@ -7,13 +7,14 @@ namespace App\Console\Commands;
 use App\Enums\ImportEventMethod;
 use App\Enums\ImportEventStatus;
 use App\Enums\ImportEventType;
+use App\Models\Department;
 use App\Models\ImportEvent;
 use App\Models\User;
 use Illuminate\Console\Command;
 
 final class CreateImportEvents extends Command
 {
-    protected $signature = 'app:create_import_events';
+    protected $signature = 'rp:create-import-events';
 
     protected $description = 'Create Import Events for Download of Students Registrations and Results';
 
@@ -26,24 +27,32 @@ final class CreateImportEvents extends Command
             '2021-2022', '2022-2023', '2023-2024',
         ];
 
+        $departments = Department::all();
+
         foreach ($sessions as $session) {
-            ImportEvent::new(
-                user: $user,
-                type: ImportEventType::REGISTRATIONS,
-                method: ImportEventMethod::SESSION,
-                data: ['entry_session' => $session],
-                status: ImportEventStatus::QUEUED,
-            );
+            foreach ($departments as $department) {
+                ImportEvent::new(
+                    user: $user, type: ImportEventType::REGISTRATIONS, method: ImportEventMethod::DEPARTMENT_SESSION,
+                    data: [
+                        'department' => $department->name, 'online_department_id' => $department->online_id,
+                        'session' => $session,
+                    ],
+                    status: ImportEventStatus::QUEUED,
+                );
+            }
         }
 
         foreach ($sessions as $session) {
-            ImportEvent::new(
-                user: $user,
-                type: ImportEventType::RESULTS,
-                method: ImportEventMethod::SESSION,
-                data: ['entry_session' => $session],
-                status: ImportEventStatus::QUEUED,
-            );
+            foreach ($departments as $department) {
+                ImportEvent::new(
+                    user: $user, type: ImportEventType::RESULTS, method: ImportEventMethod::DEPARTMENT_SESSION,
+                    data: [
+                        'department' => $department->name, 'online_department_id' => $department->online_id,
+                        'session' => $session,
+                    ],
+                    status: ImportEventStatus::QUEUED,
+                );
+            }
         }
     }
 }
