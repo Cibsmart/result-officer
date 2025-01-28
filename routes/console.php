7784b530f9b26aa8cc61a9a65a33890596a33834
Barnabas Ifebude <cibsmarte@gmail.com>
2025-01-28 22:47:15 +0100

    Added command to backup database daily

--- routes/console.php
+++ routes/console.php
@@ -11,3 +11,5 @@ Artisan::command('inspire', function (): void {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command(ProcessQueuedImportEvent::class)->everyMinute()->withoutOverlapping();
+Schedule::command('backup:clean')->daily()->at('10:00');
+Schedule::command('backup:run')->daily()->at('10:30');
