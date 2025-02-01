6c81405a227a6bd574be5d22ffc48c1e0d204f2f
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-01 22:26:03 +0100

    Created Results Export Page

---
+++ resources/js/pages/export/results/page.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import {Head} from "@inertiajs/vue3";
+import {BreadcrumbItem, TabItem} from "@/types";
+import RegistrationNumber from "@/pages/export/results/tabs/registrationNumber.vue";
+import BaseTabPanel from "@/components/tabs/baseTabPanel.vue";
+import DepartmentEntrySession from "@/pages/export/results/tabs/departmentEntrySession.vue";
+import BasePage from "@/layouts/main/partials/basePage.vue";
+import BaseTabs from "@/components/tabs/baseTabs.vue";
+import BaseHeader from "@/layouts/main/partials/baseHeader.vue";
+import Breadcrumb from "@/components/breadcrumb.vue";
+import BaseSection from "@/layouts/main/partials/baseSection.vue";
+
+defineProps<{
+  departments: App.Data.Department.DepartmentListData;
+  sessions: App.Data.Session.SessionListData;
+  selectedIndex: number;
+}>();
+
+const pages: BreadcrumbItem[] = [
+  {
+    name: "Results Export",
+    href: route("export.results.page", {selectedIndex: 0}),
+    current: route().current("export.results.page"),
+  },
+];
+
+const tabs: TabItem[] = [
+  {name: "By Registration Number"},
+  {name: "By Dept and Entry Session"},
+];
+</script>
+
+<template>
+  <Head title="Export Result Records"/>
+
+  <Breadcrumb :pages="pages"/>
+
+  <BaseHeader>Export Result Records</BaseHeader>
+
+  <BasePage>
+    <BaseSection>
+      <BaseTabs
+          :selectedIndex="selectedIndex"
+          :tabs="tabs">
+        <BaseTabPanel>
+          <RegistrationNumber/>
+        </BaseTabPanel>
+
+        <BaseTabPanel>
+          <DepartmentEntrySession
+              :departments="departments.data"
+              :sessions="sessions.sessions"/>
+        </BaseTabPanel>
+      </BaseTabs>
+    </BaseSection>
+  </BasePage>
+</template>
