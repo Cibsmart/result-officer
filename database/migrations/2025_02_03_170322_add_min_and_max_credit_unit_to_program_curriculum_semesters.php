9f12e7bc430c45a5270138f276a2b47ce44d8714
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-04 13:07:24 +0100

    Code style fixes

--- database/migrations/2025_02_03_170322_add_min_and_max_credit_unit_to_program_curriculum_semesters.php
+++ database/migrations/2025_02_03_170322_add_min_and_max_credit_unit_to_program_curriculum_semesters.php
@@ -7,26 +7,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
-        Schema::table('program_curriculum_semesters', function (Blueprint $table) {
+        Schema::table('program_curriculum_semesters', function (Blueprint $table): void {
             $table->tinyInteger('maximum_credit_units')->default(24)->after('minimum_elective_units');
             $table->tinyInteger('minimum_credit_units')->default(15)->after('minimum_elective_units');
         });
 
-        Schema::table('semester_enrollments', function (Blueprint $table) {
+        Schema::table('semester_enrollments', function (Blueprint $table): void {
             $table->foreignIdFor(ProgramCurriculumSemester::class)->nullable()->after('gpa')->constrained();
         });
     }
 
     public function down(): void
     {
-        Schema::table('program_curriculum_semesters', function (Blueprint $table) {
+        Schema::table('program_curriculum_semesters', function (Blueprint $table): void {
             $table->dropColumn(['minimum_credit_units', 'maximum_credit_units']);
         });
 
-        Schema::table('semester_enrollments', function (Blueprint $table) {
+        Schema::table('semester_enrollments', function (Blueprint $table): void {
             $table->dropForeign(['program_curriculum_semester_id']);
             $table->dropColumn('program_curriculum_semester_id');
         });
