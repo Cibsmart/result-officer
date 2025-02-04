c95b2b772b3d9ca4716e1fc8317e6d1c578a7baf
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-04 11:49:25 +0100

    Updated migration to include adding of ProgramCurriculumSemester Foreign Relationship to SemesterEnrollments

--- database/migrations/2025_02_03_170322_add_min_and_max_credit_unit_to_program_curriculum_semesters.php
+++ database/migrations/2025_02_03_170322_add_min_and_max_credit_unit_to_program_curriculum_semesters.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Models\ProgramCurriculumSemester;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,12 +14,21 @@ return new class extends Migration {
             $table->tinyInteger('maximum_credit_units')->default(24)->after('minimum_elective_units');
             $table->tinyInteger('minimum_credit_units')->default(15)->after('minimum_elective_units');
         });
+
+        Schema::table('semester_enrollments', function (Blueprint $table) {
+            $table->foreignIdFor(ProgramCurriculumSemester::class)->nullable()->after('gpa')->constrained();
+        });
     }
 
     public function down(): void
     {
         Schema::table('program_curriculum_semesters', function (Blueprint $table) {
             $table->dropColumn(['minimum_credit_units', 'maximum_credit_units']);
+        });
+
+        Schema::table('semester_enrollments', function (Blueprint $table) {
+            $table->dropForeign(['program_curriculum_semester_id']);
+            $table->dropColumn('program_curriculum_semester_id');
         });
     }
 };
