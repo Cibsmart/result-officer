ffe5f90b5dda86ddd3ee3c46cf8bcd246f50e7cd
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-05 12:21:24 +0100

    Moved Counts, Sums and Averages computation from SessionEnrollment to FinalSessionEnrollment

--- app/Models/FinalSessionEnrollment.php
+++ app/Models/FinalSessionEnrollment.php
@@ -4,8 +4,103 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\ComputationStrategy;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 final class FinalSessionEnrollment extends Model
 {
+
+    /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\Session, \App\Models\SessionEnrollment> */
+    public function session(): BelongsTo
+    {
+        return $this->belongsTo(Session::class);
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\Student, \App\Models\SessionEnrollment> */
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    /**
+     * phpcs:ignore SlevomatCodingStandard.Files.LineLength
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<\App\Models\FinalSemesterEnrollment, \App\Models\FinalSessionEnrollment>
+     */
+    public function finalSemesterEnrollments(): HasMany
+    {
+        return $this->hasMany(FinalSemesterEnrollment::class);
+    }
+
+    /**
+     * phpcs:ignore SlevomatCodingStandard.Files.LineLength
+     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough<\App\Models\FinalResult, \App\Models\FinalSemesterEnrollment, \App\Models\FinalSessionEnrollment>
+     */
+    public function finalResults(): HasManyThrough
+    {
+        return $this->hasManyThrough(FinalResult::class, FinalSemesterEnrollment::class);
+    }
+
+    public function getResultCount(): int
+    {
+        return $this->finalSemesterEnrollments->sum('result_count');
+    }
+
+    public function getCreditUnitSum(): int
+    {
+        return $this->finalSemesterEnrollments->sum('credit_unit_sum');
+    }
+
+    public function getGradePointSum(): int
+    {
+        return $this->finalSemesterEnrollments->sum('grade_point_sum');
+    }
+
+    public function getGradePointAverageSum(): float
+    {
+        return $this->finalSemesterEnrollments->sum('grade_point_average_sum');
+    }
+
+    public function getCumulativeGradePointAverage(): float
+    {
+        $institution = Institution::first();
+
+        if ($institution->strategy === ComputationStrategy::SEMESTER) {
+            return round($this->getGradePointAverageSum() / $this->finalSemesterEnrollments->count(), 3);
+        }
+
+        return round($this->getGradePointSum() / $this->getCreditUnitSum(), 3);
+    }
+
+    public function updateCountSumAndAverages(): void
+    {
+        $this->credit_unit_sum = $this->getCreditUnitSum();
+        $this->grade_point_sum = $this->getGradePointSum();
+        $this->grade_point_average_sum = $this->getGradePointAverageSum();
+        $this->grade_point_cummulative = $this->getCumulativeGradePointAverage();
+        $this->result_count_count = $this->getResultCount();
+        $this->save();
+    }
+
+
+    /** @return \Illuminate\Database\Eloquent\Casts\Attribute<int, float> */
+    protected function gradePointAverageSum(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (int $value): float => $value / 1000,
+            set: static fn (float $value): int => (int) ($value * 1000),
+        );
+    }
+
+    /** @return \Illuminate\Database\Eloquent\Casts\Attribute<int, float> */
+    protected function gradePointAverageCummulative(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (int $value): float => $value / 1000,
+            set: static fn (float $value): int => (int) ($value * 1000),
+        );
+    }
 }
