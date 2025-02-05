977b1aff74fd7de16b3dc5a008689b93f84b6db5
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-05 14:31:10 +0100

    Updated ClearStudent Action to clear and archive student results
    - Updated tests

--- app/Actions/Clearing/ClearStudent.php
+++ app/Actions/Clearing/ClearStudent.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace App\Actions\Clearing;
 
 use App\Enums\StudentStatus;
+use App\Models\FinalResult;
+use App\Models\FinalSemesterEnrollment;
+use App\Models\FinalSessionEnrollment;
+use App\Models\FinalStudent;
 use App\Models\SemesterEnrollment;
 use App\Models\SessionEnrollment;
 use App\Models\StatusChangeEvent;
@@ -16,31 +20,66 @@ final class ClearStudent
     /** @throws \Exception */
     public function execute(Student $student): void
     {
-        if (! $student->canBeCleared()) {
+        if (!$student->canBeCleared()) {
             throw new Exception('Student has not been vetted and cannot be cleared');
         }
 
-        $this->updateFCGPA($student);
+        $this->archiveResultsFor($student);
 
         StatusChangeEvent::recordStudentStatusChange($student, StudentStatus::CLEARED);
 
         $student->updateStatus(StudentStatus::CLEARED);
     }
 
-    private function updateFCGPA(Student $student): void
+    private function archiveResultsFor(Student $student): void
     {
-        $student->sessionEnrollments()
-            ->with('semesterEnrollments.registrations')
-            ->each(function (SessionEnrollment $sessionEnrollment): void {
+        $finalStudent = $student->FinalStudent;
+        assert($finalStudent instanceof FinalStudent);
 
-                $sessionEnrollment->semesterEnrollments
-                    ->each(function (SemesterEnrollment $semesterEnrollment): void {
-                        $semesterEnrollment->updateSumsAndAverage();
-                    });
+        $sessionEnrollments = $student->sessionEnrollments()
+            ->with('semesterEnrollments.registrations.course')
+            ->orderBy('year')
+            ->get();
 
-                $sessionEnrollment->updateCountSumAndAverages();
-            });
+        foreach ($sessionEnrollments as $sessionEnrollment) {
+            $finalSessionEnrollment = FinalSessionEnrollment::fromSessionEnrollment(
+                sessionEnrollment: $sessionEnrollment,
+                finalStudent: $finalStudent,
+            );
 
-        $student->updateCountSumAndAverages();
+            $this->archiveSessionResults($sessionEnrollment, $finalSessionEnrollment);
+
+            $finalSessionEnrollment->updateCountSumAndAverages();
+        }
+
+        $finalStudent->updateCountSumAndAverages();
+    }
+
+    private function archiveSessionResults(
+        SessionEnrollment $sessionEnrollment,
+        FinalSessionEnrollment $finalSessionEnrollment,
+    ): void {
+        $semesterEnrollments = $sessionEnrollment->semesterEnrollments;
+
+        foreach ($semesterEnrollments as $semesterEnrollment) {
+            $finalSemesterEnrollment = FinalSemesterEnrollment::fromSemesterEnrollment(
+                semesterEnrollment: $semesterEnrollment,
+                finalSessionEnrollment: $finalSessionEnrollment,
+            );
+
+            $this->archiveSemesterResults($semesterEnrollment, $finalSemesterEnrollment);
+
+            $finalSemesterEnrollment->updateSumsAndAverage();
+        }
+    }
+
+    private function archiveSemesterResults(
+        SemesterEnrollment $semesterEnrollment,
+        FinalSemesterEnrollment $finalSemesterEnrollment,
+    ): void {
+        $registrations = $semesterEnrollment->registrations;
+        foreach ($registrations as $registration) {
+            FinalResult::fromRegistration($registration, $finalSemesterEnrollment);
+        }
     }
 }
--- tests/Feature/Actions/ClearStudentTest.php
+++ tests/Feature/Actions/ClearStudentTest.php
@@ -3,9 +3,14 @@
 declare(strict_types=1);
 
 use App\Actions\Clearing\ClearStudent;
+use App\Enums\ComputationStrategy;
 use App\Enums\StudentStatus;
 use App\Enums\VettingEventStatus;
+use App\Models\FinalResult;
+use App\Models\Registration;
 use App\Models\StatusChangeEvent;
+use Tests\Factories\FinalStudentFactory;
+use Tests\Factories\InstitutionFactory;
 use Tests\Factories\StudentFactory;
 use Tests\Factories\VettingEventFactory;
 
@@ -13,9 +18,10 @@ use function Pest\Laravel\assertDatabaseHas;
 
 covers(ClearStudent::class);
 
-it('clears a student', function (): void {
+it('can mark a student as cleared', function (): void {
     $student = StudentFactory::new()->createOne(['status' => StudentStatus::FINAL_YEAR]);
     VettingEventFactory::new()->for($student)->createOne(['status' => VettingEventStatus::PASSED]);
+    FinalStudentFactory::new()->createOne(['student_id' => $student->id]);
 
     $action = new ClearStudent();
 
@@ -24,9 +30,24 @@ it('clears a student', function (): void {
     expect($student->status)->toBe(StudentStatus::CLEARED);
 });
 
+it('archives results of a cleared student', function (): void {
+    InstitutionFactory::new()->createOne(['strategy' => ComputationStrategy::SEMESTER->value]);
+    $student = createStudentWithResults(attributes: ['status' => StudentStatus::FINAL_YEAR]);
+    VettingEventFactory::new()->for($student)->createOne(['status' => VettingEventStatus::PASSED]);
+    FinalStudentFactory::new()->createOne(['student_id' => $student->id]);
+
+    $action = new ClearStudent();
+
+    $action->execute($student);
+
+    expect($student->status)->toBe(StudentStatus::CLEARED)
+        ->and(FinalResult::count())->toBe(Registration::count());
+});
+
 it('records status change event', function (): void {
     $student = StudentFactory::new()->createOne(['status' => StudentStatus::FINAL_YEAR]);
     VettingEventFactory::new()->for($student)->createOne(['status' => VettingEventStatus::PASSED]);
+    FinalStudentFactory::new()->createOne(['student_id' => $student->id]);
 
     $action = new ClearStudent();
 
