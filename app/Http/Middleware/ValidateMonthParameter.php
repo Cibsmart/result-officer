a5431843fe83bcce08b61734b9c4f0c710bad147
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-06 09:15:57 +0100

    Created ValidateMonthParameter Middleware

---
+++ app/Http/Middleware/ValidateMonthParameter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\Months;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateMonthParameter
+{
+    /**
+     * Handle an incoming request.
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $month = $request->route('month');
+
+        $validator = Validator::make(['month' => $month], [
+            'month' => [Rule::enum(Months::class)],
+        ]);
+
+        if ($month !== null && $validator->fails()) {
+            abort(404, 'Invalid Month');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ use App\Http\Controllers\Result\ViewStudentResultController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\Summary\DepartmentResultSummaryController;
 use App\Http\Controllers\Vetting\VettingController;
+use App\Http\Middleware\ValidateMonthParameter;
 use App\Http\Middleware\ValidateYearParameter;
 use Illuminate\Support\Facades\Route;
 
@@ -142,7 +143,7 @@ Route::middleware(['auth'])->group(static function (): void {
     Route::prefix('department/cleared/students')->group(static function (): void {
         Route::get('{department?}/{year?}/{month?}', [DepartmentClearedController::class, 'index'])
             ->name('department.cleared.index')
-            ->middleware([ValidateYearParameter::class]);
+            ->middleware([ValidateYearParameter::class, ValidateMonthParameter::class]);
         Route::post('', [DepartmentClearedController::class, 'store'])->name('department.cleared.store');
     });
 
