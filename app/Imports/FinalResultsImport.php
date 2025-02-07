51492d33d01beb26a5828d008ed0d67584934dca
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-07 09:03:47 +0100

    Implement the model method of the FinalResultsImport

--- app/Imports/FinalResultsImport.php
+++ app/Imports/FinalResultsImport.php
@@ -4,19 +4,34 @@ declare(strict_types=1);
 
 namespace App\Imports;
 
+use App\Models\ExcelImportEvent;
+use App\Models\RawFinalResult;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-final class FinalResultsImport implements ToModel
+final class FinalResultsImport implements ToModel, WithCalculatedFormulas, WithHeadingRow
 {
     use Importable;
 
+    /** @param array<string, string> $headings */
+    public function __construct(private readonly ExcelImportEvent $event, private readonly array $headings)
+    {
+    }
+
+    /** @param array<string, string> $headings */
+    public static function new(ExcelImportEvent $event, array $headings): self
+    {
+        return new self($event, $headings);
+    }
+
     /**
-     * @param array<int, string> $row
+     * @param array<string, string> $row
      * {@inheritDoc}
      */
-    public function model(array $row): void
+    public function model(array $row): RawFinalResult
     {
-        dd($row);
+        return RawFinalResult::fromExcelRow($row, $this->event, $this->headings);
     }
 }
