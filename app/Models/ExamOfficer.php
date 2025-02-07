367ec256c8900dab56b495d13b2487b6466cc2af
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-07 19:28:49 +0100

    Added AddOrCreate method in ExamOfficer

--- app/Models/ExamOfficer.php
+++ app/Models/ExamOfficer.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 
 final class ExamOfficer extends Model
 {
+    public static function getOrCreate(string $examOfficerName): self
+    {
+        $examOfficer = self::query()->where('name', $examOfficerName)->first();
+
+        if ($examOfficer) {
+            return $examOfficer;
+        }
+
+        $examOfficer = new self();
+        $examOfficer->name = $examOfficerName;
+        $examOfficer->save();
+
+        return $examOfficer;
+    }
 }
