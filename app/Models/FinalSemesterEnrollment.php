7f8f190edca031d027ea92f8d3d2d6692f867597
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-08 06:04:49 +0100

    Added getOrCreate static method in FinalSemesterEnrollment

--- app/Models/FinalSemesterEnrollment.php
+++ app/Models/FinalSemesterEnrollment.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Cache;
 
 final class FinalSemesterEnrollment extends Model
 {
@@ -23,6 +24,16 @@ final class FinalSemesterEnrollment extends Model
         $finalSemesterEnrollment->save();
 
         return $finalSemesterEnrollment;
+    }
+
+    public static function getOrCreate(FinalSessionEnrollment $sessionEnrollment, Semester $semester): self
+    {
+        return
+            Cache::remember("final_semester_enrollment_{$sessionEnrollment->id}_{$semester->id}",
+                now()->addMinutes(5),
+                fn () => self::query()->firstOrCreate(
+                    ['final_session_enrollment_id' => $sessionEnrollment->id, 'semester_id' => $semester->id],
+                ));
     }
 
     /**
