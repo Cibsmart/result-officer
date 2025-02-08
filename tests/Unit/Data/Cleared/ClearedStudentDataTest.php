d767d3fb6af7ad2cafbace80bff89bf3a15fe184
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-08 20:56:45 +0100

    Fixed failing test

--- tests/Unit/Data/Cleared/ClearedStudentDataTest.php
+++ tests/Unit/Data/Cleared/ClearedStudentDataTest.php
@@ -15,17 +15,17 @@ it('creates cleared student data from student model', function (): void {
         ->has(StatusChangeEventFactory::new()->cleared())
         ->createOne();
 
-    FinalStudentFactory::new()->for($student)->createOne();
+    $finalStudent = FinalStudentFactory::new()->for($student)->createOne();
 
     $data = ClearedStudentData::fromModel($student);
 
     expect($data)->toBeInstanceOf(ClearedStudentData::class)
-        ->toHaveProperties(['id', 'name', 'registrationNumber', 'gender', 'status', 'dateCleared'])
+        ->toHaveProperties(['id', 'name', 'registrationNumber', 'gender', 'status', 'batch'])
         ->id->toBe($student->id)
         ->name->toBe($student->name)
         ->gender->toBe($student->gender)
         ->registrationNumber->toBe($student->registration_number)
         ->status->toBe(StudentStatus::CLEARED)
         ->fcgpa->toBe(number_format($student->finalStudent->final_cummulative_grade_point_average, 2))
-        ->dateCleared->toBe($student->statusChangeEvents->first()->date->format('jS M, Y'));
+        ->batch->toBe("{$finalStudent->month} {$finalStudent->year}");
 });
