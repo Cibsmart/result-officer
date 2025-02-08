28c0ba05953b66a74911ded05c9acfe08e530fc2
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-08 20:28:18 +0100

    Replaced usePoll with inertia implementation

--- resources/js/pages/download/components/importEvents.vue
+++ resources/js/pages/download/components/importEvents.vue
@@ -5,8 +5,8 @@ import StaticFeeds from "@/components/feeds/staticFeeds.vue";
 import ActiveFeeds from "@/components/feeds/activeFeeds.vue";
 import BaseSection from "@/layouts/main/partials/baseSection.vue";
 import PrimaryLink from "@/components/links/primaryLink.vue";
-import { usePoll } from "@/composables/usePoll";
-import { computed, watch } from "vue";
+import { computed, watch, onMounted } from "vue";
+import { usePoll, router } from "@inertiajs/vue3";
 
 const props = defineProps<{
   events: Array<App.Data.Imports.ImportEventData>;
@@ -17,11 +17,18 @@ const hasPendingEvent = computed(() => props.pending !== null);
 const hasEvent = computed(() => props.events.length > 0);
 const pendingDescription = computed(() => (hasPendingEvent.value ? `Pending ${props.pending.type} Download` : ""));
 const historyDescription = computed(() => (hasEvent.value ? `Recent ${props.events[0].type} Download History` : ""));
+const reload = () => router.reload({ only: ["pending", "events"] });
 
-const { start, stop } = usePoll(hasPendingEvent, ["pending", "events"]);
+const { start, stop } = usePoll(10000, { reload }, { autoStart: false });
+
+onMounted(() => {
+  if (hasPendingEvent.value) {
+    start();
+  }
+});
 
 watch(hasPendingEvent, () => {
-  if (hasPendingEvent.value === true) {
+  if (hasPendingEvent.value) {
     start();
   } else {
     stop();
