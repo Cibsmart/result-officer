a884a512ab99929345fa3dd0881c3d7343ed2fdf
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-08 11:47:52 +0100

    Created Pipeline Check Class for FinalResultImport

---
+++ app/Pipelines/Checks/FinalResultImport/CheckSemester.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Pipelines\Checks\FinalResultImport;
+
+use App\Enums\ChecklistType;
+use App\Models\ExcelImportEvent;
+use Closure;
+
+final class CheckSemester
+{
+    private string $type = ChecklistType::SEMESTER->value;
+
+    /**
+     * @param array{event: 'App\Models\ExcelImportEvent', errors: array<string, string>} $data
+     * @return array{event: 'App\Models\ExcelImportEvent', errors: array<string, string>}
+     */
+    public function handle(array $data, Closure $next): array
+    {
+        $messages = [];
+
+        $event = $data['event'];
+        assert($event instanceof ExcelImportEvent);
+
+        $values = $event->rawFinalResults()->pluck($this->type)->unique();
+
+        foreach ($values as $value) {
+            if (in_array(strtolower($value), ['first', 'second'], true)) {
+                continue;
+            }
+
+            $messages[] = $value;
+        }
+
+        if (count($messages) > 0) {
+            $data['errors']["invalid_{$this->type}"] = $messages;
+        }
+
+        return $next($data);
+    }
+}
