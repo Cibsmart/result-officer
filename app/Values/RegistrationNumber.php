50ebde672d4054b087b2bb1a2d16586e5d97456f
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-08 09:35:49 +0100

    Updated Range of Registration that do not allow E grade

--- app/Values/RegistrationNumber.php
+++ app/Values/RegistrationNumber.php
@@ -55,6 +55,6 @@ final readonly class RegistrationNumber
 
     public function allowEGrade(): bool
     {
-        return ! in_array($this->year(), [2013, 2014, 2015, 2016, 2017], true);
+        return ! in_array($this->year(), [2011, 2012, 2013, 2014, 2015, 2016, 2017], true);
     }
 }
