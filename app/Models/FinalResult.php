4c1be5f4f81b6af395f7dd525d090e81342a6549
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-08 06:21:12 +0100

    Added static method to create FinalResult From RawFinalResult and methods to report discrepancies in Total and Grade

--- app/Models/FinalResult.php
+++ app/Models/FinalResult.php
@@ -6,7 +6,13 @@ namespace App\Models;
 
 use App\Enums\CourseStatus;
 use App\Enums\CreditUnit;
+use App\Enums\Grade;
 use App\Enums\RecordSource;
+use App\Values\ExamScore;
+use App\Values\InCourseScore;
+use App\Values\RegistrationNumber;
+use App\Values\SessionValue;
+use App\Values\TotalScore;
 use Illuminate\Database\Eloquent\Model;
 
 final class FinalResult extends Model
@@ -38,6 +44,94 @@ final class FinalResult extends Model
         $finalResult->save();
 
         return $finalResult;
+    }
+
+    public static function createFromRawFinalResult(
+        FinalSemesterEnrollment $finalSemesterEnrollment,
+        RawFinalResult $result,
+        FinalCourse $finalCourse,
+        CourseStatus $status,
+    ): self {
+        $registrationNumber = RegistrationNumber::new($result->registration_number);
+        $grade = Grade::from($result->grade);
+        $creditUnit = CreditUnit::from($result->credit_unit);
+        $inCourse = InCourseScore::new($result->in_course);
+        $exam = ExamScore::new($result->exam);
+        $total = TotalScore::new($result->total);
+        $total2 = TotalScore::fromInCourseAndExam($inCourse, $exam);
+
+        self::checkAndReportTotalDifference($total, $total2, $result);
+        self::checkAndReportGradeDifference($grade, $total->grade($registrationNumber->allowEGrade()), $result);
+
+        $session = self::getResultOriginalSession($result);
+
+        $lecturer = self::getLecturer($result);
+
+        $finalResult = new self();
+        $finalResult->final_semester_enrollment_id = $finalSemesterEnrollment->id;
+        $finalResult->final_course_id = $finalCourse->id;
+        $finalResult->course_status = $status;
+        $finalResult->credit_unit = $creditUnit;
+        $finalResult->total_score = $total->value;
+        $finalResult->scores = ['in_course' => $inCourse->value, 'exam' => $exam->value];
+        $finalResult->grade = $grade;
+        $finalResult->grade_point = $grade->point() * $creditUnit->value;
+        $finalResult->source = RecordSource::EXCEL;
+        $finalResult->lecturer_id = $lecturer;
+        $finalResult->original_session_id = $session;
+
+        $finalResult->save();
+
+        return $finalResult;
+    }
+
+    public static function checkAndReportTotalDifference(
+        TotalScore $total,
+        TotalScore $total2,
+        RawFinalResult $result,
+    ): void {
+        if ($total->value === $total2->value) {
+            return;
+        }
+
+        $result->message .= "Total score mismatch: Excel: {$total->value}, Computed: {$total2->value}";
+        $result->save();
+    }
+
+    public static function checkAndReportGradeDifference(
+        Grade $grade,
+        Grade $grade2,
+        RawFinalResult $result,
+    ): void {
+        if ($grade->value === $grade2->value) {
+            return;
+        }
+
+        $result->message .= "Grade mismatch. Excel: {$grade->value}, Computed: {$grade2->value}";
+        $result->save();
+    }
+
+    private static function getResultOriginalSession(RawFinalResult $result): ?int
+    {
+        $session = null;
+
+        if ($result->originating_session) {
+            $session = SessionValue::new($result->session);
+            $sessionOriginal = SessionValue::new($result->originating_session);
+
+            $session = $session->value === $sessionOriginal->value
+                ? null
+                : $session->getSession()->id;
+        }
+
+        return $session;
+    }
+
+    private static function getLecturer(RawFinalResult $result): ?int
+    {
+        return $result->examiner !== null
+            ? Lecturer::getOrCreateUsingName($result->examiner, $result->examiner_department)->id
+            : null;
     }
 
     /**
