9588f59498f76e39f4fdb3c07da945f79c8b23cb
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-09 21:08:02 +0100

    Implemented CurriculumCourses Import

---
+++ app/Imports/CurriculumCoursesImport.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Imports;
+
+use App\Models\ExcelImportEvent;
+use App\Models\RawProgramCurriculum;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+final class CurriculumCoursesImport implements ToModel, WithCalculatedFormulas, WithHeadingRow
+{
+    use Importable;
+
+    /** @param array<string, string> $headings */
+    public function __construct(private readonly ExcelImportEvent $event, private readonly array $headings)
+    {
+    }
+
+    /** @param array<string, string> $headings */
+    public static function new(ExcelImportEvent $event, array $headings): self
+    {
+        return new self($event, $headings);
+    }
+
+    /**
+     * @param array<string, string> $row
+     * {@inheritDoc}
+     */
+    public function model(array $row): ?RawProgramCurriculum
+    {
+        if (
+            ! isset($row[$this->headings['course_code']])
+            || $row[$this->headings['course_code']] === ''
+        ) {
+            return null;
+        }
+
+        return RawProgramCurriculum::fromExcelRow($row, $this->event, $this->headings);
+    }
+}
