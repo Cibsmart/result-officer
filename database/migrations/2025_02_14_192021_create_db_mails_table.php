3db1ea380ea01c2b21706f19cfb065ba69cec82e
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-14 19:53:00 +0100

    Add migrations and models for student histories and mails
    Created migrations for `student_histories` and `db_mails` tables along with their respective models. Updated type definitions to include a new record source, `"user"`, to align with recent database changes.

---
+++ database/migrations/2025_02_14_192021_create_db_mails_table.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\StudentHistory;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('db_mails', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->string('title');
+            $table->date('date');
+            $table->foreignIdFor(StudentHistory::class)->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('db_mails');
+    }
+};
