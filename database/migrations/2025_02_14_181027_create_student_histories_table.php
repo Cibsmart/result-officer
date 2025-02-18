8ca8d54d5f24abb1ddc98eb463879e1b50f2d5fe
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-18 13:43:23 +0100

    Add User relation to student_histories table
    This commit adds a nullable foreign key for the User model to the student_histories table to allow associating users with student updates. Additionally, the cascade on delete constraint was removed from the db_mail_id foreign key for better control over related data.

--- database/migrations/2025_02_14_181027_create_student_histories_table.php
+++ database/migrations/2025_02_14_181027_create_student_histories_table.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Models\DBMail;
 use App\Models\Student;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,6 +19,7 @@ return new class extends Migration
         Schema::create('student_histories', function (Blueprint $table): void {
             $table->id();
             $table->foreignIdFor(Student::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->nullable()->constrained();
             $table->unsignedBigInteger('modifiable_id');
             $table->string('modifiable_type');
             $table->string('action');
@@ -25,7 +27,7 @@ return new class extends Migration
             $table->json('data')->nullable();
             $table->text('remark');
             $table->string('source');
-            $table->foreignIdFor(DBMail::class, 'db_mail_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(DBMail::class, 'db_mail_id')->nullable()->constrained();
             $table->timestamps();
         });
     }
