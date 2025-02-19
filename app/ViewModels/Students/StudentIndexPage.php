97baf8dbda14ce2364372e2c1e55b6af650458a5
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-19 10:49:53 +0100

    Refactor pagination type to use AbstractPaginator
    Replaced Paginator with AbstractPaginator in StudentIndexPage and updated the student index method in the controller accordingly. Ensured type safety with an assert statement to verify the correct instance. This change improves compatibility and aligns with pagination best practices.

--- app/ViewModels/Students/StudentIndexPage.php
+++ app/ViewModels/Students/StudentIndexPage.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\ViewModels\Students;
 
-use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Pagination\AbstractPaginator;
 use Spatie\LaravelData\Data;
 
 final class StudentIndexPage extends Data
 {
     public function __construct(
-        /** @var \Illuminate\Contracts\Pagination\Paginator<\App\Data\Students\StudentBasicData> $paginated */
-        public readonly Paginator $paginated,
+        /** @var \Illuminate\Pagination\AbstractPaginator<\App\Data\Students\StudentBasicData> $paginated */
+        public readonly AbstractPaginator $paginated,
     ) {
     }
 }
