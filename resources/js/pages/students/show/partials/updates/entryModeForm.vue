9641ce4bfb687dea850ce1c04b1e8409a615d8a4
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-22 17:28:48 +0100

    Update entry mode form to use dropdown and remove mail fields
    Replaced the text input for entry mode with a dropdown for better user experience, utilizing the new `useEntryModes` composable. Removed mail-related fields and logic as they are no longer required in the form. Adjusted validation and submission logic accordingly.

--- resources/js/pages/students/show/partials/updates/entryModeForm.vue
+++ resources/js/pages/students/show/partials/updates/entryModeForm.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup>
 import BaseFormSection from "@/components/forms/baseFormSection.vue";
-import TextInput from "@/components/inputs/textInput.vue";
 import InputError from "@/components/inputs/inputError.vue";
 import InputLabel from "@/components/inputs/inputLabel.vue";
 import PrimaryButton from "@/components/buttons/primaryButton.vue";
 import { useForm } from "@inertiajs/vue3";
 import SecondaryButton from "@/components/buttons/secondaryButton.vue";
-import { computed, watch } from "vue";
+import { computed } from "vue";
 import CardFooter from "@/components/cards/cardFooter.vue";
 import TextareaInput from "@/components/inputs/textareaInput.vue";
-import Toggle from "@/components/inputs/toggle.vue";
+import SelectInput from "@/components/inputs/selectInput.vue";
+import { useEntryModes } from "@/composables/entryModes";
 
 const props = defineProps<{
   student: App.Data.Students.StudentData;
@@ -17,56 +17,46 @@ const props = defineProps<{
 
 const emit = defineEmits<(e: "close") => void>();
 
+const { modes } = useEntryModes();
+
 const form = useForm({
-  registration_number: props.student.baic.registrationNumber,
+  entry_mode: props.student.others.entryMode,
+  entry_mode_object: modes[0],
   remark: "",
-  has_mail: false,
-  mail_title: "",
-  mail_date: "",
 });
 
-const title = `Update Student's Registration Number (${props.student.baic.registrationNumber})`;
+const title = `Update Student's Entry Mode (${props.student.basic.registrationNumber})`;
 
-const canNotUpdate = computed(
-  () => props.student.baic.registrationNumber === form.registration_number || form.processing,
-);
-
-watch(
-  () => form.has_mail,
-  () => {
-    form.mail_title = "";
-    form.mail_date = "";
-    form.clearErrors();
-  },
-);
+const canNotUpdate = computed(() => props.student.others.entryMode === form.entry_mode_object.id || form.processing);
 
 const submit = () =>
-  form.patch(route("student.registrationNumber.update", { student: props.student.baic.slug }), {
-    onSuccess: () => emit("close"),
-  });
+  form
+    .transform((data) => ({ ...data, entry_mode: data.entry_mode_object.id }))
+    .patch(route("student.entryMode.update", { student: props.student.basic.slug }), {
+      onSuccess: () => emit("close"),
+    });
 </script>
 
 <template>
   <BaseFormSection
     :header="title"
-    description="Correct student's registration number and submit">
+    description="Correct student's Entry Mode and submit">
     <form
       class="mt-6 space-y-6"
       @submit.prevent="submit">
       <div class="">
         <InputLabel
-          for="registration_number"
-          value="Registration Number" />
+          for="entry_mode"
+          value="Entry Mode" />
 
-        <TextInput
-          id="registration_number"
-          v-model="form.registration_number"
-          autocomplete="off"
-          autofocus
-          required
-          type="text" />
+        <SelectInput
+          id="month"
+          v-model="form.entry_mode_object"
+          :items="modes"
+          :selected="student.others.entryMode"
+          class="mt-1 block w-full" />
 
-        <InputError :message="form.errors.registration_number" />
+        <InputError :message="form.errors.entry_mode" />
       </div>
 
       <div class="">
@@ -81,44 +71,6 @@ const submit = () =>
 
         <InputError :message="form.errors.remark" />
       </div>
-
-      <div class="">
-        <Toggle
-          v-model="form.has_mail"
-          label="Has mail" />
-      </div>
-
-      <template v-if="form.has_mail">
-        <div class="">
-          <InputLabel
-            for="mail_title"
-            value="Mail Title" />
-
-          <TextareaInput
-            id="mail_title"
-            v-model="form.mail_title"
-            autocomplete="mail_title"
-            required />
-
-          <InputError :message="form.errors.mail_title" />
-        </div>
-
-        <div class="mt-2">
-          <InputLabel
-            for="mail_date"
-            value="Mail Date" />
-
-          <TextInput
-            id="mail_date"
-            v-model="form.mail_date"
-            autocomplete="mail_date"
-            placeholder="YYYY-MM-DD"
-            required
-            type="text" />
-
-          <InputError :message="form.errors.mail_date" />
-        </div>
-      </template>
 
       <CardFooter class="mt-6">
         <div class="mt-2 flex justify-end">
