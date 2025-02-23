4296945d70578a01570d1a6bbfcb2547def304e0
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-23 23:15:37 +0100

    Add new API routes for sessions and states
    Added routes for fetching session and state data under the authenticated middleware group. These changes ensure proper routing to the respective controllers.

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Api\DepartmentController;
+use App\Http\Controllers\Api\SessionController;
+use App\Http\Controllers\Api\StateController;
 
 Route::middleware(['auth:sanctum'])->group(static function (): void {
     Route::get('departments', DepartmentController::class);
+    Route::get('sessions', SessionController::class::class);
+    Route::get('states', StateController::class);
 });
