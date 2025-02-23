2c435b4fd29329f2d22423d1846c0368b7fe4a7d
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-24 00:05:09 +0100

    Update student's state and local government form handling
    Replaced registration number update functionality with state and local government selection via dropdowns. Integrated data loading for states and local governments using composables and adjusted form structure accordingly. Removed unused mail-related fields for cleaner UI and code.

--- resources/js/pages/students/show/partials/updates/localGovernmentForm.vue
+++ resources/js/pages/students/show/partials/updates/localGovernmentForm.vue
@@ -6,10 +6,14 @@ import InputLabel from "@/components/inputs/inputLabel.vue";
 import PrimaryButton from "@/components/buttons/primaryButton.vue";
 import { useForm } from "@inertiajs/vue3";
 import SecondaryButton from "@/components/buttons/secondaryButton.vue";
-import { computed, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import CardFooter from "@/components/cards/cardFooter.vue";
 import TextareaInput from "@/components/inputs/textareaInput.vue";
 import Toggle from "@/components/inputs/toggle.vue";
+import { SelectItem } from "@/types";
+import SelectInput from "@/components/inputs/selectInput.vue";
+import { useStates } from "@/composables/states";
+import FormGroup from "@/components/forms/formGroup.vue";
 
 const props = defineProps<{
   student: App.Data.Students.StudentData;
@@ -17,57 +21,104 @@ const props = defineProps<{
 
 const emit = defineEmits<(e: "close") => void>();
 
+const { states, isLoading, error } = useStates();
+const localGovernments = ref<SelectItem[]>([{ id: 0, name: "Loading..." }]);
+
+const originalState = computed(() => states.value.find((state) => state.id === props.student.others.stateId));
+
 const form = useForm({
-  registration_number: props.student.basic.registrationNumber,
+  state: props.student.others.stateId,
+  local_government: props.student.others.localGovernmentId,
+  state_object: { id: props.student.others.stateId, name: "" } as SelectItem,
+  local_government_object: { id: props.student.others.localGovernmentId, name: "" } as SelectItem,
   remark: "",
-  has_mail: false,
-  mail_title: "",
-  mail_date: "",
 });
 
-const title = `Update Student's Registration Number (${props.student.basic.registrationNumber})`;
+const title = `Update Student's State (${props.student.basic.registrationNumber})`;
 
 const canNotUpdate = computed(
-  () => props.student.basic.registrationNumber === form.registration_number || form.processing,
+  () => props.student.others.localGovernmentId === form.local_government_object.id || form.processing,
 );
 
 watch(
-  () => form.has_mail,
+  () => isLoading.value,
   () => {
-    form.mail_title = "";
-    form.mail_date = "";
-    form.clearErrors();
+    if (!isLoading.value && states.value.length > 1) {
+      loadLocalGovernments(originalState.value);
+    }
   },
 );
 
 const submit = () =>
-  form.patch(route("student.registrationNumber.update", { student: props.student.basic.slug }), {
-    onSuccess: () => emit("close"),
-  });
+  form
+    .transform((data) => ({ ...data, local_government: data.local_government_object.id, state: data.state_object.id }))
+    .patch(route("student.localGovernment.update", { student: props.student.basic.slug }), {
+      onSuccess: () => emit("close"),
+    });
+
+const loadLocalGovernments = (state) => {
+  form.reset(["local_government_object", "local_government"]);
+  if (localGovernments.value !== null) {
+    localGovernments.value = state.localGovernments.data;
+  }
+};
 </script>
 
 <template>
   <BaseFormSection
     :header="title"
-    description="Correct student's registration number and submit">
+    description="Update student's department and submit">
+    <p
+      v-if="error"
+      class="text-red-600">
+      {{ error }}
+    </p>
+
     <form
       class="mt-6 space-y-6"
       @submit.prevent="submit">
-      <div class="">
-        <InputLabel
-          for="registration_number"
-          value="Registration Number" />
+      <FormGroup>
+        <div class="flex-1">
+          <InputLabel
+            for="state"
+            value="State" />
 
-        <TextInput
-          id="registration_number"
-          v-model="form.registration_number"
-          autocomplete="off"
-          autofocus
-          required
-          type="text" />
+          <SelectInput
+            v-if="!isLoading"
+            id="state"
+            v-model="form.state_object"
+            :items="states"
+            :selected="student.others.stateId"
+            @update:modelValue="loadLocalGovernments" />
 
-        <InputError :message="form.errors.registration_number" />
-      </div>
+          <SelectInput
+            v-else
+            id="state_loading"
+            :items="states" />
+
+          <InputError :message="form.errors.state" />
+        </div>
+
+        <div class="flex-1">
+          <InputLabel
+            for="local_government"
+            value="Local Government" />
+
+          <SelectInput
+            v-if="!isLoading && localGovernments.length > 1"
+            id="local_government"
+            v-model="form.local_government_object"
+            :items="localGovernments"
+            :selected="student.others.localGovernmentId" />
+
+          <SelectInput
+            v-else
+            id="local_government_loading"
+            :items="localGovernments" />
+
+          <InputError :message="form.errors.local_government" />
+        </div>
+      </FormGroup>
 
       <div class="">
         <InputLabel
@@ -81,44 +132,6 @@ const submit = () =>
 
         <InputError :message="form.errors.remark" />
       </div>
-
-      <div class="">
-        <Toggle
-          v-model="form.has_mail"
-          label="Has mail" />
-      </div>
-
-      <template v-if="form.has_mail">
-        <div class="">
-          <InputLabel
-            for="mail_title"
-            value="Mail Title" />
-
-          <TextareaInput
-            id="mail_title"
-            v-model="form.mail_title"
-            autocomplete="mail_title"
-            required />
-
-          <InputError :message="form.errors.mail_title" />
-        </div>
-
-        <div class="mt-2">
-          <InputLabel
-            for="mail_date"
-            value="Mail Date" />
-
-          <TextInput
-            id="mail_date"
-            v-model="form.mail_date"
-            autocomplete="mail_date"
-            placeholder="YYYY-MM-DD"
-            required
-            type="text" />
-
-          <InputError :message="form.errors.mail_date" />
-        </div>
-      </template>
 
       <CardFooter class="mt-6">
         <div class="mt-2 flex justify-end">
