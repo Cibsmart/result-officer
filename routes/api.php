e51e14a780b9307de6dbf3040edb3f7940edbb0e
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-23 20:33:30 +0100

    Add route for fetching departments
    Introduce a new API route to retrieve departments. This route is protected with the 'auth:sanctum' middleware and uses the DepartmentController.

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Api\DepartmentController;
+
 Route::middleware(['auth:sanctum'])->group(static function (): void {
+    Route::get('departments', DepartmentController::class);
 });
