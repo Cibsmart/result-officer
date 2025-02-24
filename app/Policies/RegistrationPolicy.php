f53a414336e5a78078da9cdc717d5c4b635a5f64
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-24 12:43:33 +0100

    Add policies and authorization to Student and Registration routes
    Introduce `StudentPolicy` and `RegistrationPolicy` to enforce authorization rules for `delete`, `restore`, and `forceDelete` actions. Update routes to use the `can` middleware, ensuring only authorized users can perform sensitive operations on students and registrations.

---
+++ app/Policies/RegistrationPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+final class RegistrationPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
+    public function delete(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
+    public function restore(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
+    public function forceDelete(): bool
+    {
+        return false;
+    }
+}
