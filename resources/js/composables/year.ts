f9b2d4f5b0d44b6bed3186896df76a9b838ecdd5
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-25 05:59:59 +0100

    Refactor `useYears` to accept dynamic start and end years.
    Updated the `useYears` function to include parameters for `startYear` and `endYear`, allowing greater flexibility when generating the years array. Default values ensure backward compatibility while making the function more versatile.

--- resources/js/composables/year.ts
+++ resources/js/composables/year.ts
@@ -1,14 +1,11 @@
 import { SelectItem } from "@/types";
 
-export function useYears() {
-  const startYear = 2009;
-  const currentYear = new Date().getFullYear();
-
+export function useYears(startYear: number = 1999, endYear: number = new Date().getFullYear()) {
   const years: SelectItem[] = [
     { id: 0, name: "Select Year" },
 
-    ...Array.from({ length: currentYear - startYear + 2 }, (_, i) => {
-      const year = currentYear - i;
+    ...Array.from({ length: endYear - startYear + 2 }, (_, i) => {
+      const year = endYear - i;
       return { id: year, name: year.toString() };
     }),
   ];
