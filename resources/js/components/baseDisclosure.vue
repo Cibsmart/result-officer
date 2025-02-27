cbac99d4b03cd928e2597fcf951dd5000cffc8d0
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-27 19:46:16 +0100

    Update baseDisclosure icon and improve UI styling
    Replaced ChevronUpIcon with ChevronRightIcon to better represent disclosure behavior. Simplified styling for better readability and adjusted title slot usage for consistency. These changes aim to enhance the component's usability and visual clarity.

--- resources/js/components/baseDisclosure.vue
+++ resources/js/components/baseDisclosure.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import { computed } from "vue";
-import { ChevronUpIcon } from "@heroicons/vue/20/solid";
+import { ChevronRightIcon } from "@heroicons/vue/20/solid";
 import Badge from "@/components/badge.vue";
 
 const props = withDefaults(
@@ -33,11 +33,9 @@ const maxWidthClass = computed(() => {
       class="mx-auto w-full rounded-sm bg-gray-100 p-2 dark:bg-gray-900">
       <Disclosure v-slot="{ open }">
         <DisclosureButton
-          class="flex w-full justify-between rounded-lg bg-indigo-100 px-2 py-1.5 text-left text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-hidden focus-visible:ring-3 focus-visible:ring-indigo-500/75 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700">
+          class="flex w-full justify-between rounded-lg px-2 py-1.5 text-left text-sm font-medium text-black hover:bg-gray-50 focus:outline-hidden focus-visible:ring-3 focus-visible:ring-indigo-500/75 dark:text-white dark:hover:bg-gray-800">
           <div class="flex flex-1 justify-between text-sm font-black">
-            <span>
-              <slot name="title" />
-            </span>
+            <span>{{ title }}</span>
 
             <Badge
               v-if="badge"
@@ -46,8 +44,8 @@ const maxWidthClass = computed(() => {
             </Badge>
           </div>
 
-          <ChevronUpIcon
-            :class="open ? 'rotate-180 transform' : ''"
+          <ChevronRightIcon
+            :class="open ? 'rotate-90 transform' : ''"
             class="ml-5 h-5 w-5 text-indigo-500 dark:text-white" />
         </DisclosureButton>
 
