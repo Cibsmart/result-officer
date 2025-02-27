153b2414a9b096afdddc6e52e395c57abbfd7b30
Barnabas Ifebude <cibsmarte@gmail.com>
2025-02-27 17:43:25 +0100

    Add `date` attribute to `ExcelImportEventData`
    The `date` attribute is now included in `ExcelImportEventData` to represent the creation date of events, formatted as 'M d, Y'. Corresponding TypeScript definitions have been updated to reflect this change.

--- resources/js/types/generated.d.ts
+++ resources/js/types/generated.d.ts
@@ -252,6 +252,7 @@ declare namespace App.Data.Imports {
     status: App.Enums.ImportEventStatus;
     statusColor: App.Enums.StatusColor;
     message: string | null;
+    date: string;
   };
   export type ExcelImportEventListData = {
     events: Array<App.Data.Imports.ExcelImportEventData>;
