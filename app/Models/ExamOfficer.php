0fdedc76e6ba56086e0677fe076df7765c3c03eb
Barnabas Ifebude <cibsmarte@gmail.com>
2025-03-01 13:55:04 +0100

    Optimize ExamOfficer retrieval with caching
    Implemented caching for `getOrCreate` to reduce database queries by storing results in cache for one day. Added a helper method `createNew` to streamline the creation of new exam officers. These changes improve performance and maintainability.

--- app/Models/ExamOfficer.php
+++ app/Models/ExamOfficer.php
@@ -5,17 +5,25 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 final class ExamOfficer extends Model
 {
     public static function getOrCreate(string $examOfficerName): self
     {
-        $examOfficer = self::query()->where('name', $examOfficerName)->first();
+        $examOfficer = Cache::remember("exam_officer_name.{$examOfficerName}",
+            now()->addDay(),
+            fn () => self::query()->where('name', $examOfficerName)->first());
 
         if ($examOfficer) {
             return $examOfficer;
         }
 
+        return self::createNew($examOfficerName);
+    }
+
+    public static function createNew(string $examOfficerName): self
+    {
         $examOfficer = new self();
         $examOfficer->name = $examOfficerName;
         $examOfficer->save();
