1e0753c0873f4f85487d11425403b9dd2dd5b208
Barnabas Ifebude <cibsmarte@gmail.com>
2025-03-02 22:30:19 +0100

    Add validation checks for Excel import pipeline
    Introduced individual validation classes to check data integrity for `CourseCode`, `CourseTitle`, `CreditUnit`, `Exam`, `InCourse`, `InCourse2`, `RegistrationNumber`, `Semester`, `Session`, and `Total`. These checks validate respective fields from raw Excel imports and populate error details for invalid or missing values where applicable.

---
+++ app/Pipelines/Checks/ExcelImports/RawExcelResults/CheckSession.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Pipelines\Checks\ExcelImports\RawExcelResults;
+
+use App\Enums\ChecklistType;
+use App\Models\ExcelImportEvent;
+use App\Values\SessionValue;
+use Closure;
+use Exception;
+
+final class CheckSession
+{
+    private string $type = ChecklistType::SESSION->value;
+
+    /**
+     * @param array{event: 'App\Models\ExcelImportEvent', errors: array<string, string>} $data
+     * @return array{event: 'App\Models\ExcelImportEvent', errors: array<string, string>}
+     */
+    public function handle(array $data, Closure $next): array
+    {
+        $messages = [];
+
+        $event = $data['event'];
+        assert($event instanceof ExcelImportEvent);
+
+        $values = $event->rawExcelResults()->pluck($this->type)->unique();
+
+        foreach ($values as $value) {
+            try {
+                SessionValue::new($value);
+            } catch (Exception) {
+                $messages[] = $value;
+            }
+        }
+
+        if (count($messages) > 0) {
+            $data['errors']["invalid_{$this->type}"] = $messages;
+        }
+
+        return $next($data);
+    }
+}
