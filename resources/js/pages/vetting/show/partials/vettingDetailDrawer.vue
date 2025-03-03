ce7682fd3a075d998af25fb6f179256d587b9bea
Barnabas Ifebude <cibsmarte@gmail.com>
2025-03-03 23:19:35 +0100

    Add VettingDetailDrawer component to display vetting steps
    Introduced a new Vue component for showing detailed vetting reports in a drawer. This includes dynamic handling of drawer state, fetching vetting steps, and displaying them with disclosure and card components. Improves user interface for vetting report management.

---
+++ resources/js/pages/vetting/show/partials/vettingDetailDrawer.vue
@@ -0,0 +1,71 @@
+<script lang="ts" setup>
+import Drawer from "@/components/drawer.vue";
+import CardHeading from "@/components/cards/cardHeader.vue";
+import Disclosure from "@/components/baseDisclosure.vue";
+import Card from "@/components/cards/card.vue";
+import { ref, computed, watch } from "vue";
+import { useVettingSteps } from "@/composables/vettingReports";
+
+const props = defineProps<{
+  slug: string;
+  openReportDrawer: boolean;
+}>();
+
+const emit = defineEmits<(e: "close") => void>();
+
+const { steps, fetchVettingSteps, isLoading } = useVettingSteps();
+
+const openDrawer = ref(false);
+watch(
+  () => props.openReportDrawer,
+  () => {
+    openDrawer.value = props.openReportDrawer;
+    fetchVettingSteps(props.slug);
+  },
+);
+
+const title = computed(() => `Vetting Report for ${props.slug}`);
+
+const handleClose = () => {
+  openDrawer.value = false;
+  emit("close");
+};
+</script>
+
+<template>
+  <Drawer
+    :show="openDrawer"
+    :title="title"
+    size="medium"
+    sub="Vetting Report Details"
+    @close="handleClose">
+    <template v-if="!isLoading">
+      <div
+        v-for="vettingStep in steps"
+        :key="vettingStep.id">
+        <Disclosure
+          :badge="vettingStep.status"
+          :color="vettingStep.color"
+          :title="vettingStep.title"
+          class="mt-2">
+          <Card>
+            <CardHeading>{{ vettingStep.description }}</CardHeading>
+
+            <ul
+              class="list-inside list-decimal divide-y divide-gray-300 dark:divide-gray-700"
+              role="list">
+              <li
+                v-for="report in vettingStep.reports"
+                :key="report.id"
+                class="p-2">
+                {{ report.message }}
+              </li>
+            </ul>
+          </Card>
+        </Disclosure>
+      </div>
+    </template>
+
+    <template v-else> Loading...</template>
+  </Drawer>
+</template>
