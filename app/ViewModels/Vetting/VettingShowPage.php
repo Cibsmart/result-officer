b7a8558e78d18a7122a17f504ed625ab94f0bc83
Barnabas Ifebude <cibsmarte@gmail.com>
2025-03-03 23:18:33 +0100

    Add TypeScript type and event property to VettingShowPage
    Introduced a new `event` property with a TypeScript type annotation using `VettingEventGroupData` for improved type checking and interoperability. Enhanced the class's functionality by incorporating a Closure type for the `event` property. These changes aim to ensure better code clarity and type safety.

--- app/ViewModels/Vetting/VettingShowPage.php
+++ app/ViewModels/Vetting/VettingShowPage.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\ViewModels\Vetting;
 
+use App\Data\Vetting\VettingEventGroupData;
+use Closure;
 use Inertia\DeferProp;
 use Spatie\LaravelData\Data;
+use Spatie\TypeScriptTransformer\Attributes\TypeScriptType;
 
 final class VettingShowPage extends Data
 {
     public function __construct(
+        #[TypeScriptType(VettingEventGroupData::class)]
+        public readonly Closure $event,
         public readonly DeferProp $data,
     ) {
     }
