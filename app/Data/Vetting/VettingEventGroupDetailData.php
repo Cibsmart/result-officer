0aad5264dd5f1a9c00843d8a35f9e1205a307323
Barnabas Ifebude <cibsmarte@gmail.com>
2025-03-03 20:58:32 +0100

    Add factory method to VettingEventGroupDetailData
    Introduced a static `for` method to instantiate `VettingEventGroupDetailData` from a `VettingEventGroup` model. This simplifies object creation by handling data transformation and associations within the method.

--- app/Data/Vetting/VettingEventGroupDetailData.php
+++ app/Data/Vetting/VettingEventGroupDetailData.php
@@ -4,15 +4,29 @@ declare(strict_types=1);
 
 namespace App\Data\Vetting;
 
+use App\Models\VettingEventGroup;
 use Illuminate\Support\Collection;
 use Spatie\LaravelData\Data;
 
 final class VettingEventGroupDetailData extends Data
 {
+    /** @param \Illuminate\Support\Collection<int, \App\Data\Vetting\VettingData> $vettings */
     public function __construct(
         public readonly VettingEventGroupData $event,
         public readonly Collection $vettings,
 
     ) {
+    }
+
+    public static function for(VettingEventGroup $vettingEvent): self
+    {
+        $vettings = $vettingEvent->vettingEvents()
+            ->with('student.program.department')
+            ->get();
+
+        return new self(
+            event: VettingEventGroupData::fromModel($vettingEvent),
+            vettings: VettingData::collect($vettings),
+        );
     }
 }
