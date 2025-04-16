1340a12e46436ab62bb8f20f6c666072debd0a84
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-16 18:21:39 +0100

    Add isActive key to all subMenuItems

--- resources/js/components/sidebar/Navigation.vue
+++ resources/js/components/sidebar/Navigation.vue
@@ -11,11 +11,31 @@ const mainNav: NavItem[] = [
         icon: Download,
         isActive: true,
         items: [
-            { title: 'Courses', href: route('download.courses.page') },
-            { title: 'Departments', href: route('download.departments.page') },
-            { title: 'Students', href: route('download.students.page', { selectedIndex: 0 }) },
-            { title: 'Registrations', href: route('download.registrations.page', { selectedIndex: 0 }) },
-            { title: 'Results', href: route('download.results.page', { selectedIndex: 0 }) },
+            {
+                title: 'Courses',
+                href: route('download.courses.page'),
+                isActive: route().current('download.courses.page'),
+            },
+            {
+                title: 'Departments',
+                href: route('download.departments.page'),
+                isActive: route().current('download.departments.page'),
+            },
+            {
+                title: 'Students',
+                href: route('download.students.page', { selectedIndex: 0 }),
+                isActive: route().current('download.students.page'),
+            },
+            {
+                title: 'Registrations',
+                href: route('download.registrations.page', { selectedIndex: 0 }),
+                isActive: route().current('download.registrations.page'),
+            },
+            {
+                title: 'Results',
+                href: route('download.results.page', { selectedIndex: 0 }),
+                isActive: route().current('download.results.page'),
+            },
         ],
     },
     {
@@ -24,8 +44,8 @@ const mainNav: NavItem[] = [
         icon: Users,
         isActive: false,
         items: [
-            { title: 'List', href: route('students.index') },
-            { title: 'View', href: route('students.show') },
+            { title: 'List', href: route('students.index'), isActive: route().current('students.index') },
+            { title: 'View', href: route('students.show'), isActive: route().current('students.show') },
         ],
     },
     {
@@ -34,11 +54,19 @@ const mainNav: NavItem[] = [
         icon: Folders,
         isActive: false,
         items: [
-            { title: 'View', href: route('results.index') },
-            { title: 'Summary', href: route('summary.form') },
-            { title: 'Composite', href: route('composite.form') },
-            { title: 'Upload Excel', href: route('import.results.index') },
-            { title: 'Export Excel', href: route('export.results.page') },
+            { title: 'View', href: route('results.index'), isActive: route().current('results.index') },
+            { title: 'Summary', href: route('summary.form'), isActive: route().current('summary.form') },
+            { title: 'Composite', href: route('composite.form'), isActive: route().current('composite.form') },
+            {
+                title: 'Upload Excel',
+                href: route('import.results.index'),
+                isActive: route().current('import.results.index'),
+            },
+            {
+                title: 'Export Excel',
+                href: route('export.results.page'),
+                isActive: route().current('export.results.page'),
+            },
         ],
     },
     {
@@ -47,9 +75,17 @@ const mainNav: NavItem[] = [
         icon: BookCheck,
         isActive: false,
         items: [
-            { title: 'Possible Graduands', href: route('graduand.index') },
-            { title: 'Vet Students', href: route('vettingEvent.index') },
-            { title: 'Cleared Students', href: route('department.cleared.index') },
+            { title: 'Possible Graduands', href: route('graduand.index'), isActive: route().current('graduand.index') },
+            {
+                title: 'Vet Students',
+                href: route('vettingEvent.index'),
+                isActive: route().current('vettingEvent.index'),
+            },
+            {
+                title: 'Cleared Students',
+                href: route('department.cleared.index'),
+                isActive: route().current('department.cleared.index'),
+            },
         ],
     },
     {
@@ -58,8 +94,12 @@ const mainNav: NavItem[] = [
         icon: GraduationCap,
         isActive: false,
         items: [
-            { title: 'View', href: route('finalResults.index') },
-            { title: 'Upload', href: route('import.final-results.index') },
+            { title: 'View', href: route('finalResults.index'), isActive: route().current('finalResults.index') },
+            {
+                title: 'Upload',
+                href: route('import.final-results.index'),
+                isActive: route().current('import.final-results.index'),
+            },
         ],
     },
     {
@@ -67,7 +107,13 @@ const mainNav: NavItem[] = [
         href: '#',
         icon: ListCheck,
         isActive: false,
-        items: [{ title: 'Upload', href: route('import.curriculum.index') }],
+        items: [
+            {
+                title: 'Upload',
+                href: route('import.curriculum.index'),
+                isActive: route().current('import.curriculum.index'),
+            },
+        ],
     },
 ];
 </script>
