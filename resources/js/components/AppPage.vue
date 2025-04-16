1bf83f9a6d7f6a73aca2f49b16c5ac73d6566a72
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-16 19:27:38 +0100

    Rename basePage to AppPage and move to components

---
+++ resources/js/components/AppPage.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import Heading from '@/components/Heading.vue';
+
+defineProps<{
+    title: string;
+    description: string;
+}>();
+</script>
+<template>
+    <div class="space-y-4 px-6 py-6">
+        <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+                <Heading
+                    :description="description"
+                    :title="title" />
+            </div>
+
+            <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                <slot name="actions" />
+            </div>
+        </div>
+
+        <slot />
+    </div>
+</template>
--- resources/js/layouts/main/partials/basePage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="px-4 text-gray-800 sm:px-6 lg:px-8 dark:text-white">
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
-                <slot />
-            </div>
-        </div>
-    </div>
-</template>
