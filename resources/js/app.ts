9bbe0f8dd6c2039a810edae0473145c63ab78ad9
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-16 14:04:57 +0100

    Remove Universal Layout

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,8 +4,6 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import LayoutMain from '@/layouts/main/layoutMain.vue';
-import LayoutGuest from '@/layouts/guest/layoutGuest.vue';
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 import { notifications } from '@/plugins/notifications';
@@ -31,18 +29,8 @@ const appName = import.meta.env.VITE_APP_NAME || 'ResultPro';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: async (name: string) => {
-        const page = await resolvePageComponent(
-            `./pages/${name}.vue`,
-            import.meta.glob<DefineComponent>('./pages/**/*.vue'),
-        );
-
-        const layout = name.startsWith('auth/') ? LayoutGuest : LayoutMain;
-
-        page.default.layout = page.default.layout || layout;
-
-        return page;
-    },
+    resolve: async (name: string) =>
+        resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
