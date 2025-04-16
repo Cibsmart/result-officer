a116bd95d26200d44d651d80214efca2e6cde39a
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-16 17:10:04 +0100

    Refactor Download Course Page
    - Remove custom components dependencies and replace with shadcn components

--- resources/js/pages/download/courses/page.vue
+++ resources/js/pages/download/courses/page.vue
@@ -1,14 +1,12 @@
 <script lang="ts" setup>
-import { Head, useForm } from '@inertiajs/vue3';
-import BasePage from '@/layouts/main/partials/basePage.vue';
-import BaseHeader from '@/layouts/main/partials/baseHeader.vue';
-import BaseSection from '@/layouts/main/partials/baseSection.vue';
-import { BreadcrumbsItem } from '@/types';
-import Breadcrumb from '@/components/breadcrumb.vue';
-import PrimaryButton from '@/components/buttons/primaryButton.vue';
-import BaseFormSection from '@/components/forms/baseFormSection.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import { BreadcrumbItem } from '@/types';
 import { computed } from 'vue';
 import ImportEvents from '@/pages/download/components/importEvents.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import Heading from '@/components/Heading.vue';
+import { Button } from '@/components/ui/button';
+import { Download } from 'lucide-vue-next';
 
 const props = defineProps<{
     events: Array<App.Data.Imports.ImportEventData>;
@@ -16,51 +14,39 @@ const props = defineProps<{
 }>();
 
 const hasPendingEvent = computed(() => props.pending !== null);
-const disableButton = computed(() => form.processing || hasPendingEvent.value);
 
-const pages: BreadcrumbsItem[] = [
-    {
-        name: 'Download Courses',
-        href: route('download.courses.page'),
-        current: route().current('download.courses.page'),
-    },
-];
-
-const form = useForm({});
-
-const submit = () => {
-    form.post(route('download.courses.store'));
-};
+const breadcrumbs: BreadcrumbItem[] = [{ title: 'Course Download', href: route('download.courses.page') }];
 </script>
 
 <template>
     <Head title="Download Courses Record" />
 
-    <Breadcrumb :pages="pages" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="space-y-4 px-6 py-6">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <Heading
+                        description="Download Course Records from the Portal"
+                        title="Download Courses" />
+                </div>
 
-    <BaseHeader>Download Courses Record</BaseHeader>
+                <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <Button
+                        :disabled="hasPendingEvent"
+                        asChild>
+                        <Link
+                            :href="route('download.courses.store')"
+                            method="post">
+                            <Download />
+                            Download Courses
+                        </Link>
+                    </Button>
+                </div>
+            </div>
 
-    <BasePage>
-        <BaseSection>
-            <BaseFormSection
-                description="Click to Download All Courses from the Portal"
-                header="Download Courses">
-                <form
-                    class="mt-6 space-y-6"
-                    @submit.prevent="submit">
-                    <div>
-                        <PrimaryButton
-                            :disabled="disableButton"
-                            tooltip
-                            >Download
-                        </PrimaryButton>
-                    </div>
-                </form>
-            </BaseFormSection>
-        </BaseSection>
-
-        <ImportEvents
-            :events="events"
-            :pending="pending" />
-    </BasePage>
+            <ImportEvents
+                :events="events"
+                :pending="pending" />
+        </div>
+    </AppLayout>
 </template>
