3fd503f013ab000434faf9434a27d2e80fc046b4
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-17 23:13:05 +0100

    Add index to inputs component group

---
+++ resources/js/components/inputs/index.ts
@@ -0,0 +1,4 @@
+export { default as TextInput } from './TextInput.vue';
+export { default as TextareaInput } from './TextareaInput.vue';
+export { default as InputError } from './InputError.vue';
+export { default as InputLabel } from './InputLabel.vue';
