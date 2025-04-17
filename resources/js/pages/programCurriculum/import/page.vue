2e344985fc9dd3a3c5ce5156069faa0794dbf5a8
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-17 11:27:38 +0100

    Refactor Curriculum Index page to use the new AppLayout

--- resources/js/pages/programCurriculum/import/page.vue
+++ resources/js/pages/programCurriculum/import/page.vue
@@ -1,9 +1,5 @@
 <script lang="ts" setup>
 import { Head, useForm } from '@inertiajs/vue3';
-import Breadcrumb from '@/components/breadcrumb.vue';
-import BaseHeader from '@/layouts/main/partials/baseHeader.vue';
-import BasePage from '@/layouts/main/partials/basePage.vue';
-import BaseSection from '@/layouts/main/partials/baseSection.vue';
 import PrimaryButton from '@/components/buttons/primaryButton.vue';
 import InputLabel from '@/components/inputs/inputLabel.vue';
 import FormGroup from '@/components/forms/formGroup.vue';
@@ -11,10 +7,13 @@ import BaseFormSection from '@/components/forms/baseFormSection.vue';
 import InputError from '@/components/inputs/inputError.vue';
 import EmptyState from '@/components/emptyState.vue';
 import { computed, ref, watch, onMounted } from 'vue';
-import { BreadcrumbsItem } from '@/types';
+import { BreadcrumbItem } from '@/types';
 import { usePoll } from '@inertiajs/vue3';
 import SelectInput from '@/components/inputs/selectInput.vue';
 import UploadedExcelList from '@/components/uploadedExcelList.vue';
+import AppPage from '@/components/AppPage.vue';
+import { Card } from '@/components/ui/card';
+import AppLayout from '@/layouts/AppLayout.vue';
 
 const props = defineProps<{
     data: App.Data.Imports.ExcelImportEventListData;
@@ -33,13 +32,7 @@ const submit = () => {
     form.post(route('import.curriculum.store'));
 };
 
-const pages: BreadcrumbsItem[] = [
-    {
-        name: 'Final Result Import',
-        href: route('import.curriculum.index'),
-        current: route().current('import.curriculum.index'),
-    },
-];
+const breadcrumbs: BreadcrumbItem[] = [{ title: 'Course List Import', href: route('import.curriculum.index') }];
 
 const hasEvent = computed(() => props.data.events.length > 0);
 
@@ -81,86 +74,86 @@ const programs = ref([{ id: 0, name: 'Select Program' }]);
 <template>
     <Head title="Import Curriculum" />
 
-    <Breadcrumb :pages="pages" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <AppPage
+            description="Import or Upload Course List from Excel to the Database"
+            title="Course List Import">
+            <Card class="p-6">
+                <BaseFormSection
+                    description="Select the Course List Excel (.xlsx) File and click Upload"
+                    header="Upload Course List">
+                    <form
+                        class="mt-6 space-y-6"
+                        @submit.prevent="submit">
+                        <FormGroup>
+                            <div class="flex-1 shrink-0">
+                                <InputLabel
+                                    for="department"
+                                    value="Department" />
 
-    <BaseHeader>Import Curriculum (Course List)</BaseHeader>
+                                <SelectInput
+                                    id="department"
+                                    v-model="form.department"
+                                    :items="departments.data"
+                                    @update:modelValue="loadPrograms" />
 
-    <BasePage>
-        <BaseSection>
-            <BaseFormSection
-                description="Select the Course List Excel (.xlsx) File and click Upload"
-                header="Upload Course List">
-                <form
-                    class="mt-6 space-y-6"
-                    @submit.prevent="submit">
-                    <FormGroup>
-                        <div class="flex-1 shrink-0">
-                            <InputLabel
-                                for="department"
-                                value="Department" />
+                                <InputError :message="form.errors.department" />
+                            </div>
 
-                            <SelectInput
-                                id="department"
-                                v-model="form.department"
-                                :items="departments.data"
-                                @update:modelValue="loadPrograms" />
+                            <div class="flex-1 shrink-0">
+                                <InputLabel
+                                    for="program"
+                                    value="Program" />
 
-                            <InputError :message="form.errors.department" />
-                        </div>
+                                <SelectInput
+                                    id="program"
+                                    v-model="form.program"
+                                    :items="programs" />
 
-                        <div class="flex-1 shrink-0">
-                            <InputLabel
-                                for="program"
-                                value="Program" />
+                                <InputError :message="form.errors.program" />
+                            </div>
+                        </FormGroup>
 
-                            <SelectInput
-                                id="program"
-                                v-model="form.program"
-                                :items="programs" />
+                        <FormGroup>
+                            <div class="flex-1">
+                                <InputLabel
+                                    for="file"
+                                    value="Excel File" />
 
-                            <InputError :message="form.errors.program" />
-                        </div>
-                    </FormGroup>
+                                <input
+                                    ref="fileInput"
+                                    type="file"
+                                    @change="onFileChange" />
 
-                    <FormGroup>
-                        <div class="flex-1">
-                            <InputLabel
-                                for="file"
-                                value="Excel File" />
+                                <progress
+                                    v-if="form.progress"
+                                    :value="form.progress.percentage"
+                                    max="100">
+                                    {{ form.progress.percentage }}%
+                                </progress>
 
-                            <input
-                                ref="fileInput"
-                                type="file"
-                                @change="onFileChange" />
+                                <InputError :message="form.errors.file" />
+                            </div>
 
-                            <progress
-                                v-if="form.progress"
-                                :value="form.progress.percentage"
-                                max="100">
-                                {{ form.progress.percentage }}%
-                            </progress>
+                            <div>
+                                <PrimaryButton :disabled="form.processing">Upload</PrimaryButton>
+                            </div>
+                        </FormGroup>
+                    </form>
+                </BaseFormSection>
+            </Card>
 
-                            <InputError :message="form.errors.file" />
-                        </div>
+            <Card>
+                <template v-if="hasEvent">
+                    <UploadedExcelList :data="data" />
+                </template>
 
-                        <div>
-                            <PrimaryButton :disabled="form.processing">Upload</PrimaryButton>
-                        </div>
-                    </FormGroup>
-                </form>
-            </BaseFormSection>
-        </BaseSection>
-
-        <BaseSection>
-            <template v-if="hasEvent">
-                <UploadedExcelList :data="data" />
-            </template>
-
-            <template v-else>
-                <EmptyState
-                    description="Start by selecting a file to upload and click Upload"
-                    title="No Course List Upload Found" />
-            </template>
-        </BaseSection>
-    </BasePage>
+                <template v-else>
+                    <EmptyState
+                        description="Start by selecting a file to upload and click Upload"
+                        title="No Course List Upload Found" />
+                </template>
+            </Card>
+        </AppPage>
+    </AppLayout>
 </template>
