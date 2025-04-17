5c4ee1d680cd3036e504cd908675c87f32b1d66e
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-17 16:15:31 +0100

    Delete Unused components

--- resources/js/components/cards/card.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div
-        class="divide-y divide-gray-300 overflow-hidden rounded-lg bg-white shadow-sm dark:divide-gray-700 dark:bg-gray-800">
-        <div class="px-2 py-3 sm:p-6">
-            <slot />
-        </div>
-    </div>
-</template>
--- resources/js/components/cards/cardHeader.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="border-b border-gray-200 bg-white pb-2 dark:border-gray-700 dark:bg-gray-800">
-        <h3 class="text-base font-semibold text-gray-700 dark:text-gray-300">
-            <slot />
-        </h3>
-    </div>
-</template>
