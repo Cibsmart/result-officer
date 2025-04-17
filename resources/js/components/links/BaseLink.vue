f7114ba3acd5bf2e720b7bc4ccf0d799a034d573
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-17 23:13:53 +0100

    Apply Button to BaseLink component

--- resources/js/components/links/BaseLink.vue
+++ resources/js/components/links/BaseLink.vue
@@ -1,14 +1,20 @@
 <script lang="ts" setup>
+import { Button } from '@/components/ui/button';
+
 const props = defineProps<{
     href: string;
 }>();
 </script>
 
 <template>
-    <a
-        :href="props.href"
-        class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-xs font-semibold tracking-widest text-gray-700 uppercase shadow-xs transition duration-150 ease-in-out hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden disabled:opacity-25 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:focus:ring-offset-gray-800"
-        target="_blank">
-        <slot />
-    </a>
+    <Button
+        asChild
+        size="sm">
+        <a
+            :href="props.href"
+            class="text-xs font-semibold tracking-widest uppercase"
+            target="_blank">
+            <slot />
+        </a>
+    </Button>
 </template>
