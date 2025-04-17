6b993fe3a561ee38b8f8c03b3ab3ff073415befb
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-17 23:14:52 +0100

    Rename and Capitalize custom input components

---
+++ resources/js/components/inputs/TextInput.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { Input } from '@/components/ui/input';
+
+const model = defineModel<string | number>({ required: true });
+</script>
+
+<template>
+    <Input v-model="model" />
+</template>
