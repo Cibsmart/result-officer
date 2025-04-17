3883ffe04ab3ceb7a78413bfd131ee96fe5fb992
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-17 17:26:09 +0100

    Refactor custom Button and Link Components to use shadcn button

--- resources/js/components/buttons/DangerButton.vue
+++ resources/js/components/buttons/DangerButton.vue
@@ -1,6 +1,11 @@
+<script lang="ts" setup>
+import { Button } from '@/components/ui/button';
+</script>
 <template>
-    <button
-        class="inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out hover:bg-red-500 focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:outline-hidden active:bg-red-700 dark:focus:ring-offset-gray-800">
+    <Button
+        class="text-xs font-semibold tracking-widest uppercase"
+        type="button"
+        variant="destructive">
         <slot />
-    </button>
+    </Button>
 </template>
--- resources/js/components/links/PrimaryLinkSmall.vue
+++ resources/js/components/links/PrimaryLinkSmall.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { Link } from '@inertiajs/vue3';
+import { Button } from '@/components/ui/button';
 
 const props = defineProps<{
     href: string;
@@ -7,9 +8,13 @@ const props = defineProps<{
 </script>
 
 <template>
-    <Link
-        :href="props.href"
-        class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-2 py-1 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800">
-        <slot />
-    </Link>
+    <Button
+        asChild
+        size="sm">
+        <Link
+            :href="props.href"
+            class="text-xs tracking-widest uppercase">
+            <slot />
+        </Link>
+    </Button>
 </template>
