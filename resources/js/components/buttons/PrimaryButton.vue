2ba03c7e7fd700675282b7e585aa1b93978317b4
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-17 16:39:33 +0100

    Use shadcn button in Primary and Secondary Button components

--- resources/js/components/buttons/PrimaryButton.vue
+++ resources/js/components/buttons/PrimaryButton.vue
@@ -1,8 +1,10 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Button } from '@/components/ui/button';
+</script>
 <template>
-    <button
-        class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden active:bg-gray-900 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:hover:bg-gray-400 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300"
+    <Button
+        class="text-xs font-semibold tracking-widest uppercase"
         type="submit">
         <slot />
-    </button>
+    </Button>
 </template>
