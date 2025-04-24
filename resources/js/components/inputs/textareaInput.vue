6cdeb418225616f30ef2af83a9305799e63c7f1e
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-24 18:10:58 +0100

    Rename input components to remove and delete camel-cased file names

--- resources/js/components/inputs/textareaInput.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import { Textarea } from '@/components/ui/textarea';
-
-const model = defineModel<string>({ required: true });
-</script>
-
-<template>
-    <Textarea v-model="model" />
-</template>
