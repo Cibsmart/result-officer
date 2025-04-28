defd2829565bebf44eedf118476850ba5ec6d96d
Barnabas Ifebude <cibsmarte@gmail.com>
2025-04-28 12:33:53 +0100

    Create UpdateGrade One-Time Command

---
+++ app/Console/Commands/OneTime/UpdateGradeCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands\OneTime;
+
+use Illuminate\Console\Command;
+
+final class UpdateGradeCommand extends Command
+{
+    protected $signature = 'rp:update-grade';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+
+    }
+}
